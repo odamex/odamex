647052b32d3e925e5c62b7889d7334f390b2bc02
Joe Kennedy <joejkennedy@gmail.com>
2007-01-26 23:54:13 +0000

    should fix sound distortyness
    SVN r117 (trunk)

--- client/sdl/i_sound.cpp
+++ client/sdl/i_sound.cpp
@@ -1,7 +1,7 @@
-// Emacs style mode select   -*- C++ -*- 
+// Emacs style mode select   -*- C++ -*-
 //-----------------------------------------------------------------------------
 //
-// $Id:$
+// $Id$
 //
 // Copyright (C) 1993-1996 by id Software, Inc.
 //
@@ -47,9 +47,9 @@ static int nextchannel = 0;
 
 CVAR (snd_crossover, "0", CVAR_ARCHIVE);
 
-static byte *expand_sound_data(byte *data, int samplerate, int length)
+static Uint8 *expand_sound_data(Uint8 *data, int samplerate, int length)
 {
-   byte *expanded = NULL;
+   Uint8 *expanded = NULL;
    int expanded_length;
    int expand_ratio;
    int i;
@@ -59,19 +59,19 @@ static byte *expand_sound_data(byte *data, int samplerate, int length)
       // Most of Doom's sound effects are 11025Hz
       // need to expand to 2 channels, 11025->22050
 
-      expanded = (byte *)Z_Malloc(length * 4, PU_STATIC, NULL);
+      expanded = (Uint8 *)Z_Malloc(length * 4, PU_STATIC, NULL);
 
-      for (i=0; i<length; ++i)
+      for (i = 0; i < length; ++i)
       {
-         expanded[i*4 + 0] = expanded[i*4 + 2] = data[i]; // chan L sample +0,+1
-		 expanded[i*4 + 1] = expanded[i*4 + 3] = data[i]; // chan R sample +0,+1
+         expanded[(i*4) + 0] = expanded[(i*4) + 2] = data[i]; // chan L sample +0,+1
+         expanded[(i*4) + 1] = expanded[(i*4) + 3] = data[i]; // chan R sample +0,+1
       }
    }
    else if (samplerate == 22050)
    {
-      expanded = (byte *)Z_Malloc(length * 2, PU_STATIC, NULL);
+      expanded = (Uint8 *)Z_Malloc(length * 2, PU_STATIC, NULL);
 
-      for (i=0; i<length; ++i)
+      for (i = 0; i < length; ++i)
       {
           expanded[i*2 + 0] = data[i]; // chan L
           expanded[i*2 + 1] = data[i]; // chan R
@@ -86,7 +86,7 @@ static byte *expand_sound_data(byte *data, int samplerate, int length)
       expanded_length = (length * 22050) / samplerate;
       expand_ratio = (length << 8) / expanded_length;
 
-      expanded = (byte *)Z_Malloc(expanded_length * 2, PU_STATIC, NULL);
+      expanded = (Uint8 *)Z_Malloc(expanded_length * 2, PU_STATIC, NULL);
 
       for (i=0; i<expanded_length; ++i)
       {
@@ -101,22 +101,102 @@ static byte *expand_sound_data(byte *data, int samplerate, int length)
 }
 
 
+// Expands the 11025Hz, 8bit, mono sound effects in Doom to
+// 22050Hz, 16bit stereo
+
+static Uint8 *ExpandSoundData(byte *data, int samplerate, int length)
+{
+   byte *expanded = NULL;
+   int expanded_length;
+   int expand_ratio;
+   int i;
+
+   if (samplerate == 11025)
+   {
+        // Most of Doom's sound effects are 11025Hz
+
+        // need to expand to 2 channels, 11025->22050 and 8->16 bit
+
+      expanded = (Uint8 *)Z_Malloc(length * 8, PU_STATIC, NULL);
+
+      for (i=0; i<length; ++i)
+      {
+         Uint16 sample;
+
+         sample = data[i] | (data[i] << 8);
+         sample -= 32768;
+
+         expanded[i * 8] = expanded[i * 8 + 2]
+               = expanded[i * 8 + 4] = expanded[i * 8 + 6] = sample & 0xff;
+         expanded[i * 8 + 1] = expanded[i * 8 + 3]
+               = expanded[i * 8 + 5] = expanded[i * 8 + 7] = (sample >> 8) & 0xff;
+      }
+   }
+   else if (samplerate == 22050)
+   {
+
+      expanded = (Uint8 *)Z_Malloc(length * 4, PU_STATIC, NULL);
+
+      for (i=0; i<length; ++i)
+      {
+         Uint16 sample;
+
+         sample = data[i] | (data[i] << 8);
+         sample -= 32768;
+
+         expanded[i * 4] = expanded[i * 4 + 2] = sample & 0xff;
+         expanded[i * 4 + 1] = expanded[i * 4 + 3] = (sample >> 8) & 0xff;
+      }
+   }
+   else
+   {
+        // Generic expansion function for all other sample rates
+
+        // number of samples in the converted sound
+      expanded_length = (length * 22050) / samplerate;
+      expand_ratio = (length << 8) / expanded_length;
+
+      expanded = (Uint8 *)Z_Malloc(expanded_length * 2, PU_STATIC, NULL);
+
+
+      for (i=0; i<expanded_length; ++i)
+      {
+         Uint16 sample;
+         int src;
+
+         src = (i * expand_ratio) >> 8;
+
+         sample = data[src] | (data[src] << 8);
+         sample -= 32768;
+
+            // expand 8->16 bits, mono->stereo
+
+         expanded[i * 4] = expanded[i * 4 + 2] = sample & 0xff;
+         expanded[i * 4 + 1] = expanded[i * 4 + 3] = (sample >> 8) & 0xff;
+      }
+   }
+
+   return expanded;
+}
+
 static void getsfx (struct sfxinfo_struct *sfx)
 {
 	int samplerate;
-	int length;
-	byte *data;
+	int length ,expanded_length;
+	Uint8 *data;
 	Mix_Chunk *chunk;
 
-	data = (byte *)W_CacheLumpNum(sfx->lumpnum, PU_STATIC);
+        data = (Uint8 *)W_CacheLumpNum(sfx->lumpnum, PU_STATIC);
 
 	samplerate = (data[3] << 8) | data[2];
 	length = (data[5] << 8) | data[4];
 
+        expanded_length = (length * 22050) / (samplerate / 4);
+
 	chunk = (Mix_Chunk *)Z_Malloc(sizeof(Mix_Chunk), PU_STATIC, NULL);
 	chunk->allocated = 1;
-	chunk->abuf = (Uint8 *)expand_sound_data(data + 8, samplerate, length);
-	chunk->alen = (length * 2) * (22050 / samplerate);
+        chunk->abuf = (Uint8 *)ExpandSoundData(data + 8, samplerate, length);
+	chunk->alen = expanded_length;
 	chunk->volume = MIX_MAX_VOLUME;
 	sfx->data = chunk;
 }
@@ -172,7 +252,7 @@ int I_StartSound (int id, int vol, int sep, int pitch, bool loop)
 			return -1;
 		}
         } while (sounds_in_use[channel] != -1);
-       
+
 	nextchannel = channel;
 
 	// play sound
@@ -182,7 +262,7 @@ int I_StartSound (int id, int vol, int sep, int pitch, bool loop)
 	sounds_in_use[channel] = soundtag;
         channel |= (soundtag << 8);
         soundtag++;
-        
+
 	// set seperation, etc.
 
 	I_UpdateSoundParams(channel, vol, sep, pitch);
@@ -195,13 +275,13 @@ void I_StopSound (int handle)
 {
 	if(!sound_initialized)
 		return;
-	
-        int tag = handle >> 8;     
-        
+
+        int tag = handle >> 8;
+
 	handle &= 0xff;
-        
+
         // Stopping wrong sound sounds_in_use[handle] != tag (handle)
-        
+
 	if (sounds_in_use[handle] != tag)
             fprintf(stderr, "stopping wrong sound: %i != %i (%i)\n", sounds_in_use[handle], tag, handle);
 
@@ -230,7 +310,7 @@ void I_UpdateSoundParams (int handle, float vol, int sep, int pitch)
         int tag = handle >> 8;
 
 	handle &= 0xff;
-        
+
 	if (sounds_in_use[handle] != tag)
             fprintf(stderr, "tag is wrong playing sound: tag:%i, handle:%i\n", tag, handle);
 
@@ -264,7 +344,7 @@ void I_InitSound (void)
 {
 	if(Args.CheckParm("-nosound"))
 		return;
-	
+
 	Printf(PRINT_HIGH, "I_InitSound: Initializing SDL_mixer\n");
 
 	if (SDL_InitSubSystem(SDL_INIT_AUDIO) < 0)
@@ -286,7 +366,7 @@ void I_InitSound (void)
 	}
 
 
-	if (Mix_OpenAudio(22050, AUDIO_U8, 2, 1024) < 0)
+        if (Mix_OpenAudio(22050, AUDIO_S16LSB, 2, 1024) < 0)
 	{
 		Printf(PRINT_HIGH, "Error initializing SDL_mixer: %s\n", SDL_GetError());
 		return;
@@ -301,9 +381,9 @@ void I_InitSound (void)
 	SDL_PauseAudio(0);
 
 	Printf(PRINT_HIGH, "I_InitSound: sound module ready\n");
-	
+
 	I_InitMusic();
-        
+
         // Half of fix for stopping wrong sound, these need to be -1
         // to be regarded as empty (they'd be initialised to something weird)
         for(int i = 0; i < 256; i++)
@@ -322,5 +402,5 @@ void STACK_ARGS I_ShutdownSound (void)
 }
 
 
-VERSION_CONTROL (i_sound_cpp, "$Id:$")
+VERSION_CONTROL (i_sound_cpp, "$Id$")
 
