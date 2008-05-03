fe6d4c6ab37825730d4b7b11ac7c687c804894b0
Denis Lukianov <denis@voxelsoft.com>
2008-05-03 19:04:16 +0000

    add tests for rcon and rcon_password
    SVN r663 (trunk)

---
+++ tests/commands/cmdRcon.tcl
@@ -0,0 +1,56 @@
+#!/bin/sh
+# \
+exec tclsh "$0" "$@"
+
+source tests/commands/common.tcl
+
+proc main {} {
+ global server client serverout clientout
+
+ # set defaults
+ expr srand([clock clicks])
+ set pass [expr int(rand()*1000000)]
+ client "print_stdout 1"
+ client "name Player"
+
+ wait
+
+ # no passwords
+ server "set rcon_password"
+ clear
+ client "set rcon_password"
+ expect $serverout {}
+
+ # password too short
+ server "set rcon_password a"
+ clear
+ client "set rcon_password a"
+ expect $serverout {}
+
+ # bad password
+ server "rcon_password $pass"
+ clear
+ client "rcon_password whatever"
+ expect $serverout {}
+
+ # good password
+ clear
+ client "rcon_password $pass"
+ expect $serverout {rcon login from 127.0.0.1:10501}
+ 
+ # rcon ability
+ clear
+ client "rcon say hi"
+ expect $serverout {[console]: hi}
+}
+
+start
+
+set error [catch { main }]
+
+if { $error } {
+ puts "FAIL Test crashed!"
+}
+
+end
+
