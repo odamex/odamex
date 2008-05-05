0df44073c0911141131d6ee505ec625a5bcca729
Denis Lukianov <denis@voxelsoft.com>
2008-05-05 19:46:59 +0000

    add timelimit test for deathmatch
    SVN r706 (trunk)

--- tests/commands/cvarTimelimit.tcl
+++ tests/commands/cvarTimelimit.tcl
@@ -4,8 +4,33 @@ exec tclsh "$0" "$@"
 
 source tests/commands/common.tcl
 
-proc main {} {
+proc deathmatch {} {
  global server client serverout clientout port
+
+ server "deathmatch 1"
+
+ # 7 second timelimit
+ clear
+ server "map 1"
+ server "timelimit 0.125"
+ server "map 2"
+ clear
+
+ # not yet
+ wait 5
+ expect $serverout ""
+ expect $serverout ""
+
+ # wait our time
+ wait 5
+ expect $serverout "Time limit hit. Game won by Player!"
+ expect $clientout "Time limit hit. Game won by Player!" 0
+}
+
+proc coop {} {
+ global server client serverout clientout port
+
+ server "deathmatch 0"
 
  # 7 second timelimit
  clear
@@ -25,6 +50,12 @@ proc main {} {
  expect $serverout "--------------------------------------"
  expect $clientout "" 0
  expect $clientout "--------------------------------------" 0
+}
+
+
+proc main {} {
+ deathmatch
+ coop
 }
 
 start
