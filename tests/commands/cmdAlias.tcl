139aa6e8c406ac2921e4ad210da2acaaae77ae29
Denis Lukianov <denis@voxelsoft.com>
2008-05-18 17:40:39 +0000

    add a basic test for the alias command
    SVN r783 (trunk)

---
+++ tests/commands/cmdAlias.tcl
@@ -0,0 +1,64 @@
+#!/bin/bash
+# \
+exec tclsh "$0" "$@"
+
+source tests/commands/common.tcl
+
+proc testAlias { module } {
+ global server client serverout clientout port
+
+ if { $module == "server"} {
+  set out $serverout
+  set offset 1
+ } else {
+  set out $clientout
+  set offset 0
+ }
+
+ # add an alias
+ clear
+ $module {alias cmdAlias "echo hi"}
+ expect $out {}
+ $module {cmdAlias}
+ expect $out {Unknown command echo hi} $offset
+
+ # remove the alias
+ clear
+ $module {alias cmdAlias}
+ expect $out {}
+ $module {cmdAlias}
+ expect $out {Unknown command cmdAlias} $offset
+
+ # add an alias to an alias
+ clear
+ $module {alias cmdAlias "echo hi"}
+ $module {alias cmdAlias2 "cmdAlias"}
+ expect $out {}
+ $module {cmdAlias2}
+ expect $out {Unknown command echo hi} $offset
+
+ # remove the alias
+ clear
+ $module {alias cmdAlias}
+ $module {alias cmdAlias2}
+ expect $out {}
+ $module {cmdAlias}
+ expect $out {Unknown command cmdAlias} $offset
+ $module {cmdAlias2}
+ expect $out {Unknown command cmdAlias2} $offset
+}
+
+proc main {} {
+ testAlias server
+ testAlias client
+}
+
+start
+
+set error [catch { main }]
+
+if { $error } {
+ puts "FAIL Test crashed!"
+}
+
+end
