d4995d9f848f9b68b6e14cae991c2c4d3a1cb42a
Denis Lukianov <denis@voxelsoft.com>
2008-05-18 19:32:29 +0000

    rough stab at odatv
    SVN r786 (trunk)

--- proxy/main.cpp
+++ proxy/main.cpp
@@ -17,14 +17,22 @@
 
 netadr_t net_local, net_remote;
 
-void LocalToRemote()
+void OnInit()
 {
-	NET_SendPacket(net_message.cursize, net_message.data, net_remote);
+	NET_StringToAdr("127.0.0.1:10667", &net_local);
+	NET_StringToAdr("voxelsoft.com:10666", &net_remote);
 }
 
-void RemoteToLocal()
+void OnPacket()
 {
-	NET_SendPacket(net_message.cursize, net_message.data, net_local);
+	if(NET_CompareAdr(net_from, net_local))
+	{
+		NET_SendPacket(net_message.cursize, net_message.data, net_remote);
+	}
+	else if(NET_CompareAdr(net_from, net_remote))
+	{
+		NET_SendPacket(net_message.cursize, net_message.data, net_local);
+	}
 }
 
 typedef void (*fp)();
@@ -32,32 +40,29 @@ typedef void (*fp)();
 struct protocol_t
 {
 	const char *name;
-	fp l2r, r2l;
+	fp onInit;
+	fp onPacket;
 };
+
+void OnPacketTV();
+void OnInitTV();
 
 int main()
 {
-	protocol_t protocol = {"transparent", LocalToRemote, RemoteToLocal};
+	//protocol_t protocol = {"transparent", OnInit, OnPacket};
+	protocol_t protocol = {"odatv", OnInitTV, OnPacketTV};
 
 	// Create a UDP socket
 	localport = 10999;
 	InitNetCommon();
 
-	NET_StringToAdr("127.0.0.1:10667", &net_local);
-	NET_StringToAdr("voxelsoft.com:10666", &net_remote);
+	protocol.onInit();
 
 	while (true)
 	{
 		while (NET_GetPacket())
 		{
-			if(NET_CompareAdr(net_from, net_local))
-			{
-				protocol.l2r();
-			}
-			else if(NET_CompareAdr(net_from, net_remote))
-			{
-				protocol.r2l();
-			}
+			protocol.onPacket();
 		}
 
 		usleep(1);
---
+++ proxy/tv/protocol.cpp
@@ -0,0 +1,95 @@
+//
+// OdaTV - Allow multiple clients to watch the same server
+// without creating extra traffic on that server
+//
+// This proxy will create the first connection transparently,
+// but other connections will be hidden from the server
+//
+
+#include <string.h>
+#include <vector>
+#include <iostream>
+
+#include "../../master/i_net.h"
+
+std::vector<netadr_t> spectators;
+netadr_t net_server;
+
+buf_t challenge_message(MAX_UDP_PACKET), first_message(MAX_UDP_PACKET);
+
+void OnInitTV()
+{
+	NET_StringToAdr("voxelsoft.com:10666", &net_server);	
+}
+
+void OnClientTV(int i)
+{
+	if(i == 0)
+	{
+		// only forward the first client's packet
+		NET_SendPacket(net_message.cursize, net_message.data, net_server);
+	}
+	else	
+	{
+		// TODO should keep client's tick info
+		// TODO and also catch client disconnect messages
+	}
+}
+
+void OnNewClientTV()
+{
+	spectators.push_back(net_from);
+
+	if(spectators.size() == 1)
+	{
+		// only forward the first client's packets
+		NET_SendPacket(net_message.cursize, net_message.data, net_server);
+		std::cout << "first client connect from " << NET_AdrToString(net_from) << std::endl;
+	}
+	else
+	{
+		// send back to the non-first client a fake challenge
+		NET_SendPacket(challenge_message.cursize, challenge_message.data, net_from);
+		std::cout << "shadow client connect from " << NET_AdrToString(net_from) << std::endl;
+	}
+}
+
+void OnPacketTV()
+{
+	if(NET_CompareAdr(net_from, net_server))
+	{
+		// if this is a server connect message, keep a copy for other clients
+		int t = MSG_ReadLong();
+		if(t == CHALLENGE)
+		{
+			std::cout << "first client connected" << std::endl;
+			challenge_message = net_message;
+		}
+		if(t == 0)
+		{
+			first_message = net_message;
+		}
+
+		// replicate server packet to all connected clients
+		for(int i = 0; i < spectators.size(); i++)
+		{
+			NET_SendPacket(net_message.cursize, net_message.data, spectators[i]);
+		}
+	}
+	else
+	{
+		// is this an existing client?
+		for(int i = 0; i < spectators.size(); i++)
+		{
+			if(NET_CompareAdr(net_from, spectators[i]))
+			{
+				OnClientTV(i);
+				return;
+			}
+		}
+
+		// must be a new client
+		OnNewClientTV();
+	}
+}
+
