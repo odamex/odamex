10d20a4244ba92d6e9f42162087bd1605bbf1d70
Denis Lukianov <denis@voxelsoft.com>
2008-05-18 17:47:14 +0000

    deprecate old tests
    SVN r784 (trunk)

--- tests/all.sh
+++ tests/all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-for i in tests/*; do echo $i | grep -v $0 | grep "\.sh" | awk '{print "echo -n " $1 "\" \" ;" $1 " 2>>tmp"}' | bash; done;
+for i in tests/*.tcl; do echo $i; $i; done;
 for i in tests/commands/*.tcl; do echo $i; $i; done;
