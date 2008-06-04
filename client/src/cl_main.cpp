ed335e2c6863de52319684066a4e0987b59c9729
rice <russell@odamex.net>
2008-06-04 04:34:12 +0000

    Improve the WAD download system - Allow resumable downloads (you can only resume if the server disconnects you and you reconnect straight away) - Allocate the buf_t on the heap, this calls the correct constructor/destructor when necessary. - Do an over-the-boundary check when a download "completes", so we can verify with the MD5 hash system that wad file is correct, since the byte count can never can be accurate.
    SVN r914 (trunk)

--- client/src/cl_main.cpp
+++ client/src/cl_main.cpp
@@ -1930,9 +1930,84 @@ void CL_ExitLevel()
 struct download_t
 {
 	std::string filename, md5;
-	buf_t buf;
+	buf_t *buf;
 	unsigned int got_bytes;
-}download;
+} download = { "", "", NULL, 0 };
+
+
+void IntDownloadComplete(void)
+{
+    std::string actual_md5 = MD5SUM(download.buf->ptr(), download.buf->maxsize());
+
+	Printf(PRINT_HIGH, "\nDownload complete, got %u bytes\n", download.buf->maxsize());
+	Printf(PRINT_HIGH, "%s\n %s\n", download.filename.c_str(), actual_md5.c_str());
+
+	if(download.md5 == "")
+	{
+		Printf(PRINT_HIGH, "Server gave no checksum, assuming valid\n", (int)download.buf->maxsize());
+	}
+	else if(actual_md5 != download.md5)
+	{
+		Printf(PRINT_HIGH, " %s on server\n", download.md5.c_str());
+		Printf(PRINT_HIGH, "Download failed: bad checksum\n");
+
+        download.filename = "";
+        download.md5 = "";
+        download.got_bytes = 0;
+		
+        if (download.buf != NULL)
+        {
+            delete download.buf;
+            download.buf = NULL;
+        }   
+			
+        CL_QuitNetGame();
+        return;
+    }
+
+    // got the wad! save it!
+    std::string filename = "./"; // denis - todo try first of waddir/DOOMWADDIR/startdir/progdir in that order
+    filename += download.filename;
+
+    // check for existing file
+    if(M_FileExists(filename.c_str()))
+    {
+        // there is an existing file, so use a new file whose name includes the checksum
+        filename += ".";
+        filename += actual_md5;
+    }
+
+    if (!M_WriteFile(filename, download.buf->ptr(), download.buf->maxsize()))
+    {
+        download.filename = "";
+        download.md5 = "";
+        download.got_bytes = 0;
+		
+        if (download.buf != NULL)
+        {
+            delete download.buf;
+            download.buf = NULL;
+        }   
+            
+        CL_QuitNetGame();
+        return;            
+    }
+
+    Printf(PRINT_HIGH, "Saved download as \"%s\"\n", filename.c_str());
+
+    download.filename = "";
+    download.md5 = "";
+    download.got_bytes = 0;
+
+    if (download.buf != NULL)
+    {
+        delete download.buf;
+        download.buf = NULL;
+    }
+
+    CL_QuitNetGame();
+    CL_Reconnect();
+}
 
 //
 // CL_RequestDownload
@@ -1940,18 +2015,35 @@ struct download_t
 //
 void CL_RequestDownload(std::string filename, std::string filehash)
 {
+    // [Russell] - Allow resumeable downloads
+	if ((download.filename != filename) && 
+        (download.md5 != filehash))
+    {
+        download.filename = filename;
+        download.md5 = filehash;
+        download.got_bytes = 0;
+    }
+	
 	// denis todo clear previous downloads
 	MSG_WriteMarker(&net_buffer, clc_wantwad);
 	MSG_WriteString(&net_buffer, filename.c_str());
 	MSG_WriteString(&net_buffer, filehash.c_str());
-	MSG_WriteLong(&net_buffer, 0);
-
-	download.filename = filename;
-	download.md5 = filehash;
+	MSG_WriteLong(&net_buffer, download.got_bytes);
 
 	NET_SendPacket(net_buffer, serveraddr);
 
 	Printf(PRINT_HIGH, "Requesting download...\n");
+	
+	// check for completion
+	// [Russell] - We go over the boundary, because sometimes the download will
+	// pause at 100% if the server disconnected you previously, you can 
+	// reconnect a couple of times and this will let the checksum system do its
+	// work
+	if ((download.buf != NULL) && 
+        (download.got_bytes >= download.buf->maxsize()))
+	{
+        IntDownloadComplete();
+	}
 }
 
 //
@@ -1975,10 +2067,19 @@ void CL_DownloadStart()
 		CL_QuitNetGame();
 		return;
 	}
-
-	download.got_bytes = 0;
-	download.buf.resize(file_len);
-
+	
+    // [Russell] - Allow resumeable downloads
+	if (download.got_bytes == 0)
+    {
+        if (download.buf != NULL)
+        {
+            delete download.buf;
+            download.buf = NULL;
+        }
+        
+        download.buf = new buf_t ((size_t)file_len);
+    }
+    
 	Printf(PRINT_HIGH, "Downloading %d bytes...\n", file_len);
 }
 
@@ -1997,9 +2098,20 @@ void CL_Download()
 		return;
 
 	// check ranges
-	if(offset + len > download.buf.maxsize() || len > left)
+	if(offset + len > download.buf->maxsize() || len > left || p == NULL)
 	{
-		Printf(PRINT_HIGH, "Bad download packet (%d, %d) encountered (%d), aborting\n", (int)offset, (int)left, (int)download.buf.size());
+		Printf(PRINT_HIGH, "Bad download packet (%d, %d) encountered (%d), aborting\n", (int)offset, (int)left, (int)download.buf->size());
+        
+        download.filename = "";
+        download.md5 = "";
+        download.got_bytes = 0;
+        
+        if (download.buf != NULL)
+        {
+            delete download.buf;
+            download.buf = NULL;
+        }
+        
 		CL_QuitNetGame();
 		return;
 	}
@@ -2020,64 +2132,30 @@ void CL_Download()
 	NET_SendPacket(net_buffer, serveraddr);
 
 	// copy into downloaded buffer
-	memcpy(download.buf.ptr() + offset, p, len);
+	memcpy(download.buf->ptr() + offset, p, len);
 	download.got_bytes += len;
 
 	// calculate percentage for the user
 	static int old_percent = 0;
-	int percent = (download.got_bytes*100)/download.buf.maxsize();
+	int percent = (download.got_bytes*100)/download.buf->maxsize();
 	if(percent != old_percent)
 	{
-		Printf(PRINT_HIGH, ".");
-
 		if(!(percent % 10))
 			Printf(PRINT_HIGH, "%d%%", percent);
+		else
+            Printf(PRINT_HIGH, ".");
 
 		old_percent = percent;
 	}
 
 	// check for completion
-	if(download.got_bytes == download.buf.maxsize())
+	// [Russell] - We go over the boundary, because sometimes the download will
+	// pause at 100% if the server disconnected you previously, you can 
+	// reconnect a couple of times and this will let the checksum system do its
+	// work
+	if(download.got_bytes >= download.buf->maxsize())
 	{
-		std::string actual_md5 = MD5SUM(download.buf.ptr(), download.buf.maxsize());
-
-		Printf(PRINT_HIGH, "\nDownload complete, got %d bytes\n", (int)download.buf.maxsize());
-		Printf(PRINT_HIGH, "%s\n %s\n", download.filename.c_str(), actual_md5.c_str());
-
-		if(download.md5 == "")
-		{
-			Printf(PRINT_HIGH, "Server gave no checksum, assuming valid\n", (int)download.buf.maxsize());
-		}
-		else if(actual_md5 != download.md5)
-		{
-			Printf(PRINT_HIGH, " %s on server\n", download.md5.c_str());
-			Printf(PRINT_HIGH, "Download failed: bad checksum\n");
-			CL_QuitNetGame();
-			return;
-		}
-
-		// got the wad! save it!
-		std::string filename = "./"; // denis - todo try first of waddir/DOOMWADDIR/startdir/progdir in that order
-		filename += download.filename;
-
-		// check for existing file
-		if(M_FileExists(filename.c_str()))
-		{
-			// there is an existing file, so use a new file whose name includes the checksum
-			filename += ".";
-			filename += actual_md5;
-		}
-
-        if (!M_WriteFile(filename, download.buf.ptr(), download.buf.maxsize()))
-        {
-			CL_QuitNetGame();
-			return;            
-        }
-
-		Printf(PRINT_HIGH, "Saved download as \"%s\"\n", filename.c_str());
-
-		CL_QuitNetGame();
-		CL_Reconnect();
+        IntDownloadComplete();
 	}
 }
 
