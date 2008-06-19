600ca679db8ed65d4fddd34966170e1e0d931060
rice <russell@odamex.net>
2008-06-19 12:09:12 +0000

    sync cvars flags with server variants
    SVN r953 (trunk)

--- client/src/g_game.cpp
+++ client/src/g_game.cpp
@@ -84,8 +84,8 @@ void	G_DoSaveGame (void);
 
 void	CL_RunTics (void);
 
-CVAR (skill, "1", CVAR_ARCHIVE | CVAR_NOENABLEDISABLE) // [Toke - todo] should not be cvar on client
-CVAR (deathmatch, "1", CVAR_ARCHIVE | CVAR_NOENABLEDISABLE)  // [Toke - todo] should not be cvar on client
+CVAR (skill, "1", CVAR_ARCHIVE | CVAR_NOENABLEDISABLE | CVAR_LATCH) // [Toke - todo] should not be cvar on client
+CVAR (deathmatch, "1", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_LATCH)  // [Toke - todo] should not be cvar on client
 
 EXTERN_CVAR (novert)
 
