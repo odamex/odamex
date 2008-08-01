a2a59ce871c64c9e20f128d1d564a4c18ee202cf
rice <russell@odamex.net>
2008-08-01 09:25:25 +0000

    Change launcher and README file to 0.4.1
    SVN r1036 (trunk)

--- odalaunch/src/dlg_main.cpp
+++ odalaunch/src/dlg_main.cpp
@@ -657,7 +657,7 @@ void dlgMain::OnOpenOdaGet(wxCommandEvent &event)
 // About information
 void dlgMain::OnAbout(wxCommandEvent& event)
 {
-    wxString strAbout = _T("Odamex Launcher 0.4 - "
+    wxString strAbout = _T("Odamex Launcher 0.4.1 - "
                             "Copyright 2008 The Odamex Team");
     
     wxMessageBox(strAbout, strAbout);
