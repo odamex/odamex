0bbe35bc1a2a46856c0e929a26a0ed4c5d29be7d
Mike Lightner (Manc) <mike@odamex.net>
2010-01-13 06:31:08 +0000

    The server's -iwad parameter is no longer restricted to WADs that have the IWAD header only. Add a comment to keep my changes to d_main.cpp identical across components
    SVN r1410 (trunk)

--- server/src/d_main.cpp
+++ server/src/d_main.cpp
@@ -266,7 +266,7 @@ static bool CheckIWAD (std::string suggestion, std::string &titlestring)
 			if(M_FileExists(suggestion.c_str()))
 				iwad = suggestion;
 		}
-
+		/*	[ML] Removed 1/13/10: we can trust the user to provide an iwad
 		if(iwad.length())
 		{
 			FILE *f;
@@ -291,6 +291,7 @@ static bool CheckIWAD (std::string suggestion, std::string &titlestring)
 				fclose(f);
 			}
 		}
+		*/
 	}
 
 	if(!iwad.length())
