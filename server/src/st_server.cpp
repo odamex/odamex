059b37d1356e7cec28f771db065bc4b87bc28e04
Mike Lightner (Manc) <mike@odamex.net>
2010-03-22 22:37:36 +0000

    Add st_server.cpp to contain the dummy functions for the status bar definitions.  Hopefully not a long term solution but we'll see (see server r_sky.cpp).
    SVN r1591 (odaraven)

---
+++ server/src/st_server.cpp
@@ -0,0 +1,37 @@
+// Emacs style mode select   -*- C++ -*-
+//-----------------------------------------------------------------------------
+//
+// $Id: c_console.cpp 1371 2010-01-04 06:21:26Z mike $
+//
+// Copyright (C) 1998-2006 by Randy Heit (ZDoom 1.22).
+// Copyright (C) 2006-2009 by The Odamex Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//	Server dummy module for status bar definitions in gamemode
+//	[ML] 3/22/10 - I don't think this should be necessary in the long term 
+//-----------------------------------------------------------------------------
+
+#include "st_stuff.h"
+
+void ST_DoomTicker (void) {}
+void ST_DoomDrawer (void) {}
+void ST_DoomStart (void) {}
+void ST_DoomInit (void) {}
+
+void ST_HticTicker (void) {}
+void ST_HticDrawer (void) {}
+void ST_HticStart (void) {}
+void ST_HticInit (void) {}
+
+
+VERSION_CONTROL (st_server_cpp, "$Id: c_console.cpp 1371 2010-01-04 06:21:26Z mike $")
