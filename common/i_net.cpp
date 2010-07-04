d8ae723e9353570f5a233dad93eaaab8ee9a7c22
Michael J. Wood <mwoodj@huntsvegas.org>
2010-07-04 04:52:19 +0000

    - Add sv_bindaddress cvar which allows a server to be bound to a specified IP address.  - In NET_StringToAdr() don't test if the string contains an IP address as gethostbyname() handles it anyway.
     Thanks to GhostlyDeath for the patch (Bug #587)
    
    
    SVN r1635 (trunk)

--- common/i_net.cpp
+++ common/i_net.cpp
@@ -95,6 +95,7 @@ buf_t compressed, decompressed;
 lzo_byte wrkmem[LZO1X_1_MEM_COMPRESS];
 
 EXTERN_CVAR(port)
+EXTERN_CVAR(sv_bindaddress)
 
 msg_info_t clc_info[clc_max];
 msg_info_t svc_info[svc_max];
@@ -114,25 +115,50 @@ SOCKET UDPsocket (void)
 	return s;
 }
 
+void NetadrToSockadr (netadr_t *a, struct sockaddr_in *s);
+void SockadrToNetadr (struct sockaddr_in *s, netadr_t *a);
+
 //
 // BindToLocalPort
 //
 void BindToLocalPort (SOCKET s, u_short wanted)
 {
 	int v;
+	struct hostent* h;
 	struct sockaddr_in address;
+	netadr_t na;
 
 	memset (&address, 0, sizeof(address));
 	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
+	
+	// GhostlyDeath <July 3, 2010> -- Bind to local IP
+	if (serverside)
+	{
+		// Just use our existing function
+		if (!NET_StringToAdr(sv_bindaddress.cstring(), &na))
+			address.sin_addr.s_addr = INADDR_ANY;	// Failure
+		else
+			NetadrToSockadr(&na, &address);
+	}
+	else	// Client should use any address
+		address.sin_addr.s_addr = INADDR_ANY;
 	u_short next = wanted;
 
 	// denis - try several ports
 	do
 	{
 		address.sin_port = htons(next++);
-
+		
 		v = bind (s, (sockaddr *)&address, sizeof(address));
+		
+		// GhostlyDeath <July 3, 2010> -- Fallback to any address
+		if (address.sin_addr.s_addr != INADDR_ANY && (errno == EACCES || errno == EADDRNOTAVAIL))
+		{
+			Printf (PRINT_HIGH, "BindToLocalPort: %s\n", strerror(errno));
+			next--;
+			address.sin_addr.s_addr = INADDR_ANY;
+			continue;
+		}
 
 		if(next > wanted + 16)
 		{
@@ -145,7 +171,14 @@ void BindToLocalPort (SOCKET s, u_short wanted)
 	sprintf(tmp, "%d", next - 1);
 	port.ForceSet(tmp);
 
-	Printf(PRINT_HIGH, "Bound to local port %d\n", next - 1);
+	if (address.sin_addr.s_addr == INADDR_ANY)
+		Printf(PRINT_HIGH, "Bound to local port %d\n", next - 1);
+	else
+	{
+		SockadrToNetadr(&address, &na);
+		
+		Printf(PRINT_HIGH, "Bound to local address %s\n", NET_AdrToString(na));
+	}
 }
 
 
@@ -207,17 +240,12 @@ bool NET_StringToAdr (const char *s, netadr_t *a)
              *colon = 0;
              sadr.sin_port = htons(atoi(colon+1));
           }
-
-     if (copy[0] >= '0' && copy[0] <= '9')
-     {
-          *(int *)&sadr.sin_addr = inet_addr(copy);
-     }
-     else
-     {
-          if (! (h = gethostbyname(copy)) )
-                return 0;
-          *(int *)&sadr.sin_addr = *(int *)h->h_addr_list[0];
-     }
+	
+	// GhostlyDeath <July 3, 2010> -- Just use gethostbyname(), because the old
+	// code was too bad and should never be mentioned ever again.
+	if (!(h = gethostbyname(copy)))
+		return false;
+	*(int *)&sadr.sin_addr = *(int *)h->h_addr_list[0];
 
      SockadrToNetadr (&sadr, a);
 
@@ -286,12 +314,12 @@ void NET_SendPacket (buf_t &buf, netadr_t &to)
 {
     int                   ret;
     struct sockaddr_in    addr;
-
+   	
     NetadrToSockadr (&to, &addr);
 
 	ret = sendto (net_socket, (const char *)buf.ptr(), buf.size(), 0, (struct sockaddr *)&addr, sizeof(addr));
 
-	buf.clear();
+	buf.clear();	
 
     if (ret == -1)
     {
