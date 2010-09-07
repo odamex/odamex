a504f8cc810b2302ee62b145958a697088235e6f
Mike Lightner (Manc) <mike@odamex.net>
2010-09-07 02:20:57 +0000

    Add header and version footer
    SVN r1878 (0.6-dev)

--- common/p_things.cpp
+++ common/p_things.cpp
@@ -1,4 +1,25 @@
+// Emacs style mode select   -*- C++ -*-
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright (C) 2006-2010 by The Odamex Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
 // [RH] new file to deal with general things
+//
+//-----------------------------------------------------------------------------
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -351,3 +372,5 @@ BOOL P_DeactivateMobj (AActor *mobj)
 	}
 	return false;
 }
+
+VERSION_CONTROL (p_spec_cpp, "$Id$")
