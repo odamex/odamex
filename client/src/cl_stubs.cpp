2a7743ecd99fdfc15e5e970b576376f8a32540a4
Charlie Gunyon <charles.gunyon@gmail.com>
2010-09-09 15:46:13 +0000

    Added cl_stubs.cpp and sv_stubs.cpp to client and server respectively, these files will hold all necessary stubs; additionally moved stubs found in client/d_netinfo.cpp and server/d_netinfo.cpp into those files... however both client and server have the same stubs, will have to check if those functions are actually called anywhere.
    SVN r1887 (0.6-dev)

---
+++ client/src/cl_stubs.cpp
@@ -0,0 +1,31 @@
+// Emacs style mode select   -*- C++ -*-
+//-----------------------------------------------------------------------------
+//
+// $Id: d_netinfo.cpp 1788 2010-08-24 04:42:57Z russellrice $
+//
+// Copyright (C) 1998-2006 by Randy Heit (ZDoom).
+// Copyright (C) 2006-2010 by The Odamex Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//	 Clientside function stubs
+//
+//-----------------------------------------------------------------------------
+
+void D_SetupUserInfo (void) {}
+void D_SendServerInfoChange (const cvar_t *cvar, const char *value) {}
+void D_DoServerInfoChange (byte **stream) {}
+void D_WriteUserInfoStrings (int i, byte **stream, bool compact) {}
+void D_ReadUserInfoStrings (int i, byte **stream, bool update) {}
+
+VERSION_CONTROL (d_netinfo_cpp, "$Id: d_netinfo.cpp 1788 2010-08-24 04:42:57Z russellrice $")
+
---
+++ server/src/sv_stubs.cpp
@@ -0,0 +1,32 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id: d_netinfo.cpp 1788 2010-08-24 04:42:57Z russellrice $
+//
+// Copyright (C) 1998-2006 by Randy Heit (ZDoom 1.22).
+// Copyright (C) 2000-2006 by Sergey Makovkin (CSDoom .62).
+// Copyright (C) 2006-2010 by The Odamex Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//	 Serverside function stubs
+//
+//-----------------------------------------------------------------------------
+
+void D_SetupUserInfo (void) {}
+void D_UserInfoChanged (cvar_t *cvar) {} 
+void D_DoServerInfoChange (byte **stream) {} 
+void D_WriteUserInfoStrings (int i, byte **stream, bool compact) {} 
+void D_ReadUserInfoStrings (int i, byte **stream, bool update) {}
+
+VERSION_CONTROL (d_netinfo_cpp, "$Id: d_netinfo.cpp 1788 2010-08-24 04:42:57Z russellrice $")
+
