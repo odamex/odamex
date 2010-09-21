745dc60c7358d2475eb79164c22fcfe741ceb169
rice <russell@odamex.net>
2010-09-21 00:35:33 +0000

    - Call Flush(); to ensure window positions/dimensions are saved to disk - Do not mix wxT with _T when building the command line - Fix wxExecute return value according to docs - On msw, wx will generate its own message box if it cannot create a process, so avoid creating our own for now
    SVN r1937 (0.6-dev)

--- odalaunch/src/dlg_main.cpp
+++ odalaunch/src/dlg_main.cpp
@@ -222,6 +222,8 @@ void dlgMain::OnClose(wxCloseEvent &event)
     ConfigInfo.Write(wxT("MainWindowPosX"), GetPosition().x);
     ConfigInfo.Write(wxT("MainWindowPosY"), GetPosition().y);
 
+    ConfigInfo.Flush();
+
     event.Skip();
 }
 
@@ -764,7 +766,7 @@ void dlgMain::LaunchGame(const wxString &Address, const wxString &ODX_Path,
     }
     
     #ifdef __WXMSW__
-      wxString binname = ODX_Path + wxT('\\') + _T("odamex");
+      wxString binname = ODX_Path + wxT('\\') + wxT("odamex");
     #elif __WXMAC__
       wxString binname = ODX_Path + wxT("/odamex.app/Contents/MacOS/odamex");
     #else
@@ -794,13 +796,17 @@ void dlgMain::LaunchGame(const wxString &Address, const wxString &ODX_Path,
     ConfigInfo.Read(wxT(EXTRACMDLINEARGS), &ExtraCmdLineArgs, wxT(""));
     
     if (!ExtraCmdLineArgs.IsEmpty())
-        cmdline += wxString::Format(_T(" %s"), 
+        cmdline += wxString::Format(wxT(" %s"), 
                                     ExtraCmdLineArgs.c_str());
 
-	if (wxExecute(cmdline, wxEXEC_ASYNC, NULL) == -1)
+    // wxWidgets likes to spit out its own message box on msw after our one
+    #ifndef __WXMSW__
+	if (wxExecute(cmdline, wxEXEC_ASYNC, NULL) <= 0)
         wxMessageBox(wxString::Format(wxT("Could not start %s!"), 
                                         binname.c_str()));
-	
+    #else
+    wxExecute(cmdline, wxEXEC_ASYNC, NULL);
+    #endif
 }
 
 
