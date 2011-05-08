85975c6b54da94f4dbdf6c19d297669681697540
Michael J. Wood <mwoodj@huntsvegas.org>
2011-05-08 00:04:57 +0000

    Add doxygen comments for the Xbox class.
    SVN r2170 (0.6-dev)

--- ag-odalaunch/Xbox/xbox_main.h
+++ ag-odalaunch/Xbox/xbox_main.h
@@ -37,38 +37,144 @@
 #define inet_ntoa Xbox::InetNtoa
 #define gethostbyname Xbox::GetHostByName
 
+/**
+agOdalaunch namespace.
+
+All code for the ag-odalaunch launcher is contained within
+the agOdalaunch namespace.
+*/
 namespace agOdalaunch {
 
-// hostent
+/**
+Standard hostent structure.
+
+The hostent structure is typically used with the gethostbyname() function.
+*/
 struct hostent
 {
-	char  *h_name;       /* canonical name of host */
-	char **h_aliases;    /* alias list */
-	int    h_addrtype;   /* host address type */
-	int    h_length;     /* length of address */
-	char **h_addr_list;  /* list of addresses */
-#define h_addr h_addr_list[0]
+	char  *h_name;       /** canonical name of host */
+	char **h_aliases;    /** alias list */
+	int    h_addrtype;   /** host address type */
+	int    h_length;     /** length of address */
+	char **h_addr_list;  /** list of addresses */
+#define h_addr h_addr_list[0] /** The first address in h_addr_list */
 };
 
+/**
+Xbox Class.
+
+The Xbox class provides utilities and function overrides
+that are needed for the Xbox platform.
+*/
 class Xbox
 {
 public:
+	/**
+	Network Byte Order address to ascii string.
+
+	This is a custom implementation of inet_ntoa() for Xbox. inet_ntoa()
+	converts an internet address in network byte order to an ascii string.
+ 
+	@param in Network address.
+	@returns Address ascii string.
+	*/
 	static char *InetNtoa(struct in_addr in);
+
+	/**
+	Get Host by Name
+
+	This is a custom implementation of gethostbyname() for Xbox.
+	gethostbyname() returns a hostent structure for the provided hostname
+	or IP address.
+
+	@param name A hostname or IP address.
+	@returns A filled in hostent structure or NULL on error.
+	*/
 	static struct hostent *GetHostByName(const char *name);
 
-	static int  InitializeJoystick(void);
-	static void EnableJoystickUpdates(bool);
+	/**
+	Initialize the Xbox joystick.
 
+	This function initializes the Xbox joystick which will move the cursor
+	within the launcher as well as activate widgets.
+
+	@returns 0 on success, -1 on error.
+	*/
+	static int  InitializeJoystick(void);
+
+	/**
+	Enable or disable Xbox joystick updates.
+
+	The Xbox joystick can be enabled or disabled by passing true or false
+	to this function.
+
+	@param enable true or false.
+	*/
+	static void EnableJoystickUpdates(bool enable);
+
+	/**
+	Mount Xbox partitions.
+
+	This function mounts the Xbox partitions which makes them available for
+	reading and writing.
+	*/
 	static void MountPartitions();
+
+	/**
+	Unmount Xbox partitions.
+
+	This function unmounts the Xbox partitions.
+	*/
 	static void UnMountPartitions();
 
+	/**
+	Output a string for debugging.
+
+	Provided a string, with optional standard variable arguments, this function
+	will write the contents to a debug file and, if running in the debugger, print
+	the string to the debug console.
+
+	@param str The formatted string.
+	@param ... Standard variable arguments.
+	*/
 	static void OutputDebugString(const char *str, ...);
+
+	/**
+	Initialize the log file.
+
+	This function open the Xbox log file for writing.
+
+	@returns 0 on success, -1 on error.
+	*/
 	static int  InitLogFile();
+
+	/**
+	Close the log file.
+
+	This function closes the Xbox log file.
+	*/
 	static void CloseLogFile();
 
+	/**
+	Initialize the network adapter.
+
+	Initialize the Xbox network adapter and attempt to obtain an IP address.
+	*/
 	static void InitNet();
+
+	/**
+	Close the network adapter.
+
+	Close the Xbox network adapter and release any leased IP address.
+	*/
 	static void CloseNetwork();
 
+	/**
+	Enable the debug console.
+
+	This function enables the debug console which causes debug output to be
+	sent to the debug console.
+	*/
 	static void EnableDebugConsole();
 
 private:
