4100c42c5b985771e98d9468de23675be44059c4
Mike Lightner (Manc) <mike@odamex.net>
2011-09-13 02:54:55 +0000

    Grumble grumble server grumble...
    SVN r2485 (0.6-dev)

--- server/src/g_level.cpp
+++ server/src/g_level.cpp
@@ -1316,7 +1316,7 @@ void G_DoLoadLevel (int position)
 	// DOOM determines the sky texture to be used
 	// depending on the current episode, and the game version.
 	// [RH] Fetch sky parameters from level_locals_t.
-	skytexture = R_TextureNumForName (level.skypic);
+	sky1texture = R_TextureNumForName (level.skypic);
 
 	for (i = 0; i < players.size(); i++)
 	{
