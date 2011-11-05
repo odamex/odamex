56d5046b568e59e14283f73684d9cfa79f19fab6
Mike Lightner (Manc) <mike@odamex.net>
2011-11-05 03:19:50 +0000

    Version bump 0.5.6
    SVN r2576 (0.6-dev)

--- common/version.h
+++ common/version.h
@@ -30,10 +30,10 @@
 #endif
 
 // Lots of different representations for the version number
-#define CONFIGVERSIONSTR "55"
-#define GAMEVER (0*256+55)
+#define CONFIGVERSIONSTR "56"
+#define GAMEVER (0*256+56)
 
-#define DOTVERSIONSTR "0.5.5"
+#define DOTVERSIONSTR "0.5.6"
 
 #define SERVERMAJ (gameversion / 256)
 #define SERVERMIN ((gameversion % 256) / 10)
