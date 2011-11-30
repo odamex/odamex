413e8f8f3e512f680ee9ac362fc7a4c9bb1f8765
Mike Lightner (Manc) <mike@odamex.net>
2011-11-30 05:56:28 +0000

    Update the server with another stub after r2616...
    SVN r2617 (0.6-dev)

--- server/src/p_effect.cpp
+++ server/src/p_effect.cpp
@@ -25,6 +25,7 @@
 
 
 void P_DrawSplash(int count, fixed_t x, fixed_t y, fixed_t z, angle_t angle, int kind) {}
+void P_DrawRailTrail (vec3_t start, vec3_t end) {}
 void P_RunEffects(void) {}
 void P_ThinkParticles(void) {}
 
