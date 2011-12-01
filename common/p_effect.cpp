2d61deea27791dcd43c80809a6c5d5b51b7e1bdb
Mike Lightner (Manc) <mike@odamex.net>
2011-12-01 05:17:37 +0000

    I should stop working after 11pm...
    SVN r2624 (0.6-dev)

---
+++ common/p_effect.cpp
@@ -0,0 +1,315 @@
+// Emacs style mode select   -*- C++ -*-
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright (C) 1998-2006 by Randy Heit (ZDoom).
+// Copyright (C) 2006-2010 by The Odamex Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//	COMMON Particle effect thinkers
+//
+//-----------------------------------------------------------------------------
+
+#include "doomtype.h"
+#include "doomstat.h"
+#include "c_cvars.h"
+#include "actor.h"
+#include "p_effect.h"
+#include "p_local.h"
+#include "g_level.h"
+#include "v_video.h"
+#include "m_random.h"
+#include "r_defs.h"
+#include "r_things.h"
+#include "s_sound.h"
+
+EXTERN_CVAR (cl_rockettrails)
+
+// [RH] particle globals
+int				NumParticles;
+int				ActiveParticles;
+int				InactiveParticles;
+particle_t		*Particles;
+
+#define FADEFROMTTL(a)	(255/(a))
+
+static int grey1, grey2, grey3, grey4, red, green, blue, yellow, black,
+		   red1, green1, blue1, yellow1, purple, purple1, white,
+		   rblue1, rblue2, rblue3, rblue4, orange, yorange, dred, grey5,
+		   maroon1, maroon2;
+
+static const struct ColorList {
+	int *color, r, g, b;
+} Colors[] = {
+	{&grey1,	85,  85,  85 },
+	{&grey2,	171, 171, 171},
+	{&grey3,	50,  50,  50 },
+	{&grey4,	210, 210, 210},
+	{&grey5,	128, 128, 128},
+	{&red,		255, 0,   0  },  
+	{&green,	0,   200, 0  },  
+	{&blue,		0,   0,   255},
+	{&yellow,	255, 255, 0  },  
+	{&black,	0,   0,   0  },  
+	{&red1,		255, 127, 127},
+	{&green1,	127, 255, 127},
+	{&blue1,	127, 127, 255},
+	{&yellow1,	255, 255, 180},
+	{&purple,	120, 0,   160},
+	{&purple1,	200, 30,  255},
+	{&white, 	255, 255, 255},
+	{&rblue1,	81,  81,  255},
+	{&rblue2,	0,   0,   227},
+	{&rblue3,	0,   0,   130},
+	{&rblue4,	0,   0,   80 },
+	{&orange,	255, 120, 0  },
+	{&yorange,	255, 170, 0  },
+	{&dred,		80,  0,   0  },
+	{&maroon1,	154, 49,  49 },
+	{&maroon2,	125, 24,  24 },
+	{NULL}
+};
+
+//
+// [RH] Particle functions
+//
+#ifndef _MSC_VER
+// inlined with VC++
+particle_t *NewParticle (void)
+{
+	particle_t *result = NULL;
+	if (InactiveParticles != -1)
+	{
+		result = Particles + InactiveParticles;
+		InactiveParticles = result->next;
+		result->next = ActiveParticles;
+		ActiveParticles = result - Particles;
+	}
+	return result;
+}
+#endif
+
+static void MakeFountain (AActor *actor, int color1, int color2)
+{
+	particle_t *particle;
+
+	if (!(level.time & 1))
+		return;
+
+	particle = JitterParticle (51);
+
+	if (particle) {
+		angle_t an = M_Random()<<(24-ANGLETOFINESHIFT);
+		fixed_t out = FixedMul (actor->radius, M_Random()<<8);
+
+		particle->x = actor->x + FixedMul (out, finecosine[an]);
+		particle->y = actor->y + FixedMul (out, finesine[an]);
+		particle->z = actor->z + actor->height + FRACUNIT;
+		if (out < actor->radius/8)
+			particle->velz += FRACUNIT*10/3;
+		else
+			particle->velz += FRACUNIT*3;
+		particle->accz -= FRACUNIT/11;
+		if (M_Random() < 30) {
+			particle->size = 4;
+			particle->color = color2;
+		} else {
+			particle->size = 6;
+			particle->color = color1;
+		}
+	}
+}
+
+//
+// AddParticle
+//
+// Creates a particle with "jitter"
+//
+particle_t *JitterParticle (int ttl)
+{
+	particle_t *particle = NewParticle ();
+
+	if (particle) {
+		fixed_t *val = &particle->velx;
+		int i;
+
+		// Set initial velocities
+		for (i = 3; i; i--, val++)
+			*val = (FRACUNIT/4096) * (M_Random () - 128);
+		// Set initial accelerations
+		for (i = 3; i; i--, val++)
+			*val = (FRACUNIT/16384) * (M_Random () - 128);
+
+		particle->trans = 255;	// fully opaque
+		particle->ttl = ttl;
+		particle->fade = FADEFROMTTL(ttl);
+	}
+	return particle;
+}
+
+//
+// P_RunEffects
+//
+// Run effects on all mobjs in world
+//
+void P_RunEffects (void)
+{
+	//int pnum = 0;/* = (consoleplayer().camera->subsector->sector - sectors) * numsectors*/;
+	AActor *actor;
+	TThinkerIterator<AActor> iterator;
+
+	while ( (actor = iterator.Next ()) )
+	{
+		if (actor->effects)
+		{
+			// Only run the effect if the mobj is potentially visible
+			//int rnum = pnum + (actor->subsector->sector - sectors);
+			//if (rejectempty || !(rejectmatrix[rnum>>3] & (1 << (rnum & 7))))
+			{
+				P_RunEffect (actor, actor->effects);
+			}
+		}
+	}
+}
+
+void P_RunEffect (AActor *actor, int effects)
+{
+	angle_t moveangle = R_PointToAngle2(0,0,actor->momx,actor->momy);
+	particle_t *particle;
+	if ((effects & FX_ROCKET) && cl_rockettrails) {
+		// Rocket trail
+
+		fixed_t backx = actor->x - FixedMul (finecosine[(moveangle)>>ANGLETOFINESHIFT], actor->radius*2);
+		fixed_t backy = actor->y - FixedMul (finesine[(moveangle)>>ANGLETOFINESHIFT], actor->radius*2);
+		fixed_t backz = actor->z - (actor->height>>3) * (actor->momz>>16) + (2*actor->height)/3;
+
+		angle_t an = (moveangle + ANG90) >> ANGLETOFINESHIFT;
+		int i, speed;
+
+		particle = JitterParticle (3 + (M_Random() & 31));
+		if (particle) {
+			fixed_t pathdist = M_Random()<<8;
+			particle->x = backx - FixedMul(actor->momx, pathdist);
+			particle->y = backy - FixedMul(actor->momy, pathdist);
+			particle->z = backz - FixedMul(actor->momz, pathdist);
+			speed = (M_Random () - 128) * (FRACUNIT/200);
+			particle->velx += FixedMul (speed, finecosine[an]);
+			particle->vely += FixedMul (speed, finesine[an]);
+			particle->velz -= FRACUNIT/36;
+			particle->accz -= FRACUNIT/20;
+			particle->color = yellow;
+			particle->size = 2;
+		}
+		for (i = 6; i; i--) {
+			particle_t *particle = JitterParticle (3 + (M_Random() & 31));
+			if (particle) {
+				fixed_t pathdist = M_Random()<<8;
+				particle->x = backx - FixedMul(actor->momx, pathdist);
+				particle->y = backy - FixedMul(actor->momy, pathdist);
+				particle->z = backz - FixedMul(actor->momz, pathdist) + (M_Random() << 10);
+				speed = (M_Random () - 128) * (FRACUNIT/200);
+				particle->velx += FixedMul (speed, finecosine[an]);
+				particle->vely += FixedMul (speed, finesine[an]);
+				particle->velz += FRACUNIT/80;
+				particle->accz += FRACUNIT/40;
+				if (M_Random () & 7)
+					particle->color = grey2;
+				else
+					particle->color = grey1;
+				particle->size = 3;
+			} else
+				break;
+		}
+	}
+	if ((effects & FX_GRENADE) && (cl_rockettrails)) {
+		// Grenade trail
+
+		P_DrawSplash2 (6,
+			actor->x - FixedMul (finecosine[(moveangle)>>ANGLETOFINESHIFT], actor->radius*2),
+			actor->y - FixedMul (finesine[(moveangle)>>ANGLETOFINESHIFT], actor->radius*2),
+			actor->z - (actor->height>>3) * (actor->momz>>16) + (2*actor->height)/3,
+			moveangle + ANG180, 2, 2);
+	}
+	if (effects & FX_FOUNTAINMASK) {
+		// Particle fountain
+
+		static const int *fountainColors[16] = 
+			{ &black,	&black,
+			  &red,		&red1,
+			  &green,	&green1,
+			  &blue,	&blue1,
+			  &yellow,	&yellow1,
+			  &purple,	&purple1,
+			  &black,	&grey3,
+			  &grey4,	&white
+			};
+		int color = (effects & FX_FOUNTAINMASK) >> 15;
+		MakeFountain (actor, *fountainColors[color], *fountainColors[color+1]);
+	}
+}
+
+void P_ThinkParticles (void)
+{
+	int i;
+	particle_t *particle, *prev;
+
+	i = ActiveParticles;
+	prev = NULL;
+	while (i != -1) {
+		byte oldtrans;
+
+		particle = Particles + i;
+		i = particle->next;
+		oldtrans = particle->trans;
+		particle->trans -= particle->fade;
+		if (oldtrans < particle->trans || --particle->ttl == 0) {
+			memset (particle, 0, sizeof(particle_t));
+			if (prev)
+				prev->next = i;
+			else
+				ActiveParticles = i;
+			particle->next = InactiveParticles;
+			InactiveParticles = particle - Particles;
+			continue;
+		}
+		particle->x += particle->velx;
+		particle->y += particle->vely;
+		particle->z += particle->velz;
+		particle->velx += particle->accx;
+		particle->vely += particle->accy;
+		particle->velz += particle->accz;
+		prev = particle;
+	}
+}
+
+void P_DisconnectEffect (AActor *actor)
+{
+	int i;
+
+	for (i = 64; i; i--) {
+		particle_t *p = JitterParticle (TICRATE*2);
+
+		if (!p)
+			break;
+
+		p->x = actor->x + ((M_Random()-128)<<9) * (actor->radius>>FRACBITS);
+		p->y = actor->y + ((M_Random()-128)<<9) * (actor->radius>>FRACBITS);
+		p->z = actor->z + (M_Random()<<8) * (actor->height>>FRACBITS);
+		p->accz -= FRACUNIT/4096;
+		p->color = M_Random() < 128 ? maroon1 : maroon2;
+		p->size = 4;
+	}
+}
+
+VERSION_CONTROL (p_effect_cpp, "$Id$")
