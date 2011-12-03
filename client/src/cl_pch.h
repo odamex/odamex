3d7a75e6de9c995296752a8368d0240c4829d1c4
rice <russell@odamex.net>
2011-12-03 22:23:36 +0000

    - Include system headers in client pch file - Add missing c_effect header to common pch - Separate sections with comments
    SVN r2639 (0.6-dev)

--- client/src/cl_pch.h
+++ client/src/cl_pch.h
@@ -1,6 +1,17 @@
 #ifndef __CL_PCH__
 #define __CL_PCH__
 
+// system
+#include "hardware.h"
+#include "i_input.h"
+#include "i_music.h"
+#include "i_sdlvideo.h"
+#include "i_sound.h"
+#include "i_system.h"
+#include "i_video.h"
+#include "mus2midi.h"
+
+// normal
 #include "am_map.h"
 #include "c_bind.h"
 #include "cl_main.h"
@@ -12,6 +23,7 @@
 #include "v_text.h"
 #include "wi_stuff.h"
 
+// common
 #include "c_pch.h"
 
 #endif // __CL_PCH__
--- server/src/sv_pch.h
+++ server/src/sv_pch.h
@@ -1,12 +1,16 @@
 #ifndef __SV_PCH__
 #define __SV_PCH__
 
+// system
 #include "i_system.h"
+
+// normal
 #include "sv_main.h"
 #include "sv_master.h"
 #include "sv_sqp.h"
 #include "sv_sqpold.h"
 
+// common
 #include "c_pch.h"
 
 #endif // __SV_PCH__
