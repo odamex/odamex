3c00bbfb602a91a70c64295d3001e929659834ee
Alex Mayfield <alexmax2742@gmail.com>
2012-08-25 23:24:41 +0000

    PortMIDI + Win32 fixes.
    - Fix up PortMIDI CMake module.
    - Add an all-in-one zip file with needed includes and libraries.
    
    SVN r3337 (trunk)

--- cmake/modules/FindPortMidi.cmake
+++ cmake/modules/FindPortMidi.cmake
@@ -9,20 +9,31 @@
 # See documentation on how to write CMake scripts at
 # http://www.cmake.org/Wiki/CMake:How_To_Find_Libraries
 
-include(LibFindMacros)
+find_path(PORTMIDI_INCLUDE_DIR portmidi.h
+  HINTS
+  $ENV{PORTMIDI_DIR}
+)
 
-find_path(PortMidi_INCLUDE_DIR NAMES portmidi.h)
-find_library(PortMidi_LIBRARY NAMES portmidi)
-find_library(PortTime_LIBRARY NAMES porttime)
+find_library(PORTMIDI_LIBRARY portmidi
+  HINTS
+  $ENV{PORTMIDI_DIR}
+)
 
-set(PortMidi_PROCESS_INCLUDES ${PortMidi_INCLUDE_DIR})
-set(PortMidi_PROCESS_LIBS ${PortMidi_LIBRARY})
+find_library(PORTRIME_LIBRARY porttime
+  HINTS
+  $ENV{PORTMIDI_DIR}
+)
+
 # Porttime library is merged to Portmidi in new versions, so
 # we work around problems by adding it only if it's present
-if (${PortTime_LIBRARY})
-	set(PortMidi_PROCESS_LIBS ${PortMidi_PROCESS_LIBS} ${PortTime_LIBRARY})
-endif (${PortTime_LIBRARY})
+if(${PORTRIME_LIBRARY} MATCHES "PORTRIME_LIBRARY-NOTFOUND")
+  set(PORTMIDI_LIBRARIES ${PORTMIDI_LIBRARY})
+else()
+  set(PORTMIDI_LIBRARIES ${PORTMIDI_LIBRARY} ${PORTRIME_LIBRARY})
+endif()
 
-libfind_process(PortMidi)
-mark_as_advanced(PortTime_LIBRARY)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(PortMidi
+  REQUIRED_VARS PORTMIDI_INCLUDE_DIR PORTMIDI_LIBRARIES)
 
+mark_as_advanced(PORTMIDI_LIBRARY PORTRIME_LIBRARY)
