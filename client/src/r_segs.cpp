0b5c63857a72638fe6e59eaef052181627ad70c3
Sean Leonard <seanleonard1@gmail.com>
2012-12-05 17:15:20 +0000

    -Avoid calling R_ColumnToPointOnSeg when rendering walls unless it is necessary due to sloping floors or ceilings. -This should be a considerable CPU savings for segs not touching a sloping plane.
    SVN r3480 (trunk)

--- client/src/r_segs.cpp
+++ client/src/r_segs.cpp
@@ -670,7 +670,7 @@ void R_StoreWallRange(int start, int stop)
 	fixed_t hyp;
 	fixed_t sineval;
 	angle_t distangle, offsetangle;
-	
+
 #ifdef RANGECHECK
 	if (start >= viewwidth || start > stop)
 		I_FatalError ("Bad R_StoreWallRange: %i to %i", start , stop);
@@ -738,13 +738,28 @@ void R_StoreWallRange(int start, int stop)
 	// Calculate the front sector's floor and ceiling height at the two
 	// endpoints of the drawseg
 	fixed_t px1, py1, px2, py2;
-	R_ColumnToPointOnSeg(start, linedef, px1, py1);
-	R_ColumnToPointOnSeg(stop, linedef, px2, py2);
-	
-	rw_frontcz1 = P_CeilingHeight(px1, py1, frontsector);
-	rw_frontcz2 = P_CeilingHeight(px2, py2, frontsector);
-	rw_frontfz1 = P_FloorHeight(px1, py1, frontsector);
-	rw_frontfz2 = P_FloorHeight(px2, py2, frontsector);
+
+	// [SL] Determine if we can use less expensive calculations for non-sloping lines
+	bool flatplanes =	P_IsPlaneLevel(&linedef->frontsector->ceilingplane) &&
+						P_IsPlaneLevel(&linedef->frontsector->floorplane) &&
+						(!linedef->backsector || 
+						(P_IsPlaneLevel(&linedef->backsector->ceilingplane) &&
+		 				P_IsPlaneLevel(&linedef->backsector->floorplane)));
+	if (flatplanes)
+	{
+		rw_frontcz1 = rw_frontcz2 = P_CeilingHeight(linedef->v1->x, linedef->v1->y, frontsector);
+		rw_frontfz1 = rw_frontfz2 = P_FloorHeight(linedef->v1->x, linedef->v1->y, frontsector);
+	}
+	else
+	{
+		R_ColumnToPointOnSeg(start, linedef, px1, py1);
+		R_ColumnToPointOnSeg(stop, linedef, px2, py2);
+
+		rw_frontcz1 = P_CeilingHeight(px1, py1, frontsector);
+		rw_frontcz2 = P_CeilingHeight(px2, py2, frontsector);
+		rw_frontfz1 = P_FloorHeight(px1, py1, frontsector);
+		rw_frontfz2 = P_FloorHeight(px2, py2, frontsector);
+	}
 	
 	midtexture = toptexture = bottomtexture = maskedtexture = 0;
 	ds_p->maskedtexturecol = NULL;
@@ -784,10 +799,19 @@ void R_StoreWallRange(int start, int stop)
 
 		// Calculate the back sector's floor and ceiling height at the two
 		// endpoints of the drawseg
-		rw_backcz1 = P_CeilingHeight(px1, py1, backsector);
-		rw_backcz2 = P_CeilingHeight(px2, py2, backsector);
-		rw_backfz1 = P_FloorHeight(px1, py1, backsector);
-		rw_backfz2 = P_FloorHeight(px2, py2, backsector);
+
+		if (flatplanes)
+		{
+			rw_backcz1 = rw_backcz2 = P_CeilingHeight(linedef->v1->x, linedef->v1->y, backsector);
+			rw_backfz1 = rw_backfz2 = P_FloorHeight(linedef->v1->x, linedef->v1->y, backsector);
+		}
+		else
+		{
+			rw_backcz1 = P_CeilingHeight(px1, py1, backsector);
+			rw_backcz2 = P_CeilingHeight(px2, py2, backsector);
+			rw_backfz1 = P_FloorHeight(px1, py1, backsector);
+			rw_backfz2 = P_FloorHeight(px2, py2, backsector);
+		}
 
 		extern bool doorclosed;	
 		if (doorclosed)
