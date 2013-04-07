8af053015750d3af194e9a691bc86c0bc693ba33
Alex Mayfield <alexmax2742@gmail.com>
2013-04-07 19:40:26 +0000

    NetDemo errors now only clear the gamestate and gameaction when in the middle of playing back a demo.
    Fixes an issue where automatically attempting to record a netdemo to a non-existent directory gets Odamex stuck when connecting to a server, but this fix also targets a number of other undesirable behaviors as well.
    
    SVN r3777 (trunk)

--- client/src/cl_demo.cpp
+++ client/src/cl_demo.cpp
@@ -131,9 +131,28 @@ void NetDemo::cleanUp()
 	state = oldstate = NetDemo::st_stopped;
 }
 
-
-
+/**
+ * Error handler.
+ *
+ * Generic error handler for netdemo issues.
+ * 
+ * @param message Error message.
+ */
 void NetDemo::error(const std::string &message)
+{
+	cleanUp();
+	Printf(PRINT_HIGH, "%s\n", message.c_str());
+}
+
+/**
+ * Fatal error hendler.
+ *
+ * Error handler for netdemo issues that should blank out the view of
+ * the game.  Generally used for issues that come up during playback.
+ * 
+ * @param message Error message.
+ */
+void NetDemo::fatalError(const std::string &message)
 {
 	cleanUp();
 	gameaction = ga_nothing;
@@ -141,7 +160,6 @@ void NetDemo::error(const std::string &message)
 
 	Printf(PRINT_HIGH, "%s\n", message.c_str());
 }
-
 
 //
 // writeHeader()
@@ -851,7 +869,7 @@ void NetDemo::readMessageBody(buf_t *netbuffer, uint32_t len)
 	if (cnt < len)
 	{
 		delete[] msgdata;
-		error("Can not read netdemo message.");
+		fatalError("Can not read netdemo message.");
 		return;
 	}
 
@@ -1351,14 +1369,14 @@ void NetDemo::readSnapshot(const netdemo_index_entry_t *snap)
 
 	if (len > NetDemo::MAX_SNAPSHOT_SIZE)
 	{
-		error("Snapshot too large to read");
+		fatalError("Snapshot too large to read");
 		return;
 	}
 		
 	size_t cnt = fread(snapbuf, 1, len, demofp);
 	if (cnt < len)
 	{
-		error("Unable to read snapshot from data file");
+		fatalError("Unable to read snapshot from data file");
 		return;
 	}
 
@@ -1608,7 +1626,7 @@ void NetDemo::readSnapshotData(byte *buf, size_t length)
 	arc.Close();
 
 	if (check != 0x1d)
-		error("Bad snapshot");
+		fatalError("Bad snapshot");
 	
 	consoleplayer_id = cid;
 	
