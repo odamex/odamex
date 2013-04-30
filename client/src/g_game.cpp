odamex/odamex

commit 16270925e2bccca0045360583d3da04b53722837

-Fix some additional issues with cvar latching (bug 963). -Latching now works correctly for single player and client/server, as well as playing LMP demos.
git-svn-id: http://odamex.net/svn/root/trunk@3788 b0bcef46-2027-0410-bd56-ab726e9a1d04

common/c_cvars.cpp
@@ -140,8 +140,11 @@ cvar_t::~cvar_t ()
 
 void cvar_t::ForceSet (const char *val)
 {
-	if (m_Flags & CVAR_LATCH && 
-	    !(m_Flags & CVAR_SERVERINFO && baseapp == client && multiplayer))
+	// [SL] 2013-04-16 - Latched CVARs do not change values until the next map.
+	// Servers and single-player games should abide by this behavior but
+	// multiplayer clients should just do what the server tells them.
+	if (m_Flags & CVAR_LATCH && serverside && 
+		(gamestate == GS_LEVEL || gamestate == GS_INTERMISSION))
 	{
 		m_Flags |= CVAR_MODIFIED;
 		if(val)
@@ -471,8 +474,7 @@ void cvar_t::UnlatchCVars (void)
 	var = ad.GetCVars();
 	while (var)
 	{
-		if (var->m_Flags & (CVAR_MODIFIED | CVAR_LATCH) &&
-		    !(var->m_Flags & CVAR_SERVERINFO && baseapp == client && multiplayer))
+		if (var->m_Flags & (CVAR_MODIFIED | CVAR_LATCH))
 		{
 			unsigned oldflags = var->m_Flags & ~CVAR_MODIFIED;
 			var->m_Flags &= ~(CVAR_LATCH);

client/src/g_level.cpp
@@ -473,6 +473,8 @@ void G_DoLoadLevel (int position)
 	else
 		lastposition = position;
 
+	cvar_t::UnlatchCVars();
+
 	G_InitLevelLocals ();
 
     Printf_Bold ("\n\35\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36\36"

commit 062621a2cdd05a03f08c1f8da2c59b6497edc0d5

-(From 0.6.x branch) Fix a cvar unlatching bug introduced in r3788 that occurs during the demo loop, where after the first demo of the loop has been played, cvars would not be unlatched for the next demo (bug 973).
SVN r3808 (trunk)

client/src/g_game.cpp
@@ -2320,14 +2320,14 @@ BOOL G_CheckDemoStatus (void)
 		if (timingdemo)
 			endtime = I_GetTimePolled () - starttime;
 
-		cvar_t::C_RestoreCVars ();		// [RH] Restore cvars demo might have changed
-
 		Z_Free (demobuffer);
 
 		demoplayback = false;
 		netgame = false;
 		multiplayer = false;
 		serverside = false;
+
+		cvar_t::C_RestoreCVars ();		// [RH] Restore cvars demo might have changed
 
 		if (demotest) {
 			AActor *mo = idplayer(1).mo;

common/c_cvars.cpp
@@ -446,6 +446,7 @@ void cvar_t::C_RestoreCVars (void)
 		backup->name = backup->string = "";
 	}
 	numbackedup = 0;
+	UnlatchCVars();
 }
 
 cvar_t *cvar_t::FindCVar (const char *var_name, cvar_t **prev)