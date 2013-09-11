86463f5f6b79cffa07e51f37631f6a74c579cc84
Sean Leonard <seanleonard1@gmail.com>
2013-09-11 16:54:21 +0000

    -Add a new read-only server CVAR sv_padding, which consists of a string of 200 spaces. -The hope is that the padding forces the server's launcher response packets to be split by the IP layer instead of being transmitted as a single packet that is larger than the MTU of certain routers, causing the packet to be dropped (bug 951). This is intended as a temporary work-around to the issue of launchers failing to properly query some servers due to oversized response packets being dropped before reaching their destination.
    SVN r4148 (trunk)

--- server/src/sv_cvarlist.cpp
+++ server/src/sv_cvarlist.cpp
@@ -241,5 +241,14 @@ CVAR (sv_countdown, "5", "Number of seconds to wait before starting the game fro
 CVAR (sv_emptyfreeze,  "0", "Experimental: Does not progress the game when there are no players",
       CVARTYPE_BOOL, CVAR_ARCHIVE)
 
+// Hacky abominations that should be purged with fire and brimstone
+// =================================================================
+
+// [SL] sv_padding is used to pad launcher packets to ensure that they are split by the
+// IP layer rather than being oversized and dropped by some routers
+CVAR (sv_padding, "                                                                                                                                                                                                        ", "",
+		CVARTYPE_STRING, CVAR_SERVERINFO | CVAR_NOENABLEDISABLE | CVAR_NOSET)
+
+
 VERSION_CONTROL (sv_cvarlist_cpp, "$Id$")
 
