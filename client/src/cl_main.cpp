d1a0def570f905271972fe26a90e604703e37fe9
Mike Lightner (Manc) <mike@odamex.net>
2014-06-18 19:09:17 +0000

    Ok don't revert all of it, just the sv_keepkeys part.  Kill will work in coop games.
    SVN r5027 (trunk)

--- client/src/cl_main.cpp
+++ client/src/cl_main.cpp
@@ -854,7 +854,7 @@ END_COMMAND (playerinfo)
 
 BEGIN_COMMAND (kill)
 {
-    if (sv_allowcheats)
+    if (sv_allowcheats || sv_gametype == GM_COOP)
         MSG_WriteMarker(&net_buffer, clc_kill);
     else
         Printf (PRINT_HIGH, "You must run the server with '+set sv_allowcheats 1' or disable sv_keepkeys to enable this command.\n");
--- server/src/sv_main.cpp
+++ server/src/sv_main.cpp
@@ -4209,7 +4209,7 @@ void SV_ParseCommands(player_t &player)
 		case clc_kill:
 			if(player.mo &&
                level.time > player.death_time + TICRATE*10 &&
-               sv_allowcheats)
+               (sv_allowcheats || sv_gametype == GM_COOP))
             {
 				SV_Suicide (player);
             }
