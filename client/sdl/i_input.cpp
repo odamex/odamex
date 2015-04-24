5fcf6fd7a796ca2c513a9959b19f2bd7ba055767
Sean Leonard <seanleonard1@gmail.com>
2015-04-24 05:01:36 +0000

    -Use the shiftxform table from ChocolateDoom to handle remapping input when the shift key is active.
    SVN r5315 (trunk)

--- client/sdl/i_input.cpp
+++ client/sdl/i_input.cpp
@@ -196,13 +196,57 @@ char I_GetSDL12Key(const SDL_keysym* keysym)
 		keynames[SDLK_KP_ENTER] = '\r';
 		keynames[SDLK_KP_EQUALS] = '=';
 
+
+
 		initialized = true;
 	}
 
+	// [SL] From Chocolate Doom: i_video.c
+	// Lookup table for mapping ASCII characters to their equivalent when
+	// shift is pressed on an American layout keyboard:
+	static const char shiftxform[] =
+	{
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+		11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+		21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+		31, ' ', '!', '"', '#', '$', '%', '&',
+		'"', // shift-'
+		'(', ')', '*', '+',
+		'<', // shift-,
+		'_', // shift--
+		'>', // shift-.
+		'?', // shift-/
+		')', // shift-0
+		'!', // shift-1
+		'@', // shift-2
+		'#', // shift-3
+		'$', // shift-4
+		'%', // shift-5
+		'^', // shift-6
+		'&', // shift-7
+		'*', // shift-8
+		'(', // shift-9
+		':',
+		':', // shift-;
+		'<',
+		'+', // shift-=
+		'>', '?', '@',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'[', // shift-[
+		'!', // shift-backslash - OH MY GOD DOES WATCOM SUCK
+		']', // shift-]
+		'"', '_',
+		'\'', // shift-`
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		'{', '|', '}', '~', 127
+	};
+
 	if (keysym->sym >= SDLK_FIRST && keysym->sym <= SDLK_LAST)
 	{
-		char output = keynames[keysym->sym + SDLK_FIRST];
-		if (output >= 'a' && output <= 'z')
+		int output = keynames[keysym->sym + SDLK_FIRST];
+		if (output >= 0 && output < 127)
 		{
 			// determine if the output should be a capital letter
 			int shift_state = 0;
@@ -213,7 +257,7 @@ char I_GetSDL12Key(const SDL_keysym* keysym)
 			if (keysym->mod & KMOD_CAPS)
 				shift_state ^= 1;
 			if (shift_state)
-				output += 'A' - 'a';
+				output = shiftxform[output];
 		}
 		return output;
 	}
