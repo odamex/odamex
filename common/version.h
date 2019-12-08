b6c5304f223c492d15d8f15d51d6394429ed2c53
Ch0wW <Ch0wW@baseq.fr>
2019-12-08 17:30:50 +0100

    [BETA] Update version.h for BETA 0.8.2

--- common/version.h
+++ common/version.h
@@ -25,10 +25,10 @@
 #define __VERSION_H__
 
 // Lots of different representations for the version number
-#define CONFIGVERSIONSTR "81"
-#define GAMEVER (0*256+81)
+#define CONFIGVERSIONSTR "82"
+#define GAMEVER (0*256+82)
 
-#define DOTVERSIONSTR "0.8.1"
+#define DOTVERSIONSTR "0.8.X BETA"
 
 #define COPYRIGHTSTR "Copyright (C) 2006-2019 The Odamex Team"
 
@@ -42,7 +42,7 @@
 // SAVESIG is the save game signature. It should be the minimum version
 // whose savegames this version is compatible with, which could be
 // earlier than this version.
-#define SAVESIG "ODAMEXSAVE081   "	// Needs to be exactly 16 chars long
+#define SAVESIG "ODAMEXSAVE082   "	// Needs to be exactly 16 chars long
 
 #define NETDEMOVER 3
 
