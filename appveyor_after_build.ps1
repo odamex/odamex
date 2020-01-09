7102885d91fde77cce879787ac9fdbffec496782
James Dunne <james.jdunne@gmail.com>
2020-01-09 13:52:06 -0600

    appveyor: include new path to odamex.wad for inclusion in zip artifacts.

--- appveyor_after_build.ps1
+++ appveyor_after_build.ps1
@@ -1,6 +1,6 @@
 cd build
-7z a odamex.zip ".\client\RelWithDebInfo\odamex.exe" ".\client\RelWithDebInfo\odamex.pdb" "..\odamex.wad" ".\SDL2-2.0.5\lib\x86\*.dll" ".\SDL2_mixer-2.0.1\lib\x86\*.dll"
+7z a odamex.zip ".\client\RelWithDebInfo\odamex.exe" ".\client\RelWithDebInfo\odamex.pdb" "..\wad\odamex.wad" ".\SDL2-2.0.5\lib\x86\*.dll" ".\SDL2_mixer-2.0.1\lib\x86\*.dll"
 mv odamex.zip ..
-7z a odasrv.zip ".\server\RelWithDebInfo\odasrv.exe" ".\server\RelWithDebInfo\odasrv.pdb" "..\odamex.wad"
+7z a odasrv.zip ".\server\RelWithDebInfo\odasrv.exe" ".\server\RelWithDebInfo\odasrv.pdb" "..\wad\odamex.wad"
 mv odasrv.zip ..
 cd ..
