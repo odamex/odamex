f4bf78c6e39acbbad2f76117f43a4882b0a7d854
Alex Mayfield <alexmax2742@gmail.com>
2020-04-04 21:46:01 -0400

    Add MBF frames, sprites and codepointers
    Not all of the codepointers are implemented...I need to do a little
    research on how to translate them correctly.

--- common/d_dehacked.cpp
+++ common/d_dehacked.cpp
@@ -348,9 +348,16 @@ void A_SpawnSound(AActor*);
 void A_SpawnFly(AActor*);
 void A_BrainExplode(AActor*);
 void A_MonsterRail(AActor*);
-void A_Die(AActor*);
 void A_Detonate(AActor*);
 void A_Mushroom(AActor*);
+void A_Die(AActor*);
+void A_Spawn(AActor*);
+void A_Turn(AActor*);
+void A_Face(AActor*);
+void A_Scratch(AActor*);
+void A_PlaySound(AActor*);
+void A_RandomJump(AActor*);
+void A_LineEffect(AActor*);
 
 struct CodePtr {
 	const char *name;
@@ -438,9 +445,16 @@ static const struct CodePtr CodePtrs[] = {
 	{ "SpawnSound",		A_SpawnSound },
 	{ "SpawnFly",		A_SpawnFly },
 	{ "BrainExplode",	A_BrainExplode },
-	{ "Die",	        A_Die },
-	{ "Detonate",	    A_Detonate },
-    { "Mushroom",	    A_Mushroom },
+	{ "Detonate",		A_Detonate },       // killough 8/9/98
+	{ "Mushroom",		A_Mushroom },       // killough 10/98
+	{ "Die",		A_Die },            // killough 11/98
+	{ "Spawn",		A_Spawn },          // killough 11/98
+	{ "Turn",		A_Turn },           // killough 11/98
+	{ "Face",		A_Face },           // killough 11/98
+	{ "Scratch",		A_Scratch },        // killough 11/98
+	{ "PlaySound",		A_PlaySound },      // killough 11/98
+	{ "RandomJump",		A_RandomJump },     // killough 11/98
+	{ "LineEffect",		A_LineEffect },     // killough 11/98
 	{ NULL, NULL }
 };
 
