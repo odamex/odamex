bb12db6cc1dfed2bef6ef383d243903aa935b121
Sean Leonard <seanleonard1@gmail.com>
2020-04-09 09:23:03 -0700

    Upgrade SDL 2.0 libraries for App Veyor builds

--- appveyor_before_build.ps1
+++ appveyor_before_build.ps1
@@ -1,19 +1,19 @@
 mkdir build | Out-Null
 cd build
 
-if (!(Test-Path -Path "SDL2-devel-2.0.5-VC.zip" -PathType leaf)) {
-    Start-FileDownload "https://www.libsdl.org/release/SDL2-devel-2.0.5-VC.zip"
+if (!(Test-Path -Path "SDL2-devel-2.0.12-VC.zip" -PathType leaf)) {
+    Start-FileDownload "https://www.libsdl.org/release/SDL2-devel-2.0.12-VC.zip"
 }
-if (!(Test-Path -Path "SDL2-2.0.5")) {
-    7z x "SDL2-devel-2.0.5-VC.zip"
+if (!(Test-Path -Path "SDL2-2.0.12")) {
+    7z x "SDL2-devel-2.0.12-VC.zip"
 }
 
 if (!(Test-Path -Path "SDL2_mixer-devel-2.0.1-VC.zip" -PathType leaf)) {
     Start-FileDownload "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-devel-2.0.1-VC.zip"
 }
-if (!(Test-Path -Path "SDL2_mixer-2.0.1")) {
-    7z x "SDL2_mixer-devel-2.0.1-VC.zip"
+if (!(Test-Path -Path "SDL2_mixer-2.0.4")) {
+    7z x "SDL2_mixer-devel-2.0.4-VC.zip"
 }
 
-& "C:\Program Files (x86)\cmake\bin\cmake.exe" .. -G "Visual Studio 14 2015" -T v140_xp -DUSE_MINIUPNP=False -DSDL2_INCLUDE_DIR="./SDL2-2.0.5/include/" -DSDL2_LIBRARY="./SDL2-2.0.5/lib/x86/SDL2.lib" -DSDL2MAIN_LIBRARY="./lib/x86/SDL2main.lib" -DSDL2_MIXER_INCLUDE_DIR="./SDL2_mixer-2.0.1/include/" -DSDL2_MIXER_LIBRARY="./SDL2_mixer-2.0.1/lib/x86/SDL2_mixer.lib"
+& "C:\Program Files (x86)\cmake\bin\cmake.exe" .. -G "Visual Studio 14 2015" -T v140_xp -DUSE_MINIUPNP=False -DSDL2_INCLUDE_DIR="./SDL2-2.0.12/include/" -DSDL2_LIBRARY="./SDL2-2.0.12/lib/x86/SDL2.lib" -DSDL2MAIN_LIBRARY="./lib/x86/SDL2main.lib" -DSDL2_MIXER_INCLUDE_DIR="./SDL2_mixer-2.0.4/include/" -DSDL2_MIXER_LIBRARY="./SDL2_mixer-2.0.4/lib/x86/SDL2_mixer.lib"
 cd ..
