56b21e4a0d40114294aa58fc87f64961a3acffc6
Alex Mayfield <alexmax2742@gmail.com>
2020-04-23 20:46:52 -0400

    Assign correct E2-E4 strings

--- common/g_level.cpp
+++ common/g_level.cpp
@@ -1500,7 +1500,7 @@ void G_SetLevelStrings (void)
 		if (info.cluster <= 4)
 		{
 			// Doom 1
-			int offset = (info.cluster - 1) * 9 + (info.levelnum - 1);
+			int offset = (info.cluster - 1) + (info.levelnum - 1);
 			level_name = HUSTR_E1M1 + offset;
 			muslump = MUSIC_E1M1 + offset;
 		}
