59dcaddf2cdfdfa72359b993aa29eaae8fa99e6b
Alex Mayfield <alexmax2742@gmail.com>
2020-05-02 17:35:15 -0400

    Get rid of travis/appveyor, add more to mac/linux

--- ci-build.sh
+++ ci-build.sh
@@ -10,11 +10,12 @@ set -x
 if [[ $(uname -s) == "Linux" ]]; then
     # Ubuntu
     sudo apt update
-    sudo apt install ninja-build libsdl2-dev libsdl2-mixer-dev
+    sudo apt install ninja-build libsdl2-dev libsdl2-mixer-dev \
+        libwxgtk3.0-gtk3-dev deutex
 else
     # macOS
     brew update
-    brew install ninja sdl2 sdl2_mixer
+    brew install ninja sdl2 sdl2_mixer wxmac
 fi
 
 mkdir -p build && cd build
