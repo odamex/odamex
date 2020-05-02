ab15c879d821ccaf89980ac485339922fea5cc95
Alex Mayfield <alexmax2742@gmail.com>
2020-05-02 16:31:31 -0400

    Testing shell environment

--- ci-build.sh
+++ ci-build.sh
@@ -5,6 +5,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+set -x
+
+echo "which gcc = $(which gcc)"
+echo "which clang = $(which clang)"
+echo "which ninja = $(which ninja)"
+
 mkdir -p build && cd build
 cmake .. -GNinja \
     -DCMAKE_BUILD_TYPE=Debug -DUSE_COLOR_DIAGNOSTICS=1 \
