fd9063bb8d2bd375843a8e6de71c8f4d5ce9c8bc
Alex Mayfield <alexmax2742@gmail.com>
2020-05-19 18:32:37 -0400

    First attempt at getting SDL1.2 working again

--- ci-build.sh
+++ ci-build.sh
@@ -10,8 +10,13 @@ set -x
 if [[ $(uname -s) == "Linux" ]]; then
     # Ubuntu
     sudo apt update
-    sudo apt install ninja-build libsdl2-dev libsdl2-mixer-dev \
-        libwxgtk3.0-gtk3-dev deutex
+    if [[ -z ${USE_SDL12:-} ]]; then
+        sudo apt install ninja-build libsdl2-dev libsdl2-mixer-dev \
+            libwxgtk3.0-gtk3-dev deutex
+    else
+        sudo apt install ninja-build libsdl1.2-dev libsdl-mixer1.2-dev \
+            libwxgtk3.0-gtk3-dev deutex
+    fi
 else
     # macOS
     brew update
@@ -19,5 +24,10 @@ else
 fi
 
 mkdir -p build && cd build
-cmake .. -GNinja \
-    -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_COLOR_DIAGNOSTICS=1
+if [[ -z ${USE_SDL12:-} ]]; then
+    cmake .. -GNinja \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_COLOR_DIAGNOSTICS=1
+else
+    cmake .. -GNinja \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_COLOR_DIAGNOSTICS=1 -DUSE_SDL12
+fi
