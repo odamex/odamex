e0f64afb5cb39bc8f5a0f5d1c61fb9cbc9021d94
Alex Mayfield <alexmax2742@gmail.com>
2020-05-21 19:41:20 -0400

    Fix Chocolatey MinGW

--- cmake/modules/OdamexTargetSettings.cmake
+++ cmake/modules/OdamexTargetSettings.cmake
@@ -4,6 +4,16 @@ function(odamex_target_settings TARGET)
   if(MINGW)
     # MinGW builds require a bunch of extra libraries.
     get_filename_component(MINGW_DLL_DIR "${CMAKE_CXX_COMPILER}" DIRECTORY)
+
+    # Chocolatey tries to be cute and wraps all the binaries so they don't need
+    # their DLL files, so we special-case that here.
+    if(NOT EXISTS "${MINGW_DLL_DIR}/libgcc_s_seh-1.dll")
+      get_filename_component(MINGW_DLL_DIR
+        "${MINGW_DLL_DIR}/../lib/mingw/tools/install/mingw64/bin"
+        REALPATH BASE_DIR "${CMAKE_BINARY_DIR}"
+      )
+    endif()
+
     list(APPEND ODAMEX_DLLS
       "${MINGW_DLL_DIR}/libgcc_s_seh-1.dll"
       "${MINGW_DLL_DIR}/libstdc++-6.dll"
