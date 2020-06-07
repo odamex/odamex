af900692f41e0bd1b456d15c34a98e88b94ffe4f
Alex Mayfield <alexmax2742@gmail.com>
2020-06-06 20:27:09 -0400

    Stamping out some inittial bits for the WDL logger

---
+++ common/p_wdlstats.cpp
@@ -0,0 +1,57 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright (C) 1993-1996 by id Software, Inc.
+// Copyright (C) 2006-2020 by The Odamex Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//     Defines needed by the WDL stats logger.
+//
+//-----------------------------------------------------------------------------
+
+#include "p_wdlstats.h"
+
+#include <string>
+
+#include "c_dispatch.h"
+
+std::string wdlstatdir;
+
+static void WDLStatsHelp()
+{
+	Printf(PRINT_HIGH,
+		"wdlstats - Starts logging WDL statistics to the given directory.  Unless "
+		"you are running a WDL server, you probably are not interested in this.\n\n"
+		"Usage:\n"
+		"  ] wdlstats <DIRNAME>\n"
+		"  Starts logging WDL statistics in the directory DIRNAME.\n");
+}
+
+BEGIN_COMMAND(wdlstats)
+{
+	if (argc < 2)
+	{
+		WDLStatsHelp();
+		return;
+	}
+}
+END_COMMAND(wdlstats)
+
+void P_LogWDLEvent(WDLEvents event, int arg0, int arg1, int arg2)
+{
+	// Do we have a stat directory set?
+	if (wdlstatdir.empty())
+		return;
+}
---
+++ common/p_wdlstats.h
@@ -0,0 +1,48 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright (C) 1993-1996 by id Software, Inc.
+// Copyright (C) 2006-2020 by The Odamex Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//     Defines needed by the WDL stats logger.
+//
+//-----------------------------------------------------------------------------
+
+#ifndef __WDLSTATS_H__
+#define __WDLSTATS_H__
+
+enum WDLEvents {
+	WDL_DAMAGE,
+	WDL_CARRIERDAMAGE,
+	WDL_KILL,
+	WDL_CARRIERKILL,
+	WDL_ENVIRODAMAGEUNUSED,
+	WDL_ENVIRODAMAGE,
+	WDL_ENVIROKILL,
+	WDL_ENVIROCARRIERKILL,
+	WDL_TOUCH,
+	WDL_PICKUPTOUCH,
+	WDL_CAPTURE,
+	WDL_PICKUPCAPTURE,
+	WDL_ASSIST,
+	WDL_RETURNFLAG,
+	WDL_ITEMPICKUP,
+	WDL_ACCURACY,
+};
+
+void P_LogWDLEvent(WDLEvents event, int arg0, int arg1, int arg2);
+
+#endif
