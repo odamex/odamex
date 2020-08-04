62f2c88bb71dc1b1a406880ddcdd34d5c8a3da41
Alex Mayfield <alexmax2742@gmail.com>
2020-08-03 22:47:52 -0400

    Make position adjustable
    In theory - you can't move it around unless you modify the vars in the
    source.

--- client/src/hu_sitrep.cpp
+++ client/src/hu_sitrep.cpp
@@ -27,37 +27,100 @@
 #include "cmdlib.h"
 #include "d_player.h"
 #include "hu_drawers.h"
+#include "i_video.h"
 #include "v_text.h"
 #include "v_video.h"
+
+float hud_sitrep_scale = 0.75f;
+int hud_sitrep_x = 2;
+int hud_sitrep_y = 0;
+int hud_sitrep_origin = 9;
 
 namespace hud
 {
 
+static void ToLeftTop(int& x, int& y, const unsigned short w, const unsigned short h,
+                      const float scale, const x_align_t x_align, const y_align_t y_align)
+{
+	// Get our clean scale factors.
+	int x_scale = MAX(1, int(scale * ::CleanXfac));
+	int y_scale = MAX(1, int(scale * ::CleanYfac));
+
+	int scaled_width = I_GetSurfaceWidth() / x_scale;
+	int scaled_height = I_GetSurfaceHeight() / y_scale;
+
+	switch (x_align)
+	{
+	case X_LEFT:
+		x = x;
+		break;
+	case X_CENTER:
+		x = (scaled_width / 2) - (w / 2) + x;
+		break;
+	case X_RIGHT:
+		x = scaled_width - x - w;
+		break;
+	default:
+		break;
+	}
+
+	switch (y_align)
+	{
+	case Y_TOP:
+		y = y;
+		break;
+	case Y_MIDDLE:
+		y = (scaled_height / 2) - (h / 2) + y;
+		break;
+	case Y_BOTTOM:
+		y = scaled_height - y - h;
+		break;
+	default:
+		break;
+	}
+}
+
 void SitRep()
 {
-	int y = 0;
-	float scale = 1.0f;
-	x_align_t x_align = X_LEFT;
-	y_align_t y_align = Y_TOP;
-	x_align_t x_origin = X_LEFT;
-	y_align_t y_origin = Y_TOP;
+	const int ROW_HEIGHT = 8;
+	const int BORDER_WIDTH = 2;
+
+	int x = hud_sitrep_x;
+	int y = hud_sitrep_y;
+	int w = 0;
+	int h = 0;
 
 	// Find the widest name.
 	int maxwidth = 0;
 	for (Players::iterator it = players.begin(); it != players.end(); ++it)
 	{
+		if (it->ingame() && !it->spectator)
+			continue;
+
 		int width = V_StringWidth(it->userinfo.netname.c_str());
 		maxwidth = MAX(maxwidth, width);
+		h += ROW_HEIGHT;
 	}
+	w += maxwidth + BORDER_WIDTH * 2;
+	h += BORDER_WIDTH * 2;
+
+	// Fix our origin so we can draw assuming a top-left origin.
+	ToLeftTop(x, y, w, h, hud_sitrep_scale, X_LEFT, Y_MIDDLE);
+
+	// Dim the background.
+	hud::Dim(x, y, w, h, hud_sitrep_scale, X_LEFT, Y_TOP, X_LEFT, Y_TOP);
 
 	std::string buf;
-	int iy = y;
+	int iy = y + BORDER_WIDTH;
 	for (Players::iterator it = players.begin(); it != players.end(); ++it)
 	{
-		int ix = 0;
+		if (it->ingame() && !it->spectator)
+			continue;
+
+		int ix = x + BORDER_WIDTH;
 
 		// Name
-		hud::DrawText(ix, iy, scale, x_align, y_align, x_origin, y_origin,
+		hud::DrawText(ix, iy, hud_sitrep_scale, X_LEFT, Y_TOP, X_LEFT, Y_TOP,
 		              it->userinfo.netname.c_str(), CR_GREY);
 
 		// Only draw health and armor if they have a body.
@@ -66,13 +129,13 @@ void SitRep()
 			// Health
 			ix += maxwidth;
 			StrFormat(buf, "%4d", it->mo->health);
-			hud::DrawText(ix, iy, scale, x_align, y_align, x_origin, y_origin,
+			hud::DrawText(ix, iy, hud_sitrep_scale, X_LEFT, Y_TOP, X_LEFT, Y_TOP,
 			              buf.c_str(), CR_GREY);
 
 			// Armor
 			ix += V_StringWidth(buf.c_str());
 			StrFormat(buf, "%4d", it->armorpoints);
-			hud::DrawText(ix, iy, scale, x_align, y_align, x_origin, y_origin,
+			hud::DrawText(ix, iy, hud_sitrep_scale, X_LEFT, Y_TOP, X_LEFT, Y_TOP,
 			              buf.c_str(), CR_GREY);
 		}
 
