7b66673a3e2a3645f33dff6e23c19fcc7b6f703a
Alex Mayfield <alexmax2742@gmail.com>
2020-08-21 22:53:55 -0400

    Use distro curl and brew curl

--- ci-build.sh
+++ ci-build.sh
@@ -12,15 +12,15 @@ if [[ $(uname -s) == "Linux" ]]; then
     sudo apt update
     if [[ -z ${USE_SDL12:-} ]]; then
         sudo apt install ninja-build libsdl2-dev libsdl2-mixer-dev \
-            libwxgtk3.0-gtk3-dev deutex
+            libcurl4-openssl-dev libwxgtk3.0-gtk3-dev deutex
     else
         sudo apt install ninja-build libsdl1.2-dev libsdl-mixer1.2-dev \
-            libwxgtk3.0-gtk3-dev deutex
+            libcurl4-openssl-dev libwxgtk3.0-gtk3-dev deutex
     fi
 else
     # macOS
     brew update
-    brew install ninja sdl2 sdl2_mixer wxmac
+    brew install ninja sdl2 sdl2_mixer curl wxmac
 fi
 
 mkdir -p build && cd build
