3d4fe7f516d07f7343ae2bd61efba4b70a64f2ff
Alex Mayfield <alexmax2742@gmail.com>
2020-12-20 21:16:22 -0500

    Build fixes

--- cmake/modules/OdamexTargetSettings.cmake
+++ cmake/modules/OdamexTargetSettings.cmake
@@ -61,7 +61,7 @@ function(odamex_target_settings _TARGET)
 
   # Copy library files to target directory.
   foreach(ODAMEX_DLL ${ODAMEX_DLLS})
-    add_custom_command(TARGET ${TARGET} POST_BUILD
+    add_custom_command(TARGET "${_TARGET}" POST_BUILD
       COMMAND "${CMAKE_COMMAND}" -E copy_if_different
       "${ODAMEX_DLL}" $<TARGET_FILE_DIR:${TARGET}> VERBATIM)
   endforeach()
