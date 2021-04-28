b759d236aba73d0fba51fc9e3b663f02e4e671f9
Alex Mayfield <alexmax2742@gmail.com>
2021-04-28 19:43:58 -0400

    Create my own version of upversion in python
    Two differences:
    - Written in Python, which seems to be the de-facto tooling language
      these days and is even common on Windows.
    - This version does not rely on writing out an uber-gnarly regex for
      each replacement.  Instead, replacements are done by looking for a
      specific old source string.

--- common/version.h
+++ common/version.h
@@ -68,7 +68,12 @@
 	}
 
 // Lots of different representations for the version number
+
+// Used by configuration files.  upversion.py will update thie field
+// deterministically and unambiguously so newer versions always compare
+// greater.
 #define CONFIGVERSIONSTR "90"
+
 #define DOTVERSIONSTR "0.9.0"
 #define GAMEVER (MAKEVER(0, 9, 0))
 
@@ -83,8 +88,10 @@
 
 // SAVESIG is the save game signature. It should be the minimum version
 // whose savegames this version is compatible with, which could be
-// earlier than this version.
-#define SAVESIG "ODAMEXSAVE090   "	// Needs to be exactly 16 chars long
+// earlier than this version.  Needs to be exactly 16 chars long.
+// 
+// upversion.py will update thie field deterministically and unambiguously.
+#define SAVESIG "ODAMEXSAVE090   "
 
 #define NETDEMOVER 3
 
