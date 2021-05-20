ffd9ddb9450fe085717ee8c6a1dc525d8883e3b6
Maëllig Desmottes <Ch0wW@baseq.fr>
2021-05-20 22:02:04 +0200

    Revert "Do not include Chex Quest for the Z-axis bug."
    This reverts commit 14ae2c397692cf977606e2b626a867aa98c08382.

--- common/p_teleport.cpp
+++ common/p_teleport.cpp
@@ -282,7 +282,7 @@ BOOL EV_LineTeleport (line_t *line, int side, AActor *thing)
 
 				fixed_t destz;
 
-				if (demoplayback && (gamemission == pack_tnt || gamemission == pack_plut))
+				if (demoplayback && (gamemission == pack_tnt || gamemission == pack_plut || gamemission == chex))
 					destz = m->z;	// Make sure we have the original Z-Height bug on Final Doom.
 				else
 					destz = (m->type == MT_TELEPORTMAN) ? P_FloorHeight(m) : m->z;
