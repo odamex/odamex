401b4aed513bdb469bd3b81f955f7ecc1d10317d
Mike Lightner <fuzztooth@gmail.com>
2021-06-17 19:55:44 -0500

    Revert "Add team support"
    This reverts commit c7d653a4acd9fec129d54f0b900b9ef6d0a12a7f.

--- common/p_interaction.cpp
+++ common/p_interaction.cpp
@@ -1218,12 +1218,8 @@ static void ClientObituary(AActor* self, AActor* inflictor, AActor* attacker)
 
 		toast_t toast;
 		toast.flags = toast_t::ICON | toast_t::RIGHT;
-		if (G_IsTeamGame())
-		{
-			toast.right += GetTeamInfo(self->player->userinfo.team)->TextColor;
-		}
 		toast.icon = mod;
-		toast.right += self->player->userinfo.netname;
+		toast.right = self->player->userinfo.netname;
 		COM_PushToast(toast);
 		return;
 	}
@@ -1295,14 +1291,9 @@ static void ClientObituary(AActor* self, AActor* inflictor, AActor* attacker)
 
 		toast_t toast;
 		toast.flags = toast_t::LEFT | toast_t::ICON | toast_t::RIGHT;
-		if (G_IsTeamGame())
-		{
-			toast.left += GetTeamInfo(attacker->player->userinfo.team)->TextColor;
-			toast.right += GetTeamInfo(self->player->userinfo.team)->TextColor;
-		}
-		toast.left += attacker->player->userinfo.netname;
+		toast.left = attacker->player->userinfo.netname;
 		toast.icon = mod;
-		toast.right += self->player->userinfo.netname;
+		toast.right = self->player->userinfo.netname;
 		COM_PushToast(toast);
 		return;
 	}
@@ -1314,12 +1305,8 @@ static void ClientObituary(AActor* self, AActor* inflictor, AActor* attacker)
 
 	toast_t toast;
 	toast.flags = toast_t::ICON | toast_t::RIGHT;
-	if (G_IsTeamGame())
-	{
-		toast.right += GetTeamInfo(self->player->userinfo.team)->TextColor;
-	}
 	toast.icon = mod;
-	toast.right += self->player->userinfo.netname;
+	toast.right = self->player->userinfo.netname;
 	COM_PushToast(toast);
 }
 
