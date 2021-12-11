c75bd43290b37dc2148216fd7a2bd2c2bf71c88f
Alex Mayfield <alexmax2742@gmail.com>
2021-12-11 14:09:54 -0500

    Clean up layout, add image

--- client/gui/gui_boot.cpp
+++ client/gui/gui_boot.cpp
@@ -30,10 +30,20 @@
 #include "FL/Fl_Browser.H"
 #include "FL/Fl_Button.H"
 #include "FL/Fl_Double_Window.H"
+#include "FL/Fl_PNG_Image.H"
 #include "FL/Fl_Return_Button.H"
 #include "FL/Fl_Tabs.H"
 
-static void ShowBoot()
+#include "gui_resource.h"
+
+static Fl_Image* image_icon_odamex_128()
+{
+	static Fl_Image* image = new Fl_PNG_Image("icon_odamex_128", __icon_odamex_128_png,
+	                                          __icon_odamex_128_png_len);
+	return image;
+}
+
+static Fl_Double_Window* BootWindow()
 {
 	Fl_Double_Window* w;
 	{
@@ -47,51 +57,52 @@ static void ShowBoot()
 			{
 				Fl_Group* o = new Fl_Group(0, 25, 320, 175, "Game Select");
 				{
-					new Fl_Browser(135, 35, 175, 155);
-				} // Fl_Browser* o
-				{
 					Fl_Box* o = new Fl_Box(10, 34, 115, 157);
+					o->image(image_icon_odamex_128());
 					o->align(Fl_Align(512));
 				} // Fl_Box* o
+				{
+					new Fl_Browser(135, 35, 175, 155);
+				} // Fl_Browser* o
 				o->end();
 			} // Fl_Group* o
 			{
 				Fl_Group* o = new Fl_Group(0, 25, 320, 175, "Resource Locations");
 				o->hide();
 				{
-					new Fl_Browser(10, 85, 265, 105);
-				} // Fl_Browser* o
-				{
-					new Fl_Button(285, 86, 20, 20, "A");
-				} // Fl_Button* o
-				{
-					new Fl_Button(285, 111, 20, 20, "M");
-				} // Fl_Button* o
-				{
-					new Fl_Button(285, 136, 20, 20, "D");
-				} // Fl_Button* o
-				{
-					Fl_Box* o = new Fl_Box(10, 34, 295, 42,
+					Fl_Box* o = new Fl_Box(10, 35, 295, 35,
 					                       "Add directories containing WAD and DEH files "
 					                       "here so Odamex can find them.");
 					o->align(Fl_Align(132 | FL_ALIGN_INSIDE));
 				} // Fl_Box* o
+				{
+					new Fl_Browser(10, 80, 265, 110);
+				} // Fl_Browser* o
+				{
+					new Fl_Button(285, 81, 20, 20, "@+");
+				} // Fl_Button* o
+				{
+					new Fl_Button(285, 104, 20, 20, "@2<<");
+				} // Fl_Button* o
+				{
+					new Fl_Button(285, 127, 20, 20, "@2>>");
+				} // Fl_Button* o
+				{
+					new Fl_Button(285, 150, 20, 20, "@1+");
+				} // Fl_Button* o
 				o->end();
 			} // Fl_Group* o
 			o->end();
 		} // Fl_Tabs* o
 		{
-			new Fl_Button(10, 210, 64, 20, "Quit");
+			new Fl_Button(10, 210, 65, 20, "Quit");
 		} // Fl_Button* o
 		{
-			new Fl_Return_Button(240, 210, 66, 20, "Play!");
+			new Fl_Return_Button(245, 210, 65, 20, "Play!");
 		} // Fl_Return_Button* o
 		o->end();
 	} // Fl_Double_Window* o
-
-	// Center window.
-	w->position((Fl::w() - w->w()) / 2, (Fl::h() - w->h()) / 2);
-	w->show();
+	return w;
 }
 
 void GUI_BootWindow()
@@ -103,7 +114,9 @@ void GUI_BootWindow()
 	// deforms the window.
 	Fl::keyboard_screen_scaling(0);
 
-	ShowBoot();
+	Fl_Double_Window* win = BootWindow();
+	win->position((Fl::w() - win->w()) / 2, (Fl::h() - win->h()) / 2);
+	win->show();
 
 	// Blocks until the boot window has been closed.
 	Fl::run();
