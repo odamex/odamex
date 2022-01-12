a4dcf1679de121e3cd4d30308d922e861f673ce0
DeathEgg <midwestgenius@gmail.com>
2022-01-12 12:50:40 -0800

    Revert "add sprite struct"
    This reverts commit 3e2ca79f4a2a8138ca8edfb48cc16a4559ec9baf.

--- common/r_sprites.cpp
+++ common/r_sprites.cpp
@@ -36,8 +36,6 @@
 
 #include "s_sound.h"
 
-#include "oscanner.h"
-
 #define SPRITE_NEEDS_INFO	MAXINT
 
 //
@@ -259,41 +257,7 @@ int				MaxVisSprites;
 vissprite_t 	*vissprites;
 vissprite_t		*lastvissprite;
 
-//
-// R_InitSkins
-// Called at program start.
-//
-void R_InitSkins()
-{
-	int lump = -1;
 
-	while ((lump = W_FindLump("S_SKIN", lump)) != -1)
-	{
-		const char* buffer = static_cast<char*>(W_CacheLumpNum(lump, PU_STATIC));
-
-		// todo - add # comments to oscanner
-		OScannerConfig config = {
-		    "S_SKIN", // lumpName
-		    true,    // semiComments
-		    true,     // cComments
-		};
-		OScanner os = OScanner::openBuffer(config, buffer, buffer + W_LumpLength(lump));
-
-		
-
-		while (os.scan())
-		{
-			/*if (os.compareTokenNoCase("name"))
-			{
-				os.mustScan();
-				if (os.compareTokenNoCase("=") == false)
-				{
-					os.error("Expected '=', got '%s'.", os.getToken().c_str());
-				}
-			}*/
-		}
-	}
-}
 
 //
 // R_InitSprites
@@ -308,8 +272,7 @@ void R_InitSprites(const char **namelist)
 	vissprites = (vissprite_t *)Malloc(MaxVisSprites * sizeof(vissprite_t));
 	lastvissprite = &vissprites[MaxVisSprites];
 
-	R_InitSpriteDefs(namelist);
-	R_InitSkins();
+	R_InitSpriteDefs (namelist);
 }
 
 VERSION_CONTROL (r_sprites_cpp, "$Id$")
