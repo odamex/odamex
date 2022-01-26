7c2d21c78a4c35b9e2a0ecca2306e9594a04b53c
Alex Mayfield <alexmax2742@gmail.com>
2022-01-26 00:32:16 -0500

    Add infos for some messages

--- common/i_net.cpp
+++ common/i_net.cpp
@@ -1129,6 +1129,8 @@ static void InitNetMessageFormats()
 	SVC_INFO(svc_maplist);
 	SVC_INFO(svc_maplist_update);
 	SVC_INFO(svc_maplist_index);
+	SVC_INFO(svc_toast);
+	SVC_INFO(svc_hordeinfo);
 	SVC_INFO(svc_max);
 
 	// Client Messages.
