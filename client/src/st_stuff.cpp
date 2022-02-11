42e50b1ce033872c6e59d3761c31fcb33e3d14ca
DeathEgg <midwestgenius@gmail.com>
2022-02-11 02:52:19 -0800

    adjust st_lib functions to be member functions of the structs
    did this on the heretic branch. it's a bit neater

--- client/src/st_stuff.cpp
+++ client/src/st_stuff.cpp
@@ -301,6 +301,9 @@ lumpHandle_t keys[NUMCARDS + NUMCARDS / 2];
 // face status patches [RH] no longer static
 lumpHandle_t faces[ST_NUMFACES];
 
+// negative number patch
+lumpHandle_t negminus;
+
 // face background
 static lumpHandle_t faceback;
 
@@ -988,29 +991,29 @@ void ST_drawWidgets(bool force_refresh)
 	// used by w_frags widget
 	st_fragson = !G_IsCoopGame() && st_statusbaron;
 
-	STlib_updateNum(&w_ready, force_refresh);
+	w_ready.update(force_refresh);
 
 	for (int i = 0; i < 4; i++)
 	{
-		STlib_updateNum(&w_ammo[i], force_refresh);
-		STlib_updateNum(&w_maxammo[i], force_refresh);
+		w_ammo[i].update(force_refresh);
+		w_maxammo[i].update(force_refresh);
 	}
 
-	STlib_updatePercent(&w_health, force_refresh);
-	STlib_updatePercent(&w_armor, force_refresh);
+	w_health.update(force_refresh);
+	w_armor.update(force_refresh);
 
 	for (int i = 0; i < 6; i++)
-		STlib_updateMultIcon(&w_arms[i], force_refresh);
+		w_arms[i].update(force_refresh);
 
-	STlib_updateMultIcon(&w_faces, force_refresh);
+	w_faces.update(force_refresh);
 
 	for (int i = 0; i < 3; i++)
-		STlib_updateMultIcon(&w_keyboxes[i], force_refresh);
+		w_keyboxes[i].update(force_refresh);
 
-	STlib_updateNum(&w_frags, force_refresh);
+	w_frags.update(force_refresh);
 
-	STlib_updateNum(&w_lives, true, G_IsLivesGame() ? true : false); // Force refreshing to avoid tens
-		                                                 // to be hidden by Doomguy's face
+	w_lives.update(true, G_IsLivesGame()); // Force refreshing to avoid tens
+	                                       // to be hidden by Doomguy's face
 
 }
 
@@ -1245,10 +1248,8 @@ static void ST_loadData()
 
 static void ST_unloadGraphics()
 {
-	int i;
-
 	// unload the numbers, tall and short
-	for (i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		::tallnum[i].clear();
 		::shortnum[i].clear();
@@ -1264,13 +1265,13 @@ static void ST_unloadGraphics()
 	::flagsbg.clear();
 
 	// unload gray #'s
-	for (i = 0; i < 6; i++)
+	for (int i = 0; i < 6; i++)
 	{
 		::arms[i][0].clear();
 	}
 
 	// unload the key cards
-	for (i = 0; i < NUMCARDS + NUMCARDS / 2; i++)
+	for (int i = 0; i < NUMCARDS + NUMCARDS / 2; i++)
 	{
 		::keys[i].clear();
 	}
@@ -1278,13 +1279,12 @@ static void ST_unloadGraphics()
 	::sbar.clear();
 	::faceback.clear();
 
-	for (i = 0; i < ST_NUMFACES; i++)
+	for (int i = 0; i < ST_NUMFACES; i++)
 	{
 		::faces[i].clear();
 	}
 
-	// Note: nobody ain't seen no unloading
-	//	 of stminus yet. Dude.
+	::negminus.clear();
 }
 
 static void ST_unloadData()
@@ -1294,153 +1294,69 @@ static void ST_unloadData()
 }
 
 
-void ST_createWidgets(void)
+void ST_createWidgets()
 {
 	// ready weapon ammo
-	STlib_initNum(&w_ready,
-				  ST_AMMOX,
-				  ST_AMMOY,
-				  tallnum,
-				  &st_current_ammo,
-				  &st_statusbaron,
-				  ST_AMMOWIDTH );
+	w_ready.init(ST_AMMOX, ST_AMMOY, tallnum, &st_current_ammo, &st_statusbaron,
+	             ST_AMMOWIDTH);
 
 	// health percentage
-	STlib_initPercent(&w_health,
-					  ST_HEALTHX,
-					  ST_HEALTHY,
-					  tallnum,
-					  &st_health,
-					  &st_statusbaron,
-					  tallpercent);
+	w_health.init(ST_HEALTHX, ST_HEALTHY, tallnum, &st_health, &st_statusbaron,
+	              tallpercent);
 
 	// weapons owned
-	for (int i = 0 ; i < 6; i++)
+	for (int i = 0; i < 6; i++)
 	{
-		STlib_initMultIcon(&w_arms[i],
-						   ST_ARMSX+(i%3)*ST_ARMSXSPACE,
-						   ST_ARMSY+(i/3)*ST_ARMSYSPACE,
-						   arms[i],
-						   &st_weaponowned[i],
-						   &st_armson);
+		w_arms[i].init(ST_ARMSX + (i % 3) * ST_ARMSXSPACE,
+		               ST_ARMSY + (i / 3) * ST_ARMSYSPACE, arms[i], &st_weaponowned[i],
+		               &st_armson);
 	}
 
 	// frags sum
-	STlib_initNum(&w_frags,
-				  ST_FRAGSX,
-				  ST_FRAGSY,
-				  tallnum,
-				  &st_fragscount,
-				  &st_fragson,
-				  ST_FRAGSWIDTH);
+	w_frags.init(ST_FRAGSX, ST_FRAGSY, tallnum, &st_fragscount, &st_fragson,
+	             ST_FRAGSWIDTH);
 
 	// faces
-	STlib_initMultIcon(&w_faces,
-					   ST_FACESX,
-					   ST_FACESY,
-					   faces,
-					   &st_faceindex,
-					   &st_statusbaron);
+	w_faces.init(ST_FACESX, ST_FACESY, faces, &st_faceindex, &st_statusbaron);
 
 	// armor percentage - should be colored later
-	STlib_initPercent(&w_armor,
-					  ST_ARMORX,
-					  ST_ARMORY,
-					  tallnum,
-					  &st_armor,
-					  &st_statusbaron, tallpercent);
+	w_armor.init(ST_ARMORX, ST_ARMORY, tallnum, &st_armor, &st_statusbaron, tallpercent);
 
 	// keyboxes 0-2
-	STlib_initMultIcon(&w_keyboxes[0],
-					   ST_KEY0X,
-					   ST_KEY0Y,
-					   keys,
-					   &keyboxes[0],
-					   &st_statusbaron);
+	w_keyboxes[0].init(ST_KEY0X, ST_KEY0Y, keys, &keyboxes[0], &st_statusbaron);
 
-	STlib_initMultIcon(&w_keyboxes[1],
-					   ST_KEY1X,
-					   ST_KEY1Y,
-					   keys,
-					   &keyboxes[1],
-					   &st_statusbaron);
+	w_keyboxes[1].init(ST_KEY1X, ST_KEY1Y, keys, &keyboxes[1], &st_statusbaron);
 
-	STlib_initMultIcon(&w_keyboxes[2],
-					   ST_KEY2X,
-					   ST_KEY2Y,
-					   keys,
-					   &keyboxes[2],
-					   &st_statusbaron);
+	w_keyboxes[2].init(ST_KEY2X, ST_KEY2Y, keys, &keyboxes[2], &st_statusbaron);
 
 	// ammo count (all four kinds)
-	STlib_initNum(&w_ammo[0],
-				  ST_AMMO0X,
-				  ST_AMMO0Y,
-				  shortnum,
-				  &st_ammo[0],
-				  &st_statusbaron,
-				  ST_AMMO0WIDTH);
+	w_ammo[0].init(ST_AMMO0X, ST_AMMO0Y, shortnum, &st_ammo[0], &st_statusbaron,
+	               ST_AMMO0WIDTH);
 
-	STlib_initNum(&w_ammo[1],
-				  ST_AMMO1X,
-				  ST_AMMO1Y,
-				  shortnum,
-				  &st_ammo[1],
-				  &st_statusbaron,
-				  ST_AMMO1WIDTH);
+	w_ammo[1].init(ST_AMMO1X, ST_AMMO1Y, shortnum, &st_ammo[1], &st_statusbaron,
+	               ST_AMMO1WIDTH);
 
-	STlib_initNum(&w_ammo[2],
-				  ST_AMMO2X,
-				  ST_AMMO2Y,
-				  shortnum,
-				  &st_ammo[2],
-				  &st_statusbaron,
-				  ST_AMMO2WIDTH);
+	w_ammo[2].init(ST_AMMO2X, ST_AMMO2Y, shortnum, &st_ammo[2], &st_statusbaron,
+	               ST_AMMO2WIDTH);
 
-	STlib_initNum(&w_ammo[3],
-				  ST_AMMO3X,
-				  ST_AMMO3Y,
-				  shortnum,
-				  &st_ammo[3],
-				  &st_statusbaron,
-				  ST_AMMO3WIDTH);
+	w_ammo[3].init(ST_AMMO3X, ST_AMMO3Y, shortnum, &st_ammo[3], &st_statusbaron,
+	               ST_AMMO3WIDTH);
 
 	// max ammo count (all four kinds)
-	STlib_initNum(&w_maxammo[0],
-				  ST_MAXAMMO0X,
-				  ST_MAXAMMO0Y,
-				  shortnum,
-				  &st_maxammo[0],
-				  &st_statusbaron,
-				  ST_MAXAMMO0WIDTH);
+	w_maxammo[0].init(ST_MAXAMMO0X, ST_MAXAMMO0Y, shortnum, &st_maxammo[0],
+	                  &st_statusbaron, ST_MAXAMMO0WIDTH);
 
-	STlib_initNum(&w_maxammo[1],
-				  ST_MAXAMMO1X,
-				  ST_MAXAMMO1Y,
-				  shortnum,
-				  &st_maxammo[1],
-				  &st_statusbaron,
-				  ST_MAXAMMO1WIDTH);
+	w_maxammo[1].init(ST_MAXAMMO1X, ST_MAXAMMO1Y, shortnum, &st_maxammo[1],
+	                  &st_statusbaron, ST_MAXAMMO1WIDTH);
 
-	STlib_initNum(&w_maxammo[2],
-				  ST_MAXAMMO2X,
-				  ST_MAXAMMO2Y,
-				  shortnum,
-				  &st_maxammo[2],
-				  &st_statusbaron,
-				  ST_MAXAMMO2WIDTH);
+	w_maxammo[2].init(ST_MAXAMMO2X, ST_MAXAMMO2Y, shortnum, &st_maxammo[2],
+	                  &st_statusbaron, ST_MAXAMMO2WIDTH);
 
-	STlib_initNum(&w_maxammo[3],
-				  ST_MAXAMMO3X,
-				  ST_MAXAMMO3Y,
-				  shortnum,
-				  &st_maxammo[3],
-				  &st_statusbaron,
-				  ST_MAXAMMO3WIDTH);
+	w_maxammo[3].init(ST_MAXAMMO3X, ST_MAXAMMO3Y, shortnum, &st_maxammo[3],
+	                  &st_statusbaron, ST_MAXAMMO3WIDTH);
 
 	// Number of lives (not always rendered)
-	STlib_initNum(&w_lives, ST_FX + 34, ST_FY + 25, shortnum, &st_lives, &st_statusbaron,
-	              2);
+	w_lives.init(ST_FX + 34, ST_FY + 25, shortnum, &st_lives, &st_statusbaron, 2);
 }
 
 void ST_Start()
@@ -1465,8 +1381,7 @@ void ST_Start()
 
 	for (int i = 0; i < 3; i++)
 		keyboxes[i] = -1;
-
-	STlib_init();
+	
 	ST_initNew();
 
 	ST_createWidgets();
