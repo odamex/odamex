66940f5eacd2952278e0b2b327775321f6a5ceec
DeathEgg <midwestgenius@gmail.com>
2022-02-13 03:34:18 -0800

    minor edits to code

--- client/src/st_lib.cpp
+++ client/src/st_lib.cpp
@@ -26,7 +26,6 @@
 #include "odamex.h"
 
 #include "i_video.h"
-#include "v_video.h"
 
 #include "w_wad.h"
 
@@ -38,13 +37,13 @@
 EXTERN_CVAR(st_scale)
 
 //
-// STlib_ClearRect
+// StatusBarWidget::clearRect
 //
 // Copies a rectangular portion of the status bar background surface onto
 // the drawing surface. This is used prior to drawing a widget (number or patch)
 // onto the surface.
 //
-static void STlib_ClearRect(int x, int y, int w, int h)
+void StatusBarWidget::clearRect(int x, int y, int w, int h)
 {
 	if (st_scale)
 	{
@@ -61,12 +60,12 @@ static void STlib_ClearRect(int x, int y, int w, int h)
 }
 
 //
-// STlib_DrawPatch
+// StatusBarWidget::drawPatch
 //
 // Draws a patch onto the drawing surface. STlib_ClearRect should be used to
 // draw a fresh copy of the background prior to drawing the patch.
 //
-static void STlib_DrawPatch(int x, int y, const patch_t* p)
+void StatusBarWidget::drawPatch(int x, int y, const patch_t* p)
 {
 	if (st_scale)
 	{
@@ -83,7 +82,7 @@ static void STlib_DrawPatch(int x, int y, const patch_t* p)
 	}
 }
 
-void st_number_s::init(int x_, int y_, lumpHandle_t* pl, int* num_, bool* on_,
+void st_number_t::init(int x_, int y_, lumpHandle_t* pl, int* num_, bool* on_,
                        int maxdigits_)
 {
 	x = x_;
@@ -96,19 +95,18 @@ void st_number_s::init(int x_, int y_, lumpHandle_t* pl, int* num_, bool* on_,
 }
 
 //
-// st_number_s::draw
+// st_number_s::update
 //
 // A fairly efficient way to draw a number based on differences from the
 // old number. Worth the trouble?
 //
-void st_number_s::draw(bool force_refresh, bool cleararea)
+void st_number_t::update(bool force_refresh, bool cleararea)
 {
+	if (!on)
+		return;
+
 	// [jsd]: prevent null references as hard as possible
-	if (num == NULL)
-		return;
-	if (on == NULL)
-		return;
-	if (p == NULL)
+	if (num == NULL || on == NULL || p == NULL)
 		return;
 
 	// only draw if the number is different or refresh is forced
@@ -137,7 +135,7 @@ void st_number_s::draw(bool force_refresh, bool cleararea)
 
 	// clear the area
 	if (cleararea)
-		STlib_ClearRect(x - w * maxdigits, y, w * maxdigits, h);
+		clearRect(x - w * maxdigits, y, w * maxdigits, h);
 
 	// if non-number, do not draw it
 	if (number == ST_DONT_DRAW_NUM)
@@ -147,26 +145,20 @@ void st_number_s::draw(bool force_refresh, bool cleararea)
 
 	// in the special case of 0, you draw 0
 	if (number == 0)
-		STlib_DrawPatch(drawx - w, y, p0);
+		drawPatch(drawx - w, y, p0);
 
 	// draw the new number
 	for (int numdigits = maxdigits; number && numdigits; numdigits--)
 	{
 		const patch_t* pnum = W_ResolvePatchHandle(p[number % 10]);
 		drawx -= w;
-		STlib_DrawPatch(drawx, y, pnum);
+		drawPatch(drawx, y, pnum);
 		number /= 10;
 	}
 
 	// draw a minus sign if necessary
 	if (negative)
-		STlib_DrawPatch(drawx - 8, y, W_ResolvePatchHandle(negminus));
-}
-
-void st_number_s::update(bool force_refresh, bool cleararea)
-{
-	if (on)
-		draw(force_refresh, cleararea);
+		drawPatch(drawx - 8, y, W_ResolvePatchHandle(negminus));
 }
 
 void st_percent_t::init(int x, int y, lumpHandle_t* pl, int* num, bool* on,
@@ -179,7 +171,7 @@ void st_percent_t::init(int x, int y, lumpHandle_t* pl, int* num, bool* on,
 void st_percent_t::update(bool force_refresh)
 {
 	if (force_refresh && *n.on)
-		STlib_DrawPatch(n.x, n.y, W_ResolvePatchHandle(p));
+		drawPatch(n.x, n.y, W_ResolvePatchHandle(p));
 
 	n.update(force_refresh);
 }
@@ -194,7 +186,7 @@ void st_multicon_t::init(int x_, int y_, lumpHandle_t* il, int* inum_, bool* on_
 	p = il;
 }
 
-void st_multicon_s::update(bool force_refresh)
+void st_multicon_t::update(bool force_refresh)
 {
 	if (on && (force_refresh || oldinum != *inum) && (*inum != -1))
 	{
@@ -207,11 +199,11 @@ void st_multicon_s::update(bool force_refresh)
 			const int w = oldnum->width();
 			const int h = oldnum->height();
 
-			STlib_ClearRect(drawx, drawy, w, h);
+			clearRect(drawx, drawy, w, h);
 		}
 
 		const patch_t* drawinum = W_ResolvePatchHandle(p[*inum]);
-		STlib_DrawPatch(x, y, drawinum);
+		drawPatch(x, y, drawinum);
 		oldinum = *inum;
 	}
 }
@@ -237,9 +229,9 @@ void st_binicon_t::update(bool force_refresh)
 		const int h = iconp->height();
 
 		if (*val)
-			STlib_DrawPatch(x, y, iconp);
+			drawPatch(x, y, iconp);
 		else
-			STlib_ClearRect(drawx, drawy, w, h);
+			clearRect(drawx, drawy, w, h);
 
 		oldval = *val;
 	}
