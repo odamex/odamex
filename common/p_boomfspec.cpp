cf6ab40c031e325d8259b94820cb84e0b8eae9ee
bcahue <bcahue@users.noreply.github.com>
2022-02-13 13:18:20 -0500

    allow sector specials to be used on top of generalized sectors, fix silent teleporters ignoring side

--- common/p_boomfspec.cpp
+++ common/p_boomfspec.cpp
@@ -254,6 +254,15 @@ lineresult_s P_CrossCompatibleSpecialLine(line_t* line, int side, AActor* thing,
 	// Dispatch on the line special value to the line's action routine
 	// If a once only function, and successful, clear the line special
 
+	// Do not teleport on the wrong side
+	if (side)
+	{
+		if (P_IsTeleportLine(line->special))
+		{
+			return result;
+		}
+	}
+
 	switch (line->special)
 	{
 		// Regular walk once triggers
@@ -1599,7 +1608,7 @@ void P_SpawnCompatibleSectorSpecial(sector_t* sector)
 	if (sector->special & PUSH_MASK)
 		sector->flags |= SECF_PUSH;
 
-	switch (sector->special)
+	switch (sector->special & 31)
 	{
 	case 1:
 		if (IgnoreSpecial)
