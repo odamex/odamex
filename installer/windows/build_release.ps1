436e4b7c9c46143e50edcafc3a07502c585edee7
DeathEgg <midwestgenius@gmail.com>
2022-02-28 01:33:56 -0800

    Revert "Update to v11"
    This reverts commit 8b7ff0aea8a58373588f332451ed45c6742bb183.

--- installer/windows/build_release.ps1
+++ installer/windows/build_release.ps1
@@ -9,7 +9,7 @@
 # These parameters can and should be changed for new versions.
 # 
 
-Set-Variable -Name "OdamexVersion" -Value "11.0.0"
+Set-Variable -Name "OdamexVersion" -Value "10.0.0"
 Set-Variable -Name "OdamexTestSuffix" -Value "" # "-RC3"
 
 #
