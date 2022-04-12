a31dc2a9f4dbab3608c2c90c1510efb82c932ca3
bcahue <bcahue@users.noreply.github.com>
2022-04-11 23:35:37 -0400

    7z x gonna give it to ya, and by it, i mean a working pipeline (hopefully)

--- ci/win-get-demotester.ps1
+++ ci/win-get-demotester.ps1
@@ -9,7 +9,7 @@ New-Item -Name "demotester" -ItemType "directory" | Out-Null
 Invoke-WebRequest -Uri $DemoTesterPath -OutFile .\odatests.zip
 Invoke-WebRequest -Uri $DemoResourcePath -OutFile .\odatests-resources.zip
 
-7z.exe e odatests.zip -odemotester -y
-7z.exe e odatests-resources.zip -odemotester -y
+7z.exe x odatests.zip -odemotester -y
+7z.exe x odatests-resources.zip -odemotester -y
 
 Set-Location ..
