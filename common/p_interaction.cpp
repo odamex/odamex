3109ad95d9171f938cb8cc4e3939408f7e03217c
bcahue <bcahue@users.noreply.github.com>
2022-07-23 11:54:19 -0400

    remove horde boss effects on death

--- common/p_interaction.cpp
+++ common/p_interaction.cpp
@@ -1700,6 +1700,14 @@ void P_KillMobj(AActor *source, AActor *target, AActor *inflictor, bool joinkill
 	target->flags |= MF_CORPSE|MF_DROPOFF;
 	target->height >>= 2;
 
+	// Remove any sort of boss effect on kill
+	// OFlags hack because of client issues
+	if (target->oflags)
+	{
+		target->effects = 0;
+		target->translation = translationref_t();
+	}
+
 	// [RH] If the thing has a special, execute and remove it
 	//		Note that the thing that killed it is considered
 	//		the activator of the script.
