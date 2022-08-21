bb73eb22c15424de944f40a9de8bc7b8aad1c514
bcahue <bcahue@users.noreply.github.com>
2022-08-21 13:17:42 -0400

    modify the build script to use current dir instead of script dir

--- installer/windows/build_release.ps1
+++ installer/windows/build_release.ps1
@@ -9,6 +9,8 @@
 # These parameters can and should be changed for new versions.
 # 
 
+Set-Variable -Name "CurrentDir" -Value Get-Location
+
 Set-Variable -Name "OdamexVersion" -Value "10.2.0"
 Set-Variable -Name "OdamexTestSuffix" -Value "" # "-RC3"
 
@@ -16,37 +18,43 @@ Set-Variable -Name "OdamexTestSuffix" -Value "" # "-RC3"
 # The actual script follows.
 #
 
-Set-Variable -Name "CommonDir" -Value "${PSScriptRoot}\OutCommon"
-Set-Variable -Name "X64Dir" -Value "${PSScriptRoot}\OutX64"
-Set-Variable -Name "X86Dir" -Value "${PSScriptRoot}\OutX86"
-Set-Variable -Name "OutputDir" -Value "${PSScriptRoot}\Output"
+Set-Variable -Name "CommonDir" -Value "${CurrentDir}\OutCommon"
+Set-Variable -Name "X64Dir" -Value "${CurrentDir}\OutX64"
+Set-Variable -Name "X86Dir" -Value "${CurrentDir}\OutX86"
+Set-Variable -Name "OutputDir" -Value "${CurrentDir}\Output"
 
 function BuildX64 {
-    Remove-Item -Recurse -Path "${PSScriptRoot}\BuildX64"
-    New-Item  -Force -ItemType "directory" -Path "${PSScriptRoot}\BuildX64"
-    Set-Location -Path "${PSScriptRoot}\BuildX64"
+    if (Test-Path "${CurrentDir}\BuildX64")
+    {
+        Remove-Item -Recurse -Path "${CurrentDir}\BuildX64"
+    }
+    New-Item  -Force -ItemType "directory" -Path "${CurrentDir}\BuildX64"
+    Set-Location -Path "${CurrentDir}\BuildX64"
     
-    cmake.exe -G "Visual Studio 17 2022" -A "x64" "${PSScriptRoot}\..\.." `
+    cmake.exe -G "Visual Studio 17 2022" -A "x64" "${CurrentDir}\..\.." `
         -DBUILD_OR_FAIL=1 `
         -DBUILD_CLIENT=1 -DBUILD_SERVER=1 `
         -DBUILD_MASTER=1 -DBUILD_LAUNCHER=1
     cmake.exe --build . --config RelWithDebInfo
 
-    Set-Location -Path "${PSScriptRoot}"
+    Set-Location -Path "${CurrentDir}"
 }
 
 function BuildX86 {
-    Remove-Item -Recurse -Path "${PSScriptRoot}\BuildX86"
-    New-Item  -Force -ItemType "directory" -Path "${PSScriptRoot}\BuildX86"
-    Set-Location -Path "${PSScriptRoot}\BuildX86"
+    if (Test-Path "${CurrentDir}\BuildX86")
+    {
+        Remove-Item -Recurse -Path "${CurrentDir}\BuildX86"
+    }
+    New-Item  -Force -ItemType "directory" -Path "${CurrentDir}\BuildX86"
+    Set-Location -Path "${CurrentDir}\BuildX86"
     
-    cmake.exe -G "Visual Studio 17 2022" -A "Win32" "${PSScriptRoot}\..\.." `
+    cmake.exe -G "Visual Studio 17 2022" -A "Win32" "${CurrentDir}\..\.." `
         -DBUILD_OR_FAIL=1 `
         -DBUILD_CLIENT=1 -DBUILD_SERVER=1 `
         -DBUILD_MASTER=1 -DBUILD_LAUNCHER=1
     cmake.exe --build . --config RelWithDebInfo
 
-    Set-Location -Path "${PSScriptRoot}"
+    Set-Location -Path "${CurrentDir}"
 }
 
 function CopyFiles {
@@ -57,45 +65,45 @@ function CopyFiles {
     New-Item -Force -ItemType "directory" -Path "${CommonDir}\config-samples"
     New-Item -Force -ItemType "directory" -Path "${CommonDir}\licenses"
 
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\3RD-PARTY-LICENSES" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\3RD-PARTY-LICENSES" `
         -Destination "${CommonDir}\3RD-PARTY-LICENSES.txt"
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\CHANGELOG" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\CHANGELOG" `
         -Destination "${CommonDir}\CHANGELOG.txt"
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\odamex-installed.txt" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\odamex-installed.txt" `
         -Destination "${CommonDir}"
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\config-samples\*" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\config-samples\*" `
         -Destination "${CommonDir}\config-samples"
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\libraries\curl\COPYING" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\libraries\curl\COPYING" `
         -Destination "${CommonDir}\licenses\COPYING.curl.txt"
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\libraries\libminiupnpc\LICENSE" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\libraries\libminiupnpc\LICENSE" `
         -Destination "${CommonDir}\licenses\LICENSE.libminiupnpc.txt"
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\libraries\libpng\LICENSE" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\libraries\libpng\LICENSE" `
         -Destination "${CommonDir}\licenses\LICENSE.libpng.txt"
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\libraries\portmidi\license.txt" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\libraries\portmidi\license.txt" `
         -Destination "${CommonDir}\licenses\license.portmidi.txt"
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\LICENSE" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\LICENSE" `
         -Destination "${CommonDir}\LICENSE.txt"
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\MAINTAINERS" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\MAINTAINERS" `
         -Destination "${CommonDir}\MAINTAINERS.txt"
-    Copy-Item -Force -Path "${PSScriptRoot}\..\..\README" `
+    Copy-Item -Force -Path "${CurrentDir}\..\..\README" `
         -Destination "${CommonDir}\README.txt"
-    Copy-Item -Force -Path "${PSScriptRoot}\BuildX64\wad\odamex.wad", `
+    Copy-Item -Force -Path "${CurrentDir}\BuildX64\wad\odamex.wad", `
         -Destination "${CommonDir}"
-    Copy-Item -Force -Path "${PSScriptRoot}\BuildX64\libraries\SDL2_mixer-2.0.4\COPYING.txt" `
+    Copy-Item -Force -Path "${CurrentDir}\BuildX64\libraries\SDL2_mixer-2.0.4\COPYING.txt" `
         -Destination "${CommonDir}\licenses\COPYING.SDL2_mixer.txt"
-    Copy-Item -Force -Path "${PSScriptRoot}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.FLAC.txt" `
+    Copy-Item -Force -Path "${CurrentDir}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.FLAC.txt" `
         -Destination "${CommonDir}\licenses"
-    Copy-Item -Force -Path "${PSScriptRoot}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.modplug.txt" `
+    Copy-Item -Force -Path "${CurrentDir}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.modplug.txt" `
         -Destination "${CommonDir}\licenses"
-    Copy-Item -Force -Path "${PSScriptRoot}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.mpg123.txt" `
+    Copy-Item -Force -Path "${CurrentDir}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.mpg123.txt" `
         -Destination "${CommonDir}\licenses"
-    Copy-Item -Force -Path "${PSScriptRoot}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.ogg-vorbis.txt" `
+    Copy-Item -Force -Path "${CurrentDir}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.ogg-vorbis.txt" `
         -Destination "${CommonDir}\licenses"
-    Copy-Item -Force -Path "${PSScriptRoot}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.opus.txt" `
+    Copy-Item -Force -Path "${CurrentDir}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.opus.txt" `
         -Destination "${CommonDir}\licenses"
-    Copy-Item -Force -Path "${PSScriptRoot}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.opusfile.txt" `
+    Copy-Item -Force -Path "${CurrentDir}\BuildX64\libraries\SDL2_mixer-2.0.4\lib\x64\LICENSE.opusfile.txt" `
         -Destination "${CommonDir}\licenses"
-    Copy-Item -Force -Path "${PSScriptRoot}\BuildX64\libraries\SDL2-2.0.20\COPYING.txt" `
+    Copy-Item -Force -Path "${CurrentDir}\BuildX64\libraries\SDL2-2.0.20\COPYING.txt" `
         -Destination "${CommonDir}\licenses\COPYING.SDL2.txt"
 
     ########################################
@@ -105,24 +113,24 @@ function CopyFiles {
     New-Item -Force -ItemType "directory" -Path "${X64Dir}"
 
     Copy-Item -Force -Path `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\libFLAC-8.dll", `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\libmodplug-1.dll", `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\libmpg123-0.dll", `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\libogg-0.dll", `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\libopus-0.dll", `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\libvorbis-0.dll", `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\libvorbisfile-3.dll", `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\odamex.exe", `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\SDL2_mixer.dll", `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\SDL2.dll", `
-        "${PSScriptRoot}\BuildX64\odalaunch\RelWithDebInfo\odalaunch.exe", `
-        "${PSScriptRoot}\BuildX64\odalaunch\RelWithDebInfo\wxbase315u_net_vc14x_x64.dll", `
-        "${PSScriptRoot}\BuildX64\odalaunch\RelWithDebInfo\wxbase315u_vc14x_x64.dll", `
-        "${PSScriptRoot}\BuildX64\odalaunch\RelWithDebInfo\wxbase315u_xml_vc14x_x64.dll", `
-        "${PSScriptRoot}\BuildX64\odalaunch\RelWithDebInfo\wxmsw315u_core_vc14x_x64.dll", `
-        "${PSScriptRoot}\BuildX64\odalaunch\RelWithDebInfo\wxmsw315u_html_vc14x_x64.dll", `
-        "${PSScriptRoot}\BuildX64\odalaunch\RelWithDebInfo\wxmsw315u_xrc_vc14x_x64.dll", `
-        "${PSScriptRoot}\BuildX64\server\RelWithDebInfo\odasrv.exe" `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\libFLAC-8.dll", `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\libmodplug-1.dll", `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\libmpg123-0.dll", `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\libogg-0.dll", `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\libopus-0.dll", `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\libvorbis-0.dll", `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\libvorbisfile-3.dll", `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\odamex.exe", `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\SDL2_mixer.dll", `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\SDL2.dll", `
+        "${CurrentDir}\BuildX64\odalaunch\RelWithDebInfo\odalaunch.exe", `
+        "${CurrentDir}\BuildX64\odalaunch\RelWithDebInfo\wxbase315u_net_vc14x_x64.dll", `
+        "${CurrentDir}\BuildX64\odalaunch\RelWithDebInfo\wxbase315u_vc14x_x64.dll", `
+        "${CurrentDir}\BuildX64\odalaunch\RelWithDebInfo\wxbase315u_xml_vc14x_x64.dll", `
+        "${CurrentDir}\BuildX64\odalaunch\RelWithDebInfo\wxmsw315u_core_vc14x_x64.dll", `
+        "${CurrentDir}\BuildX64\odalaunch\RelWithDebInfo\wxmsw315u_html_vc14x_x64.dll", `
+        "${CurrentDir}\BuildX64\odalaunch\RelWithDebInfo\wxmsw315u_xrc_vc14x_x64.dll", `
+        "${CurrentDir}\BuildX64\server\RelWithDebInfo\odasrv.exe" `
         -Destination "${X64Dir}\"
 
     ########################################
@@ -132,24 +140,24 @@ function CopyFiles {
     New-Item -Force -ItemType "directory" -Path "${X86Dir}"
 
     Copy-Item -Force -Path `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\libFLAC-8.dll", `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\libmodplug-1.dll", `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\libmpg123-0.dll", `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\libogg-0.dll", `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\libopus-0.dll", `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\libvorbis-0.dll", `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\libvorbisfile-3.dll", `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\odamex.exe", `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\SDL2_mixer.dll", `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\SDL2.dll", `
-        "${PSScriptRoot}\BuildX86\odalaunch\RelWithDebInfo\odalaunch.exe", `
-        "${PSScriptRoot}\BuildX86\odalaunch\RelWithDebInfo\wxbase315u_net_vc14x.dll", `
-        "${PSScriptRoot}\BuildX86\odalaunch\RelWithDebInfo\wxbase315u_vc14x.dll", `
-        "${PSScriptRoot}\BuildX86\odalaunch\RelWithDebInfo\wxbase315u_xml_vc14x.dll", `
-        "${PSScriptRoot}\BuildX86\odalaunch\RelWithDebInfo\wxmsw315u_core_vc14x.dll", `
-        "${PSScriptRoot}\BuildX86\odalaunch\RelWithDebInfo\wxmsw315u_html_vc14x.dll", `
-        "${PSScriptRoot}\BuildX86\odalaunch\RelWithDebInfo\wxmsw315u_xrc_vc14x.dll", `
-        "${PSScriptRoot}\BuildX86\server\RelWithDebInfo\odasrv.exe" `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\libFLAC-8.dll", `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\libmodplug-1.dll", `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\libmpg123-0.dll", `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\libogg-0.dll", `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\libopus-0.dll", `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\libvorbis-0.dll", `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\libvorbisfile-3.dll", `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\odamex.exe", `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\SDL2_mixer.dll", `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\SDL2.dll", `
+        "${CurrentDir}\BuildX86\odalaunch\RelWithDebInfo\odalaunch.exe", `
+        "${CurrentDir}\BuildX86\odalaunch\RelWithDebInfo\wxbase315u_net_vc14x.dll", `
+        "${CurrentDir}\BuildX86\odalaunch\RelWithDebInfo\wxbase315u_vc14x.dll", `
+        "${CurrentDir}\BuildX86\odalaunch\RelWithDebInfo\wxbase315u_xml_vc14x.dll", `
+        "${CurrentDir}\BuildX86\odalaunch\RelWithDebInfo\wxmsw315u_core_vc14x.dll", `
+        "${CurrentDir}\BuildX86\odalaunch\RelWithDebInfo\wxmsw315u_html_vc14x.dll", `
+        "${CurrentDir}\BuildX86\odalaunch\RelWithDebInfo\wxmsw315u_xrc_vc14x.dll", `
+        "${CurrentDir}\BuildX86\server\RelWithDebInfo\odasrv.exe" `
         -Destination "${X86Dir}\"
 }
 
@@ -176,23 +184,23 @@ function Outputs {
 function ZipDebug {
     # Copy pdb files into zip.  DO NOT THROW THESE AWAY!
     Copy-Item -Force -Path `
-        "${PSScriptRoot}\BuildX64\client\RelWithDebInfo\odamex.pdb" `
+        "${CurrentDir}\BuildX64\client\RelWithDebInfo\odamex.pdb" `
         -Destination "${OutputDir}\odamex-x64-${OdamexVersion}.pdb"
     Copy-Item -Force -Path `
-        "${PSScriptRoot}\BuildX64\server\RelWithDebInfo\odasrv.pdb" `
+        "${CurrentDir}\BuildX64\server\RelWithDebInfo\odasrv.pdb" `
         -Destination "${OutputDir}\odasrv-x64-${OdamexVersion}.pdb"
     Copy-Item -Force -Path `
-        "${PSScriptRoot}\BuildX64\odalaunch\RelWithDebInfo\odalaunch.pdb" `
+        "${CurrentDir}\BuildX64\odalaunch\RelWithDebInfo\odalaunch.pdb" `
         -Destination "${OutputDir}\odalaunch-x64-${OdamexVersion}.pdb"
 
     Copy-Item -Force -Path `
-        "${PSScriptRoot}\BuildX86\client\RelWithDebInfo\odamex.pdb" `
+        "${CurrentDir}\BuildX86\client\RelWithDebInfo\odamex.pdb" `
         -Destination "${OutputDir}\odamex-x86-${OdamexVersion}.pdb"
     Copy-Item -Force -Path `
-        "${PSScriptRoot}\BuildX86\server\RelWithDebInfo\odasrv.pdb" `
+        "${CurrentDir}\BuildX86\server\RelWithDebInfo\odasrv.pdb" `
         -Destination "${OutputDir}\odasrv-x86-${OdamexVersion}.pdb"
     Copy-Item -Force -Path `
-        "${PSScriptRoot}\BuildX86\odalaunch\RelWithDebInfo\odalaunch.pdb" `
+        "${CurrentDir}\BuildX86\odalaunch\RelWithDebInfo\odalaunch.pdb" `
         -Destination "${OutputDir}\odalaunch-x86-${OdamexVersion}.pdb"
 
     7z.exe a `
