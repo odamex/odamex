5ffc43a7785391e77697c7e9a14f484d0684d4a6
bcahue <bcahue@users.noreply.github.com>
2022-08-25 16:58:19 -0400

    run pipeline on publish

--- ci/macos-buildgen.sh
+++ ci/macos-buildgen.sh
@@ -8,6 +8,7 @@ IFS=$'\n\t'
 set -x
 
 # Install packages
+# Ensure to install sdl 2.0.22, as Homebrew latest is 2.24.0
 curl https://raw.githubusercontent.com/Homebrew/homebrew-core/3a9d188a695a38244c42e68117aff412ae0884eb/Formula/sdl2.rb > $(find $(brew --repository) -name sdl2.rb) && brew install sdl2
 brew install sdl2_mixer wxmac
 
