3a991efd37d92d19042bc17d6eab3b967402be01
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2022-10-04 18:35:28 -0400

    check for SDL 2.0.16

--- client/sdl/i_sdl.h
+++ client/sdl/i_sdl.h
@@ -27,6 +27,9 @@
 
 #if (SDL_MAJOR_VERSION == 2)
 	#define SDL20
+	#if (SDL_MINOR_VERSION > 0 || SDL_PATCHLEVEL >= 16)
+		#define SDL2016
+	#endif
 #elif (SDL_MAJOR_VERSION == 1 && SDL_MINOR_VERSION == 2)
 	#define SDL12
 #endif
