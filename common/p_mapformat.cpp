5366abe3b162dfddbbf03f8a897ffcf9cc86347f
bcahue <bcahue@users.noreply.github.com>
2022-10-23 20:21:28 -0400

    set and remove MF2_PASSMOBJ as needed if MF_COUNTKILL is detected

--- common/p_mapformat.cpp
+++ common/p_mapformat.cpp
@@ -62,13 +62,13 @@ void P_MigrateActorInfo(void)
 		for (i = 0; i < NUMMOBJTYPES; ++i)
 		{
 			if (mobjinfo[i].flags & MF_COUNTKILL)
-				mobjinfo[i].flags2 |= MF2_MCROSS | MF2_PUSHWALL;
+				mobjinfo[i].flags2 |= MF2_MCROSS | MF2_PUSHWALL | MF2_PASSMOBJ;
 
 			if (mobjinfo[i].flags & MF_MISSILE)
 				mobjinfo[i].flags2 |= MF2_PCROSS | MF2_IMPACT;
 		}
 
-		mobjinfo[MT_SKULL].flags2 |= MF2_MCROSS | MF2_PUSHWALL;
+		mobjinfo[MT_SKULL].flags2 |= MF2_MCROSS | MF2_PUSHWALL | MF2_PASSMOBJ;
 		mobjinfo[MT_PLAYER].flags2 |= MF2_WINDTHRUST | MF2_PUSHWALL;
 	}
 	else if (!map_format.getZDoom() && migrated)
@@ -78,13 +78,13 @@ void P_MigrateActorInfo(void)
 		for (i = 0; i < NUMMOBJTYPES; ++i)
 		{
 			if (mobjinfo[i].flags & MF_COUNTKILL)
-				mobjinfo[i].flags2 &= ~(MF2_MCROSS | MF2_PUSHWALL);
+				mobjinfo[i].flags2 &= ~(MF2_MCROSS | MF2_PUSHWALL | MF2_PASSMOBJ);
 
 			if (mobjinfo[i].flags & MF_MISSILE)
 				mobjinfo[i].flags2 &= ~(MF2_PCROSS | MF2_IMPACT);
 		}
 
-		mobjinfo[MT_SKULL].flags2 &= ~(MF2_MCROSS | MF2_PUSHWALL);
+		mobjinfo[MT_SKULL].flags2 &= ~(MF2_MCROSS | MF2_PUSHWALL | MF2_PASSMOBJ);
 		mobjinfo[MT_PLAYER].flags2 &= ~(MF2_WINDTHRUST | MF2_PUSHWALL);
 	}
 }
