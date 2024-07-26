df655141ac66a7a32f63e178fd43dfe3542801de
bcahue <bcahue@users.noreply.github.com>
2024-07-26 01:22:30 -0400

    fix formatting

--- common/p_tick.cpp
+++ common/p_tick.cpp
@@ -51,16 +51,13 @@ void P_Ticker (void)
 {
 #ifdef CLIENT_APP
 	// Game pauses when in the menu and not online/demo
-		if (paused || (!multiplayer && !demoplayback &&
-			(menuactive || ConsoleState == c_down || ConsoleState == c_falling)) &&
-			(players.begin()->viewz != 1)) // Render the first tic to get proper viewheight
-		{
-			return;
-		}
-#endif
-
-	if(paused)
+	if (paused || (!multiplayer && !demoplayback &&
+		(menuactive || ConsoleState == c_down || ConsoleState == c_falling)) &&
+		(players.begin()->viewz != 1)) // Render the first tic to get proper viewheight
+	{
 		return;
+	}
+#endif
 
 	if (serverside)
 		P_RunHordeTics();
