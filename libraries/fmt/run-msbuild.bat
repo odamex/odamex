60fd4b9c6aadbecc5325a8b9e303fa0e4a548318
Lexi Mayfield <me@leximayfield.com>
2024-08-25 13:39:47 -0400

    Remove all submodules and in-tree deps

--- libraries/fmt/run-msbuild.bat
+++
@@ -1,3 +0,0 @@
-
-    
-    C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/amd64/MSBuild.exe -p:FrameworkPathOverride="C:\Program Files\Reference Assemblies\Microsoft\Framework;.NETFramework\v4.0" %*
