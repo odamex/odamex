e536fc2fb854686a3456305ff0d3086d077c4848
bcahue <bcahue@users.noreply.github.com>
2024-08-30 22:58:56 -0400

    place deutex in /wad to have cmake compile it (covers non-debian distros)

--- packaging/flatpak/flatpak-buildgen.sh
+++ packaging/flatpak/flatpak-buildgen.sh
@@ -8,6 +8,9 @@ set -e
 # Echo all commands for debug purposes
 set -x
 
+# Untar deutex to wad directory
+tar --zstd -xvf deutex-5.2.2.tar.zst -C wad/
+
 # Generate build
 if [[ -z ${USE_SDL12:-} ]]; then
     cmake . -GNinja \
@@ -21,14 +24,77 @@ else
         -DBUILD_MASTER=0 -DBUILD_LAUNCHER=0 -DUSE_INTERNAL_LIBS=1
 fi
 
-# No spaces in project name.
+ninja odamex
+ninja odasrv
+
+# Assemble Flatpak assets
+
+# Client app
 projectName=Odamex.Client
 projectId=net.odamex.Odamex.Client
 executableName=odamex
 
-# Assemble Flatpak assets
-# Copy the portable app to the Flatpak-based location.
-cp -r $projectName /app/
+# Copy the client app to the Flatpak-based location.
+mkdir -p /app/$projectName
+cp -r client/$executableName /app/$projectName/
+chmod +x /app/$projectName/$executableName
+mkdir -p /app/bin
+cp -r wad/odamex.wad /app/$projectName/
+cp -r wad/odamex.wad /app/bin/
+ln -s /app/$projectName/$executableName /app/bin/$executableName
+
+# Install the icon.
+iconDir=/app/share/icons/hicolor/512x512/apps
+mkdir -p $iconDir
+cp -r ./media/icon_odamex_512.png $iconDir/$projectId.png
+
+# Install the desktop file.
+desktopFileDir=/app/share/applications
+mkdir -p $desktopFileDir
+cp -r packaging/linux/$projectId.desktop $desktopFileDir/
+
+# Install the AppStream metadata file.
+metadataDir=/app/share/metainfo
+mkdir -p $metadataDir
+cp -r packaging/linux/$projectId.metainfo.xml $metadataDir/
+
+# Server app
+projectName=Odamex.Server
+projectId=net.odamex.Odamex.Server
+executableName=odasrv
+
+# Copy the client app to the Flatpak-based location.
+mkdir -p /app/$projectName
+cp -r server/$executableName /app/$projectName/
+chmod +x /app/$projectName/$executableName
+mkdir -p /app/bin
+cp -r wad/odamex.wad /app/$projectName/
+cp -r wad/odamex.wad /app/bin/
+ln -s /app/$projectName/$executableName /app/bin/$executableName
+
+# Install the icon.
+iconDir=/app/share/icons/hicolor/512x512/apps
+mkdir -p $iconDir
+cp -r ./media/icon_odasrv_512.png $iconDir/$projectId.png
+
+# Install the desktop file.
+desktopFileDir=/app/share/applications
+mkdir -p $desktopFileDir
+cp -r packaging/linux/$projectId.desktop $desktopFileDir/
+
+# Install the AppStream metadata file.
+metadataDir=/app/share/metainfo
+mkdir -p $metadataDir
+cp -r packaging/linux/$projectId.metainfo.xml $metadataDir/
+
+# Launcher app
+projectName=Odamex.Launcher
+projectId=net.odamex.Odamex.Launcher
+executableName=odalaunch
+
+# Copy the client app to the Flatpak-based location.
+mkdir -p /app/$projectName
+cp -r client/$executableName /app/$projectName/
 chmod +x /app/$projectName/$executableName
 mkdir -p /app/bin
 ln -s /app/$projectName/$executableName /app/bin/$executableName
@@ -36,7 +102,7 @@ ln -s /app/$projectName/$executableName /app/bin/$executableName
 # Install the icon.
 iconDir=/app/share/icons/hicolor/512x512/apps
 mkdir -p $iconDir
-cp -r ./media/icon_odamex_512.png $iconDir/$projectId.png
+cp -r ./media/icon_odalaunch_512.png $iconDir/$projectId.png
 
 # Install the desktop file.
 desktopFileDir=/app/share/applications
