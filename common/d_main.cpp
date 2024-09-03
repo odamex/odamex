1eb840e8e3dddd1aff294d4e0be41ed9f3c9cd0a
bcahue <bcahue@users.noreply.github.com>
2024-09-02 23:57:29 -0400

    add building flatpak to the ci, add maintainer data dir to odamex client

--- common/d_main.cpp
+++ common/d_main.cpp
@@ -359,6 +359,10 @@ void D_AddPlatformSearchDirs(std::vector<std::string> &dirs)
 	D_AddSearchDir(dirs, INSTALL_PREFIX "/" INSTALL_DATADIR "/odamex", separator);
 	D_AddSearchDir(dirs, INSTALL_PREFIX "/" INSTALL_DATADIR "/games/odamex", separator);
 	#endif
+	// Search the maintainer-directed data directory for WADs
+	#if defined(ODAMEX_INSTALL_DATADIR)
+	D_AddSearchDir(dirs, ODAMEX_INSTALL_DATADIR, separator);
+	#endif
 
 	D_AddSearchDir(dirs, "/usr/share/doom", separator);
 	D_AddSearchDir(dirs, "/usr/share/games/doom", separator);
