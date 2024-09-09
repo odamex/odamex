83596910b42e39d30e8ac2aad5700eabacadb73f
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2024-09-08 19:05:06 -0500

    Cleanup unnecessary changes

--- client/src/cl_level.cpp
+++ client/src/cl_level.cpp
@@ -71,7 +71,6 @@ EXTERN_CVAR(sv_monstersrespawn)
 EXTERN_CVAR(sv_gravity)
 EXTERN_CVAR(sv_aircontrol)
 EXTERN_CVAR(g_resetinvonexit)
-EXTERN_CVAR(g_thingfilter)
 
 // Start time for timing demos
 dtime_t starttime;
@@ -145,7 +144,7 @@ BEGIN_COMMAND (wad) // denis - changes wads
 	CL_QuitNetGame(NQ_SILENT);
 	S_StopMusic();
 	currentmusic = gameinfo.titleMusic.c_str();
-
+	
 	S_StartMusic(currentmusic.c_str());
 }
 END_COMMAND (wad)
@@ -158,7 +157,7 @@ EXTERN_CVAR(sv_allowjump)
 
 //
 // G_DoNewGame
-// Is called whenever a new Singleplayer game will be started.
+// Is called whenever a new Singleplayer game will be started. 
 //
 void G_DoNewGame (void)
 {
@@ -292,7 +291,7 @@ void G_InitNew (const char *mapname)
 	viewactive = true;
 
 	D_SetupUserInfo();
-
+	
 	level.mapname = mapname;
 
 	// [AM}] WDL stats (for testing purposes)
@@ -688,7 +687,7 @@ void G_WorldDone()
 	// Sort out default options to pass to F_StartFinale
 	finale_options_t options = { 0 };
 	options.music = !level.intermusic.empty() ? level.intermusic.c_str() : thiscluster.messagemusic.c_str();
-
+	
 	if (!level.interbackdrop.empty())
 	{
 		options.flat = level.interbackdrop.c_str();
@@ -701,7 +700,7 @@ void G_WorldDone()
 	{
 		options.flat = &thiscluster.finaleflat[0];
 	}
-
+	
 	if (secretexit)
 	{
 		options.text = (!level.intertextsecret.empty()) ? level.intertextsecret.c_str() : thiscluster.exittext;
