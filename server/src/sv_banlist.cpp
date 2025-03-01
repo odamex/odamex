fe960715d4371da3f290dc4e99868adc0c37f837
Lexi Mayfield <me@leximayfield.com>
2025-02-28 22:23:17 -0500

    Bump "easy" dependencies using vcpkg

--- server/src/sv_banlist.cpp
+++ server/src/sv_banlist.cpp
@@ -500,14 +500,14 @@ bool Banlist::json_replace(const Json::Value &json_bans)
 		Json::Value value;
 
 		// Range
-		value = (*it).get("range", Json::Value::null);
+		value = (*it).get("range", Json::Value{});
 		if (value.isNull())
 			continue;
 		else
 			ban.range.set((*it).get("range", false).asString());
 
 		// Expire time
-		value = (*it).get("expire", Json::Value::null);
+		value = (*it).get("expire", Json::Value{});
 		if (!value.isNull())
 		{
 			if (StrParseISOTime(value.asString(), &tmp))
@@ -515,12 +515,12 @@ bool Banlist::json_replace(const Json::Value &json_bans)
 		}
 
 		// Name
-		value = (*it).get("name", Json::Value::null);
+		value = (*it).get("name", Json::Value{});
 		if (!value.isNull())
 			ban.name = value.asString();
 
 		// Reason
-		value = (*it).get("reason", Json::Value::null);
+		value = (*it).get("reason", Json::Value{});
 		if (!value.isNull())
 			ban.reason = value.asString();
 
