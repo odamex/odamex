7f88b7d81792ad55739d2ae62a8d0e25916df598
Lexi Mayfield <me@leximayfield.com>
2025-03-01 23:50:26 -0500

    Attempt to fix ci for Windows

--- ci/win-buildgen.ps1
+++ ci/win-buildgen.ps1
@@ -1,11 +1,14 @@
 Set-PSDebug -Trace 1
 
+& .\vcpkg\bootstrap-vcpkg.bat
+
 mkdir build | Out-Null
 Set-Location build
 
 & cmake .. -G "Visual Studio 17 2022" `
     -DBUILD_OR_FAIL=1 `
     -DBUILD_CLIENT=1 -DBUILD_SERVER=1 `
-    -DBUILD_MASTER=1 -DBUILD_LAUNCHER=1
+    -DBUILD_MASTER=1 -DBUILD_LAUNCHER=1 `
+    -DCMAKE_TOOLCHAIN_FILE: "../vcpkg/scripts/buildsystems/vcpkg.cmake"
 
 Set-Location ..
