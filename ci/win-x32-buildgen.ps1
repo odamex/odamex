f01f8ca2d059cca3062b78923c3d1a9df0249ccf
Lexi Mayfield <me@leximayfield.com>
2025-03-02 00:32:06 -0500

    Build 32-bit windows with preset

--- ci/win-x32-buildgen.ps1
+++ ci/win-x32-buildgen.ps1
@@ -1,9 +1,5 @@
 Set-PSDebug -Trace 1
 
-mkdir "build-x32" | Out-Null
-Set-Location "build-x32"
+& .\vcpkg\bootstrap-vcpkg.bat
 
-& cmake .. `
-    -G "Visual Studio 17 2022" -A Win32 -DBUILD_ODALAUNCH=0 `
-
-Set-Location ..
+& cmake --preset "github-win-x32"
