d59a9345c0f680b408b1cea5b3e7eab0b8450018
Mia McMahill <miadawnmc@gmail.com>
2025-12-27 17:13:08 -0600

    Get tests passing again

--- tests/unit-tests/test/t_infomap.cpp
+++ tests/unit-tests/test/t_infomap.cpp
@@ -1,22 +1,30 @@
 #include "gtest/gtest.h"
 #include "odamex.h"
 #include "infomap.h"
+#include "d_main.h"
 
-TEST(InfoMap, NameToMobj) {
+class InfoMapTest : public ::testing::Test {
+protected:
+	void SetUp() override {
+		D_InitializeDoomObjectTables();
+	}
+};
+
+TEST_F(InfoMapTest, NameToMobj) {
 	EXPECT_EQ(P_NameToMobj("Revenant"), MT_UNDEAD);
 	EXPECT_EQ(P_NameToMobj("revenant"), MT_NULL);
 }
 
-TEST(InfoMap, INameToMobj) {
+TEST_F(InfoMapTest, INameToMobj) {
 	EXPECT_EQ(P_INameToMobj("Revenant"), MT_UNDEAD);
 	EXPECT_EQ(P_INameToMobj("revenant"), MT_UNDEAD);
 }
 
-TEST(InfoMap, MobjToName) {
+TEST_F(InfoMapTest, MobjToName) {
 	EXPECT_EQ(P_MobjToName(MT_UNDEAD), std::string("Revenant"));
 }
 
-TEST(InfoMap, NameToWeapon) {
+TEST_F(InfoMapTest, NameToWeapon) {
 	EXPECT_EQ(P_NameToWeapon("PlasmaRifle"), wp_plasma);
 	EXPECT_EQ(P_NameToWeapon("plasmarifle"), wp_none);
 }
