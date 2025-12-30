aff79883e1ba993e0dde33dcfeff07655077c1b1
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2025-12-29 21:17:04 -0600

    Fix crash from freeing szps in visplanes

--- common/r_defs.h
+++ common/r_defs.h
@@ -769,7 +769,7 @@ struct visplane_s
 	shaderef_t	colormap;			// [RH] Support multiple colormaps
 	fixed_t		xscale, yscale;		// [RH] Support flat scaling
 	angle_t		angle;				// [RH] Support flat rotation
-	AActor::AActorPtr skybox;
+	AActor*		skybox;
 
 	unsigned int *bottom;			// [RH] bottom and top arrays are dynamically
 	unsigned int pad;				//		allocated immediately after the
