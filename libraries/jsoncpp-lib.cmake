7d5c10b7022ebf39a18db748f9f97ad5cbd2e9b4
Mike Lightner <fuzztooth@gmail.com>
2026-01-07 12:01:29 -0600

    Make sure jsoncpp is linked correctly

--- libraries/jsoncpp-lib.cmake
+++ libraries/jsoncpp-lib.cmake
@@ -13,12 +13,27 @@ if((BUILD_CLIENT OR BUILD_SERVER) AND USE_INTERNAL_JSONCPP)
   lib_build(LIBRARY jsoncpp)
 
   find_package(jsoncpp REQUIRED)
+  set(_JSONCPP_INCLUDE_DIR "${CMAKE_CURRENT_BINARY_DIR}/local/include/jsoncpp")
   if(TARGET jsoncpp_static)
     set_target_properties(jsoncpp_static PROPERTIES IMPORTED_GLOBAL TRUE)
+    set_property(TARGET jsoncpp_static APPEND PROPERTY
+      INTERFACE_INCLUDE_DIRECTORIES "${_JSONCPP_INCLUDE_DIR}")
   elseif(TARGET jsoncpp_lib_static)
     set_target_properties(jsoncpp_lib_static PROPERTIES IMPORTED_GLOBAL TRUE)
+    set_property(TARGET jsoncpp_lib_static APPEND PROPERTY
+      INTERFACE_INCLUDE_DIRECTORIES "${_JSONCPP_INCLUDE_DIR}")
+    add_library(jsoncpp_static INTERFACE IMPORTED GLOBAL)
+    set_target_properties(jsoncpp_static PROPERTIES
+      INTERFACE_LINK_LIBRARIES jsoncpp_lib_static
+      INTERFACE_INCLUDE_DIRECTORIES "${_JSONCPP_INCLUDE_DIR}")
   elseif(TARGET jsoncpp_lib)
     set_target_properties(jsoncpp_lib PROPERTIES IMPORTED_GLOBAL TRUE)
+    set_property(TARGET jsoncpp_lib APPEND PROPERTY
+      INTERFACE_INCLUDE_DIRECTORIES "${_JSONCPP_INCLUDE_DIR}")
+    add_library(jsoncpp_static INTERFACE IMPORTED GLOBAL)
+    set_target_properties(jsoncpp_static PROPERTIES
+      INTERFACE_LINK_LIBRARIES jsoncpp_lib
+      INTERFACE_INCLUDE_DIRECTORIES "${_JSONCPP_INCLUDE_DIR}")
   else()
     message(FATAL_ERROR "JsonCpp target not found.")
   endif()
