3617bb648e9df90ae93d0740941a9cb75bde99a6
Mike Lightner <fuzztooth@gmail.com>
2026-01-07 10:40:03 -0600

    Fix internal zlib/libpng builds on macOS

--- libraries/zlib-lib.cmake
+++ libraries/zlib-lib.cmake
@@ -16,23 +16,47 @@ if(BUILD_CLIENT OR BUILD_SERVER)
     endif()
 
     # Generate the build.
-    execute_process(COMMAND "${CMAKE_COMMAND}"
+    set(_ZLIB_GEN_ARGS
       -S "${CMAKE_CURRENT_SOURCE_DIR}/zlib"
       -B "${CMAKE_CURRENT_BINARY_DIR}/zlib-build"
       -G "${CMAKE_GENERATOR}"
-      -A "${CMAKE_GENERATOR_PLATFORM}"
-      -T "${CMAKE_GENERATOR_TOOLSET}"
-      "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}"
-      "-DCMAKE_LINKER=${CMAKE_LINKER}"
-      "-DCMAKE_RC_COMPILER=${CMAKE_RC_COMPILER}"
       "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
       "-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/local")
-    unset(_COMPILE_CURL_WINSSL)
+    if(CMAKE_GENERATOR_PLATFORM)
+      list(APPEND _ZLIB_GEN_ARGS -A "${CMAKE_GENERATOR_PLATFORM}")
+    endif()
+    if(CMAKE_GENERATOR_TOOLSET)
+      list(APPEND _ZLIB_GEN_ARGS -T "${CMAKE_GENERATOR_TOOLSET}")
+    endif()
+    if(CMAKE_C_COMPILER)
+      list(APPEND _ZLIB_GEN_ARGS "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
+    endif()
+    if(CMAKE_LINKER)
+      list(APPEND _ZLIB_GEN_ARGS "-DCMAKE_LINKER=${CMAKE_LINKER}")
+    endif()
+    if(CMAKE_RC_COMPILER)
+      list(APPEND _ZLIB_GEN_ARGS "-DCMAKE_RC_COMPILER=${CMAKE_RC_COMPILER}")
+    endif()
+    if(CMAKE_OSX_ARCHITECTURES)
+      list(APPEND _ZLIB_GEN_ARGS "-DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}")
+    endif()
+    if(CMAKE_OSX_DEPLOYMENT_TARGET)
+      list(APPEND _ZLIB_GEN_ARGS "-DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+    endif()
+
+    execute_process(COMMAND "${CMAKE_COMMAND}" ${_ZLIB_GEN_ARGS} RESULT_VARIABLE _RESVAL)
+    if(_RESVAL GREATER 0)
+      message(FATAL_ERROR "Library zlib did not generate correctly.")
+    endif()
 
     # Compile the library.
     execute_process(COMMAND "${CMAKE_COMMAND}"
       --build "${CMAKE_CURRENT_BINARY_DIR}/zlib-build"
-      --config RelWithDebInfo --target install)
+      --config RelWithDebInfo --target install
+      RESULT_VARIABLE _RESVAL)
+    if(_RESVAL GREATER 0)
+      message(FATAL_ERROR "Library zlib did not build correctly.")
+    endif()
   endif()
 
   find_package(ZLIB)
