4643c7a61bd17b219c8ffca92321ca3c8d38b8e6
Jim Thoenen <jjt@pobox.com>
2026-01-16 19:16:21 -0600

    Fix spy angle interpolation - Ensure that the spied player's previous and current angles are actually snapshot-consecutive

--- client/src/cl_pred.cpp
+++ client/src/cl_pred.cpp
@@ -189,10 +189,30 @@ static void CL_PredictSpying()
 	if (consoleplayer_id == displayplayer_id)
 		return;
 
+	// Save and restore the prevangle and prevpitch so that the client viewangle interpolation
+	// actually works when spying.
+	//
+	// Details:
+	// As part of normal operations, P_PlayerThink overwrites prevangle and prevpitch with
+	// current angle and pitch, assuming that a newer angle and pitch are coming in P_MovePlayer
+	// under control of player->cmd.yaw and player->cmd.pitch.  The end result would be fresh
+	// previous and current values, which can be interpolated for nice smooth animation.
+	//
+	// Spying adds a wrinkle in that angle and pitch are *already* as up-to-date as can be,
+	// thanks to CL_SimulatePlayers, so P_PlayerThink's assumption that newer angle values are
+	// incoming doesn't hold.  However, prevangle and prevpitch genuinely have the previous
+	// snapshot's values, so hold onto them and write them back.  The end result will be the
+	// current and the previous snapshots' values reflected in their respective attributes.
+	const auto prevangle = player->mo->prevangle;
+	const auto prevpitch = player->mo->prevpitch;
+
 	predicting = false;
 
 	P_PlayerThink(player);
 	P_CalcHeight(player);
+
+	player->mo->prevangle = prevangle;
+	player->mo->prevpitch = prevpitch;
 }
 
 //
