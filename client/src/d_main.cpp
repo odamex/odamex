6d9849d2882743db67e9587fb5fbf3c14b57a774
Lotus <109847465+LotusOpenclaw@users.noreply.github.com>
2026-03-04 16:58:22 -0600

    odaheretic: keep client startmap assignment minimal

--- client/src/d_main.cpp
+++ client/src/d_main.cpp
@@ -1020,9 +1020,7 @@ void D_DoomMain()
 		g_thingfilter = -1;
 
 	// get skill / episode / map from parms
-	startmap = !EpisodeMaps[0].empty()
-	               ? EpisodeMaps[0]
-	               : ((gameinfo.flags & GI_MAPxx) ? "MAP01" : "E1M1");
+	startmap = EpisodeMaps[0];
 
 	const char* val = Args.CheckValue("-skill");
 	if (val)
