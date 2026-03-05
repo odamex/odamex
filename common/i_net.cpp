9db20a95a9c3af6fbab5cc76a23c313cd1674e3e
Jim Thoenen <jjt@pobox.com>
2026-03-05 16:16:18 -0600

    Initial Windows build with statically linked dependency libraries built via FetchContent

--- common/i_net.cpp
+++ common/i_net.cpp
@@ -77,9 +77,15 @@ typedef int SOCKET;
 #include "minilzo.h"
 
 #ifdef ODA_HAVE_MINIUPNP
-#include "miniupnpc/miniwget.h"
-#include "miniupnpc/miniupnpc.h"
-#include "miniupnpc/upnpcommands.h"
+#   ifdef INTERNAL_MINIUPNP_STATICLIB
+#       include "miniwget.h"
+#       include "miniupnpc.h"
+#       include "upnpcommands.h"
+#   else
+#       include "miniupnpc/miniwget.h"
+#       include "miniupnpc/miniupnpc.h"
+#       include "miniupnpc/upnpcommands.h"
+#   endif
 #endif
 
 unsigned int	inet_socket;
