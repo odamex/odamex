4e11e691c6122ed4f328564bcf75622907b9d4ec
endless-r0ad <24723883+endless-r0ad@users.noreply.github.com>
2026-04-19 16:16:07 -0400

    dont move toward target if it was destroyed

--- common/p_enemy.cpp
+++ common/p_enemy.cpp
@@ -485,8 +485,13 @@ bool P_SmartMove(AActor* actor)
 	AActor* target = actor->target;
 	int dropoff = 0;
 
+	if (target->WasDestroyed())
+	{
+		return false;
+	}
+
 	/* killough 9/12/98: Stay on a lift if target is on one */
-	bool on_lift = co_staylift && target && target->health > 0 && target->subsector &&
+	bool on_lift = co_staylift && target && target->health > 0 &&
 	               target->subsector->sector->tag == actor->subsector->sector->tag &&
 	               P_IsOnLift(actor);
 
