1ef2bfcc218d583206b0c8a8e4c049a9dfc39aab
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2026-04-24 23:49:16 -0500

    Set output flags on output handle, rather than on input handle

--- server/src/i_main.cpp
+++ server/src/i_main.cpp
@@ -152,15 +152,32 @@ int __cdecl main(int argc, char *argv[])
 
 		DWORD consoleMode = 0;
 
+		// TODO: don't just throw and exit if these fail,
+		// that prevents being able to pipe output
+		// and makes it so you can't start a server in github actions or vscode debugger
 		if (!GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), &consoleMode))
-			throw CDoomError("GetConsoleMode failed!\n");
+			throw CDoomError("GetConsoleMode (input) failed!\n");
 
 		consoleMode &= ~ENABLE_QUICK_EDIT_MODE;
 		consoleMode |= ENABLE_EXTENDED_FLAGS;
-		consoleMode |= ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 
 		if (!SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), consoleMode))
-			throw CDoomError("SetConsoleMode failed!\n");
+			throw CDoomError("SetConsoleMode (input) failed!\n");
+
+		// enable ansi color escape processing
+
+		if (!GetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), &consoleMode))
+			throw CDoomError("GetConsoleMode (output) failed!");
+
+		const DWORD originalOutputMode = consoleMode;
+		consoleMode |= ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+		if (!SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), consoleMode))
+		{
+			// we might just be on a version of windows before support was added, try setting mode back
+			if (!SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), originalOutputMode))
+				throw CDoomError("SetConsoleMode (OUTPUT) failed!");
+		}
 
 		// Fixes icon not showing in titlebar and alt-tab menu under windows 7
 		HANDLE hIcon;
