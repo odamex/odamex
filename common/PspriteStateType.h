0405ff8a459af1eb87cb7252ebeca380eca184c3
Jim Thoenen <jjt@pobox.com>
2026-05-05 08:07:34 -0500

    Split out Psprite item type into its own file - sidesteps an inclusion order issue

---
+++ common/PspriteStateType.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "p_pspr.h"
+
+struct PspriteStateType
+{
+	statenum_t  statenum;
+	int         tics;
+	//fixed_t     sx      { 0};
+	//fixed_t     sy      { 0};
+
+    PspriteStateType() :
+        statenum{-1},
+        tics    {-1}
+    {
+    }
+
+    PspriteStateType(statenum_t i_statenum, int i_tics) :
+        statenum{i_statenum},
+        tics    {i_tics}
+    {
+    }
+    explicit PspriteStateType(const state_t& stateDef) :
+        statenum{stateDef.statenum},
+        tics    {stateDef.tics}
+    {
+    }
+
+    explicit PspriteStateType(const pspdef_t& pspdef) :
+        statenum(pspdef.state ? pspdef.state->statenum : static_cast<statenum_t>(-1)),
+        tics    (pspdef.tics)
+    {
+    }
+
+    bool operator==(const PspriteStateType&) const = default;
+
+    bool operator==(const pspdef_t& pspdef) const       { return *this == PspriteStateType(pspdef); }
+    PspriteStateType& operator=(const pspdef_t& pspdef) { return *this = PspriteStateType(pspdef);  }
+
+	template <typename StreamType>
+	friend StreamType& operator<<(StreamType& io_stream, const PspriteStateType& i_thisRef)
+	{
+		io_stream
+		    << i_thisRef.statenum
+		    << i_thisRef.tics
+		    ;
+		return io_stream;
+	}
+
+	template <typename StreamType>
+	friend StreamType& operator>>(StreamType& io_stream, PspriteStateType& o_thisRef)
+	{
+		io_stream
+		    >> o_thisRef.statenum
+		    >> o_thisRef.tics
+		    ;
+		return io_stream;
+	}
+
+    PspriteStateType Next() const
+    {
+        // non-existent or frozen psprites have themselves as the next in the sequence.
+        if (statenum == -1 or tics == -1)
+        {
+            return *this;
+        }
+
+        // Now proceed for valid states.  tics == 0 is invalid because we never
+        // "stop" at a 0-tics state - we only slide through them until we get a state that
+        // has a non-zero tic, even -1.
+        auto iter = ::states.find(statenum);
+        if (iter != ::states.end() and tics != 0)
+        {
+            int nextTic = tics - 1;
+            while (nextTic == 0)
+            {
+                iter = ::states.find(iter->second.nextstate);
+                if (iter == ::states.end())
+                {
+                    // bail out and return default if the nextstate is invalid.  Should never happen, but
+                    // a bad dehacked patch can do it.
+                    return PspriteStateType{};
+                }
+                nextTic = iter->second.tics;
+            }
+
+            return PspriteStateType {iter->second.statenum, nextTic};
+        }
+        return PspriteStateType{};
+    }
+};
