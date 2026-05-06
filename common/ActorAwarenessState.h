1d7a05600ba06fc28006590b84cae8ffc256f019
Jim Thoenen <jjt@pobox.com>
2026-05-06 16:15:11 -0500

    Initial Awareness state utility

---
+++ common/ActorAwarenessState.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <functional>
+#include <unordered_map>
+#include <utility>
+
+enum class AwarenessEnum
+{
+    NOT_AWARE = 0,
+    FULLY_AWARE,
+    SEMI_AWARE,
+    BARELY_AWARE,
+};
+
+template <size_t MAX_PLAYER_COUNT>
+class ActorAwarenessState
+{
+    public:
+        ActorAwarenessState() :
+            m_player(MAX_PLAYER_COUNT)
+        {
+        }
+
+        AwarenessEnum Get(size_t playerId) const
+        {
+            return m_player[playerId];
+        }
+
+        AwarenessEnum Set(size_t playerId, AwarenessEnum awareness)
+        {
+            return std::exchange(m_player[playerId], awareness);
+        }
+
+    protected:
+
+        // We use an unordered_map configured to be a table so that, if by some crazy
+        // error, we get fed player IDs > 255, this continues to just work and be
+        // constant-time access.
+        std::unordered_map<size_t, AwarenessEnum, std::identity> m_player;
+};
