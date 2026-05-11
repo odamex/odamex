f040bf46194df6cc587053f24186f99519471f00
Jim Thoenen <jjt@pobox.com>
2026-05-11 16:41:25 -0500

    Make the HordeInfo message reliable because it could be missed at a critical time otherwise, like new map

--- server/src/sv_main.cpp
+++ server/src/sv_main.cpp
@@ -3170,7 +3170,7 @@ void SV_UpdateGametype(player_t& player)
 		if (player.hordeInfo != info)
 		{
 			player.hordeInfo = info;
-			MSG_WriteSVC(player.client.messenger.NetBuf(), SVC_HordeInfo(info));
+			MSG_WriteSVC(player.client.messenger.ReliableBuf(), SVC_HordeInfo(info));
 		}
 	}
 }
