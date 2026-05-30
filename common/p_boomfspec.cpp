dd0fe709b7fd297cb548c5ec85e7bbb1a38e632f
Mia McMahill <miadawnmc@gmail.com>
2026-05-30 14:54:30 -0500

    Add missing tag 0 zero for boom generalized linedefs

--- common/p_boomfspec.cpp
+++ common/p_boomfspec.cpp
@@ -105,14 +105,17 @@ bool P_CrossCompatibleSpecialLine(line_t* line, int side, AActor* thing,
 			if (!P_IsPlayerOrAvatar(*thing) && !bossaction)
 				if ((line->special & FloorChange) || !(line->special & FloorModel))
 					return false; // FloorModel is "Allow Monsters" if FloorChange is 0
+			if (!line->id) //jff 2/27/98 all walk generalized types require tag
+				return false;
 			linefunc = EV_DoGenFloor;
 		}
 		else if ((unsigned)line->special >= GenCeilingBase)
 		{
 			if (!P_IsPlayerOrAvatar(*thing) && !bossaction)
 				if ((line->special & CeilingChange) || !(line->special & CeilingModel))
-					return false; // CeilingModel is "Allow Monsters" if CeilingChange is
-					               // 0
+					return false; // CeilingModel is "Allow Monsters" if CeilingChange is 0
+			if (!line->id) //jff 2/27/98 all walk generalized types require tag
+				return false;
 			linefunc = EV_DoGenCeiling;
 		}
 		else if ((unsigned)line->special >= GenDoorBase)
@@ -124,6 +127,8 @@ bool P_CrossCompatibleSpecialLine(line_t* line, int side, AActor* thing,
 				if (line->flags & ML_SECRET) // they can't open secret doors either
 					return false;
 			}
+			if (!line->id) //3/2/98 move outside the monster check
+				return false;
 			linefunc = EV_DoGenDoor;
 		}
 		else if ((unsigned)line->special >= GenLockedBase)
@@ -146,6 +151,8 @@ bool P_CrossCompatibleSpecialLine(line_t* line, int side, AActor* thing,
 			if (!P_IsPlayerOrAvatar(*thing) && !bossaction)
 				if (!(line->special & LiftMonster))
 					return false; // monsters disallowed
+			if (!line->id) //jff 2/27/98 all walk generalized types require tag
+				return false;
 			linefunc = EV_DoGenLift;
 		}
 		else if ((unsigned)line->special >= GenStairsBase)
@@ -153,6 +160,8 @@ bool P_CrossCompatibleSpecialLine(line_t* line, int side, AActor* thing,
 			if (!P_IsPlayerOrAvatar(*thing) && !bossaction)
 				if (!(line->special & StairMonster))
 					return false; // monsters disallowed
+			if (!line->id) //jff 2/27/98 all walk generalized types require tag
+				return false;
 			linefunc = EV_DoGenStairs;
 		}
 		else if ((unsigned)line->special >= GenCrusherBase)
@@ -162,6 +171,8 @@ bool P_CrossCompatibleSpecialLine(line_t* line, int side, AActor* thing,
 			if (!P_IsPlayerOrAvatar(*thing) && !bossaction)
 				if (!(line->special & StairMonster))
 					return false; // monsters disallowed
+			if (!line->id) //jff 2/27/98 all walk generalized types require tag
+				return false;
 			linefunc = EV_DoGenCrusher;
 		}
 
