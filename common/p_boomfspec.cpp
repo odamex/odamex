2f78ef23d651f6ea2a285c975552e83ce5124cdc
Mia McMahill <miadawnmc@gmail.com>
2026-05-30 14:58:11 -0500

    Add checks to shoot specials as well

--- common/p_boomfspec.cpp
+++ common/p_boomfspec.cpp
@@ -3406,6 +3406,8 @@ bool P_ShootCompatibleSpecialLine(AActor* thing, line_t* line)
 		if (!P_IsPlayerOrAvatar(*thing))
 			if ((line->special & FloorChange) || !(line->special & FloorModel))
 				return false; // FloorModel is "Allow Monsters" if FloorChange is 0
+		if (!line->id) //jff 2/27/98 all gun generalized types require tag
+			return false;
 		linefunc = EV_DoGenFloor;
 	}
 	else if ((unsigned)line->special >= GenCeilingBase)
@@ -3413,6 +3415,8 @@ bool P_ShootCompatibleSpecialLine(AActor* thing, line_t* line)
 		if (!P_IsPlayerOrAvatar(*thing))
 			if ((line->special & CeilingChange) || !(line->special & CeilingModel))
 				return false; // CeilingModel is "Allow Monsters" if CeilingChange is 0
+		if (!line->id) //jff 2/27/98 all gun generalized types require tag
+			return false;
 		linefunc = EV_DoGenCeiling;
 	}
 	else if ((unsigned)line->special >= GenDoorBase)
@@ -3424,6 +3428,8 @@ bool P_ShootCompatibleSpecialLine(AActor* thing, line_t* line)
 			if (line->flags & ML_SECRET) // they can't open secret doors either
 				return false;
 		}
+		if (!line->id) //jff 2/27/98 all gun generalized types require tag
+			return false;
 		linefunc = EV_DoGenDoor;
 	}
 	else if ((unsigned)line->special >= GenLockedBase)
@@ -3437,6 +3443,8 @@ bool P_ShootCompatibleSpecialLine(AActor* thing, line_t* line)
 				return false;
 		}
 		else
+			return false;
+		if (!line->id) //jff 2/27/98 all gun generalized types require tag
 			return false;
 		linefunc = EV_DoGenLockedDoor;
 	}
@@ -3452,6 +3460,8 @@ bool P_ShootCompatibleSpecialLine(AActor* thing, line_t* line)
 		if (!P_IsPlayerOrAvatar(*thing))
 			if (!(line->special & StairMonster))
 				return false; // monsters disallowed
+		if (!line->id) //jff 2/27/98 all gun generalized types require tag
+			return false;
 		linefunc = EV_DoGenStairs;
 	}
 	else if ((unsigned)line->special >= GenCrusherBase)
@@ -3459,6 +3469,8 @@ bool P_ShootCompatibleSpecialLine(AActor* thing, line_t* line)
 		if (!P_IsPlayerOrAvatar(*thing))
 			if (!(line->special & StairMonster))
 				return false; // monsters disallowed
+		if (!line->id) //jff 2/27/98 all gun generalized types require tag
+			return false;
 		linefunc = EV_DoGenCrusher;
 	}
 
