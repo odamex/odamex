e4e982ca7fa30aeb4d8a2cc5d647895f4ba2886f
Jim Thoenen <jjt@pobox.com>
2026-05-31 10:51:11 -0500

    Don't do an extra skip when encountering unknown dehacked lines - just let block processing continue normally

--- common/d_dehacked.cpp
+++ common/d_dehacked.cpp
@@ -677,8 +677,6 @@ static void HandleMode(std::string_view header, DehScanner& scanner)
 
 	// Handle unknown or unimplemented data
 	DPrintFmt("Unknown chunk {} encountered. Skipping.\n", header);
-	scanner.skipLine();
-	while (scanner.getNextKeyValue());
 }
 
 static bool HandleKey(nonstd::span<const Key> keys, void* structure, std::string_view key, int value,
