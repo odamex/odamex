f9a01107284806567cd8603d3b99b8ec51809092
Mia McMahill <miadawnmc@gmail.com>
2026-06-08 13:25:04 -0500

    Use exec in flatpak select-exe script so the shell process doesn't hang around

--- packaging/flatpak/select-exe.sh
+++ packaging/flatpak/select-exe.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 odamex_client() {
-    command odamex "$@"
+    exec odamex "$@"
 }
 
 odamex_server() {
-    command odasrv "$@"
+    exec odasrv "$@"
 }
 
 odamex_launcher() {
-    command odalaunch "$@"
+    exec odalaunch "$@"
 }
 
 if declare -f "odamex_$1" >/dev/null; then
