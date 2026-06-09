42c978e68716aa1b9b13bbe7c432c70c5c5e8bd8
Mia McMahill <miadawnmc@gmail.com>
2026-06-09 06:24:53 -0500

    Add file forwarding to shell wrappers

--- packaging/flatpak/posix_aliases.sh
+++ packaging/flatpak/posix_aliases.sh
@@ -1,17 +1,17 @@
 if ! command -v odamex > /dev/null 2>&1; then
     odamex() {
-        flatpak run net.odamex.Odamex client "$@"
+        flatpak run --file-forwarding net.odamex.Odamex client "$@"
     }
 fi
 
 if ! command -v odasrv > /dev/null 2>&1; then
     odasrv() {
-        flatpak run net.odamex.Odamex server "$@"
+        flatpak run --file-forwarding net.odamex.Odamex server "$@"
     }
 fi
 
 if ! command -v odalaunch > /dev/null 2>&1; then
     odalaunch() {
-        flatpak run net.odamex.Odamex launcher "$@"
+        flatpak run --file-forwarding net.odamex.Odamex launcher "$@"
     }
 fi
