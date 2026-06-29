7b6423e128bdac976c67058a7050c9f81369f422
Mia McMahill <miadawnmc@gmail.com>
2026-06-29 04:07:03 -0500

    Remove some unecessary externs. extern is never needed on functions

--- common/p_acs.cpp
+++ common/p_acs.cpp
@@ -229,10 +229,10 @@ static DoomEntity DoomDecorationNames[60] = {{"BurningBarrel", MT_MISC77},
                                              {"Column", MT_MISC31},
                                              {"ExplosiveBarrel", MT_BARREL}};
 
-extern ItemEquipVal P_GiveAmmo(player_t *player, ammotype_t ammo, float num);
-extern ItemEquipVal P_GiveWeapon(player_t *player, weapontype_t weapon, bool dropped);
-extern ItemEquipVal P_GiveCard(player_t *player, card_t card);
-extern ItemEquipVal P_GivePower(player_t *player, int  power);
+ItemEquipVal P_GiveAmmo(player_t *player, ammotype_t ammo, float num);
+ItemEquipVal P_GiveWeapon(player_t *player, weapontype_t weapon, bool dropped);
+ItemEquipVal P_GiveCard(player_t *player, card_t card);
+ItemEquipVal P_GivePower(player_t *player, int  power);
 
 mobjtype_t FindWeaponEntity(const char* type)
 {
