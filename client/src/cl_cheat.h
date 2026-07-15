326ed5b37337b3df660cf600b695f850ef352b23
Jim Thoenen <jjt@pobox.com>
2026-07-15 09:22:36 -0500

    Copyright and license statement

--- client/src/cl_cheat.h
+++ client/src/cl_cheat.h
@@ -1,3 +1,25 @@
+// Emacs style mode select   -*- C++ -*-
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright (C) 2026 by The Odamex Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//  Client cheat sequence management
+//
+//-----------------------------------------------------------------------------
+
 #pragma once
 
 //
@@ -17,13 +39,13 @@ struct cheatseq_t
 
 namespace cheat
 {
-    // keycheat handlers
-    bool AddKey(cheatseq_t* cheat, unsigned char key, bool* eat);
+	// keycheat handlers
+	bool AddKey(cheatseq_t* cheat, unsigned char key, bool* eat);
 
-    bool AutoMap    (cheatseq_t* cheat);
-    bool ChangeLevel(cheatseq_t* cheat);
-    bool IdMyPos    (cheatseq_t* cheat);
-    bool BeholdMenu (cheatseq_t* cheat);
-    bool ChangeMusic(cheatseq_t* cheat);
-    bool SetGeneric (cheatseq_t* cheat);
+	bool AutoMap    (cheatseq_t* cheat);
+	bool ChangeLevel(cheatseq_t* cheat);
+	bool IdMyPos    (cheatseq_t* cheat);
+	bool BeholdMenu (cheatseq_t* cheat);
+	bool ChangeMusic(cheatseq_t* cheat);
+	bool SetGeneric (cheatseq_t* cheat);
 }
