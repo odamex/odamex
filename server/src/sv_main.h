d28dfe79fd83c5cd4f4c7113083965ce12737431
Jim Thoenen <jjt@pobox.com>
2026-07-21 19:34:18 -0500

    Update stubs and headers to support p_acs's use of the relevant functions

--- server/src/sv_main.h
+++ server/src/sv_main.h
@@ -132,6 +132,7 @@ void SV_CheckTeam(player_t &player);
 void SV_SendUserInfo(const player_t &player, client_t* cl);
 void SV_Suicide(player_t &player);
 void SV_SpawnMobj(AActor *mo);
+void SV_UpdateMobj(AActor* mo);
 void SV_SpawnHighPriorityMobj(AActor *mo);
 void SV_TouchSpecial(AActor& special, player_t& player);
 
