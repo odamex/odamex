4ec7b100842a4ba521e4de8b7845f48386d331c5
bcahue <bcahue@users.noreply.github.com>
2026-07-30 02:04:23 -0400

    change odalaunch to use this generated file instead

---
+++ odalaunch/src/cvardoc_data.h
@@ -0,0 +1,31 @@
+// Emacs style mode select   -*- C++ -*-
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright (C) 2006-2026 by The Odamex Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// DESCRIPTION:
+//   The cvar documentation compiled into the launcher.
+//
+//-----------------------------------------------------------------------------
+
+#pragma once
+
+#include <cstddef>
+
+// These variables get filled in by cmake on build
+extern const char ODAMEX_CVARDOC_JSON[];
+extern const size_t ODAMEX_CVARDOC_JSON_LEN;
+extern const char ODASRV_CVARDOC_JSON[];
+extern const size_t ODASRV_CVARDOC_JSON_LEN;
