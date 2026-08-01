0752157c412e44eecf7356b3d82d76629d441e68
bcahue <bcahue@users.noreply.github.com>
2026-08-01 13:10:01 -0400

    use lib_buildgen for freetype

--- libraries/freetype-lib.cmake
+++ libraries/freetype-lib.cmake
@@ -12,49 +12,16 @@ if(BUILD_CLIENT)
     set(FREETYPE_LIBRARY
       "${CMAKE_CURRENT_BINARY_DIR}/local/lib/${libprefix}freetype${libsuffix}")
 
-    set(_FREETYPE_GEN_ARGS
-      -S "${CMAKE_CURRENT_SOURCE_DIR}/freetype"
-      -B "${CMAKE_CURRENT_BINARY_DIR}/freetype-build"
-      -G "${CMAKE_GENERATOR}"
-      "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
-      "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
-      "-DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"
-      "-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/local"
-      "-DCMAKE_INSTALL_LIBDIR=lib"
-      "-DBUILD_SHARED_LIBS=OFF"
-      "-DFT_DISABLE_HARFBUZZ=ON"
-      "-DFT_DISABLE_BROTLI=ON"
-      "-DFT_DISABLE_BZIP2=ON"
-      "-DFT_DISABLE_PNG=ON"
-      "-DFT_DISABLE_ZLIB=ON")
-    if(CMAKE_GENERATOR_PLATFORM)
-      list(APPEND _FREETYPE_GEN_ARGS -A "${CMAKE_GENERATOR_PLATFORM}")
-    endif()
-    if(CMAKE_GENERATOR_TOOLSET)
-      list(APPEND _FREETYPE_GEN_ARGS -T "${CMAKE_GENERATOR_TOOLSET}")
-    endif()
-    if(CMAKE_C_COMPILER)
-      list(APPEND _FREETYPE_GEN_ARGS "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
-    endif()
-    if(CMAKE_LINKER)
-      list(APPEND _FREETYPE_GEN_ARGS "-DCMAKE_LINKER=${CMAKE_LINKER}")
-    endif()
-    if(CMAKE_RC_COMPILER)
-      list(APPEND _FREETYPE_GEN_ARGS "-DCMAKE_RC_COMPILER=${CMAKE_RC_COMPILER}")
-    endif()
-    if(CMAKE_OSX_ARCHITECTURES)
-      string(REPLACE ";" "\\;" _OSX_ARCHS "${CMAKE_OSX_ARCHITECTURES}")
-      list(APPEND _FREETYPE_GEN_ARGS "-DCMAKE_OSX_ARCHITECTURES=${_OSX_ARCHS}")
-    endif()
-    if(CMAKE_OSX_DEPLOYMENT_TARGET)
-      list(APPEND _FREETYPE_GEN_ARGS "-DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}")
-    endif()
-
     lib_buildgen(
       LIBRARY freetype
-      PARAMS ${_FREETYPE_GEN_ARGS})
+      PARAMS "-DBUILD_SHARED_LIBS=OFF"
+             "-DFT_DISABLE_HARFBUZZ=ON"
+             "-DFT_DISABLE_BROTLI=ON"
+             "-DFT_DISABLE_BZIP2=ON"
+             "-DFT_DISABLE_PNG=ON"
+             "-DFT_DISABLE_ZLIB=ON")
 
-		lib_build(LIBRARY freetype)
+    lib_build(LIBRARY freetype)
   endif()
 
   find_package(Freetype REQUIRED)
