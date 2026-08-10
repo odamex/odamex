b027f07ae8177df84e92212e456aa27884fdd50d
bcahue <bcahue@users.noreply.github.com>
2026-08-10 19:39:04 -0400

    display author in map command

--- common/g_level.cpp
+++ common/g_level.cpp
@@ -34,6 +34,7 @@
 #include "d_event.h"
 #include "d_main.h"
 #include "g_game.h"
+#include "g_mapinfo.h"
 #include "gi.h"
 #include "i_system.h"
 #include "p_acs.h"
@@ -539,7 +540,12 @@ BEGIN_COMMAND (map)
 	}
 	else
 	{
-		PrintFmt(PRINT_HIGH, "The current map is {}: \"{}\"\n", level.mapname, level.level_name);
+		std::string current =
+		    fmt::format("The current map is {}: \"{}\"", level.mapname, level.level_name);
+		if (!level.author.empty())
+			current += fmt::format(" by {}", G_StripAuthorPrefix(level.author));
+
+		PrintFmt(PRINT_HIGH, "{}\n", current);
 	}
 }
 END_COMMAND (map)
