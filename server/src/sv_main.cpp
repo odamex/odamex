16ef7cfa9a2ca94ebedc25eb36cd7fa1333ae12c
Mia McMahill <miadawnmc@gmail.com>
2026-08-23 00:01:47 -0500

    Make sure script hooks send updated cvars to clients
    This isn't the ideal solution because it leads to unlatching cvars at times we might not otherwise do it, but it works for now (famous last words)

--- server/src/sv_main.cpp
+++ server/src/sv_main.cpp
@@ -1599,9 +1599,9 @@ static void SendServerSettings(player_t& pl)
 //
 //	Sends server settings to clients when changed
 //
-void SV_ServerSettingChange()
+void SV_ServerSettingChange(bool force)
 {
-	if (gamestate != GS_LEVEL)
+	if (gamestate != GS_LEVEL && !force)
 	{
 		return;
 	}
