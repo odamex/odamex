7fe1f758de49f97867f027ca1b6997e2767c8b70
bcahue <bcahue@users.noreply.github.com>
2026-08-28 16:30:37 -0400

    apply same potential deref fix to the server

--- server/src/sv_game.cpp
+++ server/src/sv_game.cpp
@@ -509,15 +509,20 @@ void G_TeamSpawnPlayer(player_t &player) // [Toke - CTF - starts] Modified this 
 
 	const mapthing2_t* spawnspot = spot;
 
-	if (!spot && !playerstarts.empty())
-		spawnspot = &P_GetPlayerStart(player.id - 1);
-	else
+	if (spot)
 	{
 		if (player.id < 4)
 			spot->type = player.id+1;
 		else
 			spot->type = player.id+4001-4;
 	}
+	else if (!playerstarts.empty())
+	{
+		spawnspot = &P_GetPlayerStart(player.id - 1);
+	}
+
+	if (!spawnspot)
+		I_Error("No appropriate team starts");
 
 	P_SpawnPlayer(player, *spawnspot);
 }
@@ -552,17 +557,23 @@ void G_DeathMatchSpawnPlayer(player_t &player)
 
 	const mapthing2_t* spawnspot = spot;
 
-	if (!spot && !playerstarts.empty())
+	if (spot)
+	{
+		if (player.id < 4)
+			spot->type = player.id+1;
+		else
+			spot->type = player.id+4001-4;	// [RH] > 4 players
+	}
+	else if (!playerstarts.empty())
 	{
 		// no good spot, so the player will probably get stuck
 		spawnspot = &P_GetPlayerStart(player.id - 1);
 	}
 	else
 	{
-		if (player.id < 4)
-			spot->type = player.id+1;
-		else
-			spot->type = player.id+4001-4;	// [RH] > 4 players
+		// There is at least one deathmatch start or we would
+		// have errored out above, so telefrag into it.
+		spawnspot = &DeathMatchStarts.front();
 	}
 
 	P_SpawnPlayer (player, *spawnspot);
