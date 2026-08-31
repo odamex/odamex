77cdf35dd541d1300e54e64646529edd1ab05960
Mia McMahill <miadawnmc@gmail.com>
2026-08-31 15:22:47 -0500

    Skip empty demos during the demoloop
    We already skip over unrecognized formats, so this is more consistent
    with that as well as with ports like Woof. It's also just annoying for
    players who have a wad that happens to have an empty demo to have it
    suddenly jump to the fullscreen console if they sit on the title screen
    for too long.

--- client/src/cl_game.cpp
+++ client/src/cl_game.cpp
@@ -1919,7 +1919,7 @@ void G_DoPlayDemo(bool justStreamInput)
 			Z_Free(demobuffer);
 
 		PrintFmt(PRINT_WARNING, "DOOM Demo file too short\n");
-		gameaction = ga_fullconsole;
+		gameaction = singledemo ? ga_fullconsole : ga_nothing;
 		return;
 	}
 
