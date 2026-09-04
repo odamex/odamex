9b864210be02d4e884c73c644bbf52c9ab6e96a6
Mia McMahill <miadawnmc@gmail.com>
2026-09-03 23:56:52 -0500

    Check minimapactive instead

--- client/src/am_map.cpp
+++ client/src/am_map.cpp
@@ -2008,7 +2008,7 @@ void AM_drawMarks()
 void AM_drawCrosshair(am_color_t color)
 {
 	// Don't draw on top of the player arrow
-	if (am_followplayer || (am_ovminimap && viewactive))
+	if (am_followplayer || minimapactive)
 		return;
 
 	// single point for now
