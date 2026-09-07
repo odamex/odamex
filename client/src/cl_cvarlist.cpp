04c64d1729fc87b1d69da6c2529d025d7176d6c1
teesquared <3665500+teesquared@users.noreply.github.com>
2026-09-07 17:26:55 -0400

    don't lint magic numbers in cvar range

--- client/src/cl_cvarlist.cpp
+++ client/src/cl_cvarlist.cpp
@@ -206,6 +206,7 @@ CVAR_RANGE(				am_ovscaleheight, "0.5", "Scale height of overlay minimap",
 						CVARTYPE_FLOAT, CVAR_CLIENTARCHIVE | CVAR_NOENABLEDISABLE, 0.0f, 1.0f)
 
 CVAR_RANGE(				am_ovscalelock, "0", "Overlay minimap scale lock (0:None, 1:Width, 2:Height)",
+						// NOLINTNEXTLINE(readability-magic-numbers) - cvar range
 						CVARTYPE_BYTE, CVAR_CLIENTARCHIVE | CVAR_NOENABLEDISABLE, 0.0f, 2.0f)
 
 CVAR_RANGE(				am_ovlocation, "1", "Overlay minimap location (0:LeftTop, 1:LeftMid, 2:LeftBot, 3:RightTop, 4:RightMid, 5:RightBot)",
