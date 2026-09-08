992c90b57afe559dbdd24d9aff3a17e3f2fd5bac
Jim Thoenen <jjt@pobox.com>
2026-09-08 11:38:46 -0500

    A bunch more tests

--- tests/unit-tests/test/t_LargeMessageQueue.cpp
+++ tests/unit-tests/test/t_LargeMessageQueue.cpp
@@ -55,3 +55,153 @@ TEST_F(LargeMessageQueueFixture, BasicSingleMessage)
     EXPECT_EQ((std::array{'d','W','o','r','l'}),  fragment);
     EXPECT_EQ(0, emptyResult.size);
 }
+
+TEST_F(LargeMessageQueueFixture, GiantMessage)
+{
+    std::string bigBoy(63 * 1024, '_');
+
+    char crawler = 'a';
+    for (auto iter = bigBoy.begin(); iter < bigBoy.end(); iter += 1024)
+    {
+        std::fill(iter, iter + 1024, crawler);
+        crawler = crawler < 'z' ? crawler + 1 : 'a';
+    }
+
+    m_queue.Write(svc_print, bigBoy);
+
+    // The first fragment has the mini-header.  Deal with that first so that we can have
+    // a nice orderly test of the subsequent big chunks.
+
+    buf_t firstFragment{16};
+
+    const auto firstResult = m_queue.NextFragment(firstFragment.maxsize(), firstFragment.ptr());
+
+    EXPECT_EQ(firstResult.size,  16);
+    EXPECT_EQ(firstResult.state, FragmentationStateEnum::FIRST_FRAGMENT);
+
+    firstFragment.setcursize(firstResult.size);
+
+    EXPECT_EQ(firstFragment.ReadUnVarint(), svc_print);
+    EXPECT_EQ(firstFragment.ReadUnVarint(), 63 * 1024);
+
+    const size_t firstRunOfPayload = firstFragment.BytesLeftToRead();
+    EXPECT_GT(firstRunOfPayload, 0);
+
+    for (int i = 0; i < firstRunOfPayload; ++i)
+    {
+        EXPECT_EQ(firstFragment.ReadByte(), 'a');
+    }
+
+    auto testBigFragment = [&] (size_t size, char fillChar, FragmentationStateEnum state)
+    {
+        std::string fragment(size, '_');
+        const std::string expectedValue(size, fillChar);
+
+        const auto result = m_queue.NextFragment(fragment.size(), fragment.begin());
+
+        EXPECT_EQ(result.state, state);
+        EXPECT_EQ(result.size,  size);
+        EXPECT_EQ(fragment,     expectedValue);
+    };
+
+    testBigFragment(1024 - firstRunOfPayload, 'a', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'b', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'c', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'd', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'e', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'f', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'g', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'h', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'i', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'j', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'k', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'l', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'm', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'n', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'o', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'p', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'q', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'r', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 's', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 't', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'u', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'v', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'w', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'x', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'y', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'z', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'a', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'b', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'c', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'd', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'e', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'f', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'g', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'h', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'i', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'j', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'k', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'l', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'm', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'n', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'o', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'p', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'q', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'r', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 's', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 't', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'u', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'v', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'w', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'x', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'y', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'z', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'a', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'b', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'c', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'd', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'e', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'f', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'g', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'h', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'i', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'j', FragmentationStateEnum::CONTINUATION_FRAGMENT);
+    testBigFragment(1024 , 'k', FragmentationStateEnum::LAST_FRAGMENT);
+
+}
+
+TEST_F(LargeMessageQueueFixture, MultiMessage)
+{
+    m_queue.Write(svc_print, "HelloWorld");
+    m_queue.Write(svc_print, "FooBar");
+
+    std::array<char, 8> fragment;
+
+    auto result = m_queue.NextFragment(fragment.size(), fragment.begin());
+
+    EXPECT_EQ(result.size, 8);
+    EXPECT_EQ(result.state, FragmentationStateEnum::FIRST_FRAGMENT);
+    EXPECT_EQ(fragment[0], svc_print);
+    EXPECT_EQ(fragment[1], 10);
+    EXPECT_EQ((std::string{fragment.begin() + 2, fragment.end()}), "HelloW");
+
+    result = m_queue.NextFragment(fragment.size(), fragment.begin());
+
+    EXPECT_EQ(result.size, 4);
+    EXPECT_EQ(result.state, FragmentationStateEnum::LAST_FRAGMENT);
+    EXPECT_EQ((std::string{fragment.begin(), fragment.begin() + result.size}), "orld");
+
+    result = m_queue.NextFragment(fragment.size(), fragment.begin());
+
+    EXPECT_EQ(result.size, 8);
+    EXPECT_EQ(result.state, FragmentationStateEnum::ONE_SHOT);
+    EXPECT_EQ(fragment[0], svc_print);
+    EXPECT_EQ(fragment[1], 6);
+    EXPECT_EQ((std::string{fragment.begin() + 2, fragment.begin() + result.size}), "FooBar");
+
+    result = m_queue.NextFragment(fragment.size(), fragment.begin());
+
+    EXPECT_EQ(result.size, 0);
+    EXPECT_EQ(result.state, FragmentationStateEnum::NONE);
+}
+
