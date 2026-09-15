603b2019be35b6c6eadae6a74e5323d47a0d3789
Ralph Vickers <ralphis@gmail.com>
2026-09-15 14:14:09 -0400

    Credit Andrew Apted for the great foundational work he did

--- client/src/r_voxel.cpp
+++ client/src/r_voxel.cpp
@@ -3,6 +3,8 @@
 //
 // Copyright (C) 2026 by The Odamex Team.
 //
+// Software voxel rendering based on the original work by Andrew Apted.
+//
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
